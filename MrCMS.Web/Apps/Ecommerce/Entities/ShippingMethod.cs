e33d73d8f657adf222c6c919e31b077709e6d698
gmstr <gary.m.strong@gmail.com>
2013-03-27 16:36:10 +0000

    Bringing over fixes from v0.2

--- MrCMS.Web/Apps/Ecommerce/Entities/ShippingMethod.cs
+++ MrCMS.Web/Apps/Ecommerce/Entities/ShippingMethod.cs
@@ -9,42 +9,4 @@ namespace MrCMS.Web.Apps.Ecommerce.Entities
         public virtual string Name { get; set; }
         public virtual string Description { get; set; }
     }
-
-    public class ShippingRateComputationMethod
-    {
-        public virtual ShippingMethod ShippingMethod { get; set; }
-
-        public virtual bool WeightSpecific { get; set; }
-        public virtual decimal WeightFrom { get; set; }
-        public virtual decimal WeightTo { get; set; }
-
-        public virtual bool IsPriceRange { get; set; }
-        public virtual decimal BasePrice { get; set; }
-        public virtual decimal? PriceTo { get; set; }
-
-        public virtual bool IsValid(IShippingCalculationRequest request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public virtual decimal? GetPrice(IShippingCalculationRequest request)
-        {
-            throw new NotImplementedException();
-            //if (IsValid(request.Weight))
-            //{
-            //    if (PriceTo.HasValue)
-            //    {
-            //        var difference = WeightTo - WeightFrom;
-            //        var amountFrom = request.Weight - WeightFrom;
-            //        var distanceBetweenWeights = amountFrom / difference;
-            //        var priceDifference = PriceTo - BasePrice;
-            //        return Math.Round((BasePrice + priceDifference * distanceBetweenWeights).GetValueOrDefault(), 2,
-            //                          MidpointRounding.AwayFromZero);
-            //    }
-            //    else
-            //        return BasePrice;
-            //}
-            //return null;
-        }
-    }
 }
--- MrCMS.Web/Apps/Ecommerce/Indexing/ProductSearchIndex.cs
+++ MrCMS.Web/Apps/Ecommerce/Indexing/ProductSearchIndex.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Lucene.Net.Analysis;
 using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Documents;
 using Lucene.Net.Index;
-using Lucene.Net.Util;
 using MrCMS.Entities.Multisite;
 using MrCMS.Indexing.Management;
 using MrCMS.Web.Apps.Ecommerce.Pages;
@@ -13,6 +13,7 @@ using System.Linq;
 using MrCMS.Indexing.Utils;
 using MrCMS.Helpers;
 using NHibernate.Criterion;
+using Version = Lucene.Net.Util.Version;
 
 namespace MrCMS.Web.Apps.Ecommerce.Indexing
 {
@@ -20,19 +21,7 @@ namespace MrCMS.Web.Apps.Ecommerce.Indexing
     {
         public Document Convert(Product entity)
         {
-            var document = new Document();
-            document.AddField("id", entity.Id.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED)
-                    .AddField("name", entity.Name, Field.Store.YES, Field.Index.ANALYZED)
-                    .AddField("bodycontent", entity.BodyContent, Field.Store.NO, Field.Index.ANALYZED)
-                    .AddField("metatitle", entity.MetaTitle, Field.Store.NO, Field.Index.ANALYZED)
-                    .AddField("metakeywords", entity.MetaKeywords, Field.Store.NO, Field.Index.ANALYZED)
-                    .AddField("metadescription", entity.MetaDescription, Field.Store.NO, Field.Index.ANALYZED)
-                    .AddField("urlsegment", entity.UrlSegment, Field.Store.NO, Field.Index.ANALYZED)
-                    .AddField("publishOn",
-                              entity.PublishOn.HasValue
-                                  ? DateTools.DateToString(entity.PublishOn.Value, DateTools.Resolution.SECOND)
-                                  : null, Field.Store.NO, Field.Index.NOT_ANALYZED);
-            return document;
+            return new Document().SetFields(Definitions, entity);
         }
 
         public Term GetIndex(Product entity)
@@ -69,6 +58,66 @@ namespace MrCMS.Web.Apps.Ecommerce.Indexing
             return new StandardAnalyzer(Version.LUCENE_30);
         }
 
-        public string Name { get { return "Product Search Index"; }}
+
+        public string IndexName { get { return "Product Search Index"; } }
+
+        public IEnumerable<FieldDefinition<Product>> Definitions
+        {
+            get
+            {
+                yield return Id;
+                yield return Name;
+                yield return BodyContent;
+                yield return MetaTitle;
+                yield return MetaKeywords;
+                yield return MetaDescription;
+                yield return UrlSegment;
+                yield return PublishOn;
+            }
+        }
+        public static FieldDefinition<Product> Id { get { return _id; } }
+        public static FieldDefinition<Product> Name { get { return _name; } }
+        public static FieldDefinition<Product> BodyContent { get { return _bodyContent; } }
+        public static FieldDefinition<Product> MetaTitle { get { return _metaTitle; } }
+        public static FieldDefinition<Product> MetaKeywords { get { return _metaKeywords; } }
+        public static FieldDefinition<Product> MetaDescription { get { return _metaDescription; } }
+        public static FieldDefinition<Product> UrlSegment { get { return _urlSegment; } }
+        public static FieldDefinition<Product> PublishOn { get { return _publishOn; } }
+
+
+        private static readonly FieldDefinition<Product> _id =
+            new FieldDefinition<Product>("id", webpage => webpage.Id.ToString(), Field.Store.YES,
+                                         Field.Index.NOT_ANALYZED);
+
+        private static readonly FieldDefinition<Product> _name =
+            new FieldDefinition<Product>("name", webpage => webpage.Name, Field.Store.YES,
+                                         Field.Index.ANALYZED);
+
+        private static readonly FieldDefinition<Product> _bodyContent =
+            new FieldDefinition<Product>("bodycontent", webpage => webpage.BodyContent, Field.Store.NO,
+                                         Field.Index.ANALYZED);
+
+        private static readonly FieldDefinition<Product> _metaTitle =
+            new FieldDefinition<Product>("metatitle", webpage => webpage.MetaTitle, Field.Store.NO,
+                                         Field.Index.ANALYZED);
+
+        private static readonly FieldDefinition<Product> _metaKeywords =
+            new FieldDefinition<Product>("metakeywords", webpage => webpage.MetaKeywords, Field.Store.NO,
+                                         Field.Index.ANALYZED);
+
+        private static readonly FieldDefinition<Product> _metaDescription =
+            new FieldDefinition<Product>("metadescription",
+                                         webpage => webpage.MetaDescription, Field.Store.NO, Field.Index.ANALYZED);
+
+        private static readonly FieldDefinition<Product> _urlSegment =
+            new FieldDefinition<Product>("urlsegment", webpage => webpage.UrlSegment, Field.Store.NO,
+                                         Field.Index.ANALYZED);
+
+        private static readonly FieldDefinition<Product> _publishOn =
+            new FieldDefinition<Product>("publishOn",
+                                         webpage =>
+                                         DateTools.DateToString(webpage.PublishOn.GetValueOrDefault(DateTime.MaxValue),
+                                                                DateTools.Resolution.SECOND), Field.Store.NO,
+                                         Field.Index.NOT_ANALYZED);
     }
 }
