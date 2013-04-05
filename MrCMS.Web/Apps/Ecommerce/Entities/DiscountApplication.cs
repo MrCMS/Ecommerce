823e475ed35f6e3650844cd4552ee9b9f611efab
ds <dsalamande@gmail.com>
2013-04-05 22:41:17 +0200

    Discount Module

--- MrCMS.Web/Apps/Ecommerce/Entities/DiscountApplication.cs
+++ MrCMS.Web/Apps/Ecommerce/Entities/DiscountApplication.cs
@@ -1,12 +1,21 @@
 ﻿using MrCMS.Entities;
 using MrCMS.Web.Apps.Ecommerce.Models;
+using System.Collections.Generic;
 
 namespace MrCMS.Web.Apps.Ecommerce.Entities
 {
     public abstract class DiscountApplication : SiteEntity
     {
+        public DiscountApplication()
+        {
+            Discounts = new List<Discount>();
+        }
+
         public abstract decimal GetDiscount(CartModel cartModel);
-        public virtual string DiscountApplicationType { get; set; }
+
+        public virtual IList<Discount> Discounts { get; set; }
+
+        public abstract void CopyValues(DiscountApplication application);
     }
 
     public class FixedAmount : DiscountApplication
@@ -17,5 +26,10 @@ namespace MrCMS.Web.Apps.Ecommerce.Entities
         }
 
         public virtual decimal DiscountAmount { get; set; }
+
+        public override void CopyValues(DiscountApplication application)
+        {
+            this.DiscountAmount = ((FixedAmount)application).DiscountAmount;
+        }
     }
 }
--- MrCMS.Web/Apps/Ecommerce/Services/DiscountManager.cs
+++ MrCMS.Web/Apps/Ecommerce/Services/DiscountManager.cs
@@ -35,12 +35,81 @@ namespace MrCMS.Web.Apps.Ecommerce.Services
 
         public void Save(Discount discount, DiscountLimitation discountLimitation, DiscountApplication discountApplication)
         {
-            _session.Transact(session => session.SaveOrUpdate(discount));
+            Discount oldDiscount = Get(discount.Id);
+            DiscountLimitation oldDiscountLimitation = oldDiscount.Limitation;
+            if (discountLimitation != null && oldDiscountLimitation != null)
+            {
+                if (oldDiscount.Limitation.GetType() != discountLimitation.GetType())
+                {
+                    DeleteOldLimitationOrApplication(oldDiscount, oldDiscountLimitation);
+                    oldDiscount.Limitation = discountLimitation;
+                }
+                else
+                {
+                    oldDiscount.Limitation.CopyValues(discountLimitation);
+                };
+            }
+            else if (discountLimitation == null && oldDiscountLimitation != null)
+            {
+                DeleteOldLimitationOrApplication(oldDiscount, oldDiscountLimitation);
+                oldDiscount.Limitation = discountLimitation;
+            }
+            else
+            {
+                oldDiscount.Limitation = discountLimitation;
+            }
+
+            DiscountApplication oldDiscountApplication = oldDiscount.Application;
+            if (discountApplication != null && oldDiscountApplication != null)
+            {
+                if (oldDiscount.Application.GetType() != discountApplication.GetType())
+                {
+                    DeleteOldLimitationOrApplication(oldDiscount, oldDiscountApplication);
+                    oldDiscount.Application = discountApplication;
+                }
+                else
+                {
+                    oldDiscount.Application.CopyValues(discountApplication);
+                };
+            }
+            else if (discountApplication == null && oldDiscountApplication != null)
+            {
+                DeleteOldLimitationOrApplication(oldDiscount, oldDiscountApplication);
+                oldDiscount.Application = discountApplication;
+            }
+            else
+            {
+                oldDiscount.Application = discountApplication;
+            }
+
+            _session.Transact(session =>
+            {
+                session.SaveOrUpdate(discount);
+            });
+        }
+        private void DeleteOldLimitationOrApplication(Discount oldDiscount, object oldLimitationOrApplication)
+        {
+            oldDiscount.Limitation = null;
+            _session.Transact(session =>
+            {
+                session.SaveOrUpdate(oldDiscount);
+            });
+            _session.Transact(session =>
+            {
+                session.Delete(oldLimitationOrApplication);
+            });
         }
 
         public void Delete(Discount discount)
         {
-            _session.Transact(session => session.Delete(discount));
+            _session.Transact(session => 
+                {
+                    session.Delete(discount);
+                    if (discount.Limitation != null)
+                        session.Delete(discount.Limitation);
+                    if (discount.Application != null)
+                        session.Delete(discount.Application);
+                });
         }
     }
 }
