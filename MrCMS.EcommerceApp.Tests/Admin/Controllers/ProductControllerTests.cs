90586daa1648496312560e31476661689880f6f5
gmstr <gary.m.strong@gmail.com>
2013-04-17 15:49:13 +0100

    Adding test coverage for product category admin

--- MrCMS.EcommerceApp.Tests/Admin/Controllers/ProductControllerTests.cs
+++ MrCMS.EcommerceApp.Tests/Admin/Controllers/ProductControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using FakeItEasy;
 using FluentAssertions;
@@ -10,6 +11,7 @@ using MrCMS.Web.Apps.Ecommerce.Pages;
 using MrCMS.Web.Apps.Ecommerce.Services;
 using MrCMS.Web.Apps.Ecommerce.Services.Categories;
 using MrCMS.Web.Apps.Ecommerce.Services.Products;
+using MrCMS.Website.ActionResults;
 using Xunit;
 
 namespace MrCMS.EcommerceApp.Tests.Admin.Controllers
@@ -109,12 +111,170 @@ namespace MrCMS.EcommerceApp.Tests.Admin.Controllers
 
             result.Should().BeOfType<RedirectToRouteResult>();
         }
+
         [Fact]
         public void ProductController_MakeMultiVariantPost_ShouldRedirectToTheWebpageEditActionForTheProductId()
         {
-            var product = new Product{Id=123};
+            var product = new Product { Id = 123 };
 
             var result = _productController.MakeMultiVariant(product, "option1", "option2", "option3");
+
+            result.RouteValues["controller"].Should().Be("Webpage");
+            result.RouteValues["action"].Should().Be("Edit");
+            result.RouteValues["id"].Should().Be(123);
+        }
+
+        [Fact]
+        public void ProductController_ViewCategories_ShouldReturnAPartialViewResult()
+        {
+            var product = new Product { Id = 123 };
+
+            var result = _productController.ViewCategories(product);
+
+            result.Should().BeOfType<PartialViewResult>();
+        }
+
+        [Fact]
+        public void ProductController_ViewCategories_ShouldReturnThePassedProductAsTheModel()
+        {
+            var product = new Product { Id = 123 };
+
+            var result = _productController.ViewCategories(product);
+
+            result.Model.Should().Be(product);
+        }
+
+        [Fact]
+        public void ProductController_AddCategory_ShouldReturnAPartialViewResult()
+        {
+            var product = new Product { Id = 123 };
+
+            var result = _productController.AddCategory(product, "query", 1);
+
+            result.Should().BeOfType<PartialViewResult>();
+        }
+
+        [Fact]
+        public void ProductController_AddCategory_ShouldSetTheProductToTheViewData()
+        {
+            var product = new Product { Id = 123 };
+
+            var result = _productController.AddCategory(product, "query", 1);
+
+            _productController.ViewData["product"].Should().Be(product);
+        }
+
+        [Fact]
+        public void ProductController_AddCategory_ShouldCallGetCategoriesOnTheCategoryService()
+        {
+            var product = new Product { Id = 123 };
+
+            var result = _productController.AddCategory(product, "query", 1);
+
+            A.CallTo(() => _categoryService.GetCategories(product, "query", 1)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void ProductController_AddCategory_ShouldReturnTheResultOfGetCategoriesAsTheModel()
+        {
+            var product = new Product { Id = 123 };
+            var pagedList = A.Fake<IPagedList<Category>>();
+            A.CallTo(() => _categoryService.GetCategories(product, "query", 1)).Returns(pagedList);
+
+            var result = _productController.AddCategory(product, "query", 1);
+
+            result.Model.Should().Be(pagedList);
+        }
+
+        [Fact]
+        public void ProductController_AddCategoryPOST_ShouldReturnAJsonResult()
+        {
+            var product = new Product { Id = 123 };
+
+            var result = _productController.AddCategory(product, 1);
+
+            result.Should().BeOfType<JsonResult>();
+        }
+
+        [Fact]
+        public void ProductController_AddCategoryPOST_ShouldCallProductServiceAddCategory()
+        {
+            var product = new Product { Id = 123 };
+
+            var result = _productController.AddCategory(product, 1);
+
+            A.CallTo(() => _productService.AddCategory(product, 1)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void ProductController_AddCategoryPOST_ReturnAModelOfTrueForAStandardCall()
+        {
+            var product = new Product { Id = 123 };
+
+            var result = _productController.AddCategory(product, 1);
+
+            result.Data.Should().Be(true);
+        }
+
+        [Fact]
+        public void ProductController_AddCategoryPOST_ReturnAModelOfFalseIfTheCallFails()
+        {
+            var product = new Product { Id = 123 };
+            A.CallTo(() => _productService.AddCategory(product, 1)).Throws<Exception>();
+
+            var result = _productController.AddCategory(product, 1);
+
+            result.Data.Should().Be(false);
+        }
+
+        [Fact]
+        public void ProductController_RemoveCategory_ReturnsAPartialViewResult()
+        {
+            var product = new Product { Id = 123 };
+
+            var result = _productController.RemoveCategory(product, 1);
+
+            result.Should().BeOfType<PartialViewResult>();
+        }
+
+        [Fact]
+        public void ProductController_RemoveCategory_ReturnsPassedProductAsModel()
+        {
+            var product = new Product { Id = 123 };
+
+            var result = _productController.RemoveCategory(product, 1);
+
+            result.Model.Should().Be(product);
+        }
+
+        [Fact]
+        public void ProductController_RemoveCategory_SetsViewDataForCategory()
+        {
+            var product = new Product { Id = 123 };
+            var category = new Category();
+            A.CallTo(() => _documentService.GetDocument<Category>(1)).Returns(category);
+
+            var result = _productController.RemoveCategory(product, 1);
+
+            _productController.ViewData["category"].Should().Be(category);
+        }
+
+        [Fact]
+        public void ProductController_RemoveCategoryPOST_ShouldCallRemoveCategoryOnProductService()
+        {
+            var product = new Product { Id = 123 };
+
+            _productController.RemoveCategory_POST(product, 1);
+
+            A.CallTo(() => _productService.RemoveCategory(product, 1)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void ProductController_RemoveCategoryPOST_ShouldRedirectToWebpageEdit()
+        {
+            var product = new Product { Id = 123 };
+
+            var result = _productController.RemoveCategory_POST(product, 1);
 
             result.RouteValues["controller"].Should().Be("Webpage");
             result.RouteValues["action"].Should().Be("Edit");
