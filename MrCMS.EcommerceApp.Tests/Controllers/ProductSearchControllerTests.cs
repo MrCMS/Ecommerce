ab354b3cc2e232e7579ebd14c8db1667f98d0d78
Gary Strong <gary.strong@thought.co.uk>
2013-04-19 11:48:36 +0100

    Updating name of controller in tests

---
+++ MrCMS.EcommerceApp.Tests/Controllers/ProductSearchControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using FakeItEasy;
+using FluentAssertions;
+using MrCMS.Entities.Documents.Layout;
+using MrCMS.Web.Apps.Ecommerce.Controllers;
+using MrCMS.Web.Apps.Ecommerce.Pages;
+using Xunit;
+
+namespace MrCMS.EcommerceApp.Tests.Controllers
+{
+    public class ProductSearchControllerTests
+    {
+        private readonly ProductSearchController _controller;
+
+        public ProductSearchControllerTests()
+        {
+            _controller = new ProductSearchController {RequestMock = A.Fake<HttpRequestBase>()};
+        }
+
+        private ProductSearch GetProductSearch()
+        {
+            return new ProductSearch {Layout = new Layout()};
+        }
+
+        [Fact]
+        public void ProductSearchController_Show_ReturnsAViewResult()
+        {
+            var productSearch = GetProductSearch();
+
+            var show = _controller.Show(productSearch);
+
+            show.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public void ProductSearchController_Show_ReturnsProductSearch()
+        {
+            var productSearch = GetProductSearch();
+
+            var show = _controller.Show(productSearch);
+
+            show.Model.Should().BeOfType<ProductSearch>();
+        }
+    }
+}
