2925ed1d85255c0b21a1fb04b028a6539ad5fc49
gmstr <gary.m.strong@gmail.com>
2013-04-25 13:10:33 +0100

    Updating Product search admin

--- MrCMS.Web/Apps/Ecommerce/Pages/ProductSearch.cs
+++ MrCMS.Web/Apps/Ecommerce/Pages/ProductSearch.cs
@@ -1,8 +1,11 @@
-﻿using MrCMS.Entities.Documents.Web;
+﻿using System.ComponentModel;
+using MrCMS.Entities.Documents.Web;
 
 namespace MrCMS.Web.Apps.Ecommerce.Pages
 {
     public class ProductSearch : Webpage, IUniquePage
     {
+        [DisplayName("Page Size")]
+        public virtual int PageSize { get; set; }
     }
 }
