1eb6d05c4cfc1c0103b27783d0b63afa8679bf65
ds <dsalamande@gmail.com>
2013-05-30 10:35:29 +0200

    Task: Fix bug when making product multivariant

--- MrCMS.Web/Apps/Ecommerce/Models/MakeMultivariantModel.cs
+++ MrCMS.Web/Apps/Ecommerce/Models/MakeMultivariantModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 
 namespace MrCMS.Web.Apps.Ecommerce.Models
@@ -6,11 +7,17 @@ namespace MrCMS.Web.Apps.Ecommerce.Models
     public class MakeMultivariantModel
     {
         public int ProductId { get; set; }
+        [DisplayName("Option 1")]
         public string Option1 { get; set; }
+        [DisplayName("Option 1 Value")]
         public string Option1Value { get; set; }
+        [DisplayName("Option 2")]
         public string Option2 { get; set; }
+        [DisplayName("Option 2 Value")]
         public string Option2Value { get; set; }
+        [DisplayName("Option 3")]
         public string Option3 { get; set; }
+        [DisplayName("Option 3 Value")]
         public string Option3Value { get; set; }
     }
 }
