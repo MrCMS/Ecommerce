8454e1f648865e3bcf12f82268dab8ebcf745537
ds <dsalamande@gmail.com>
2013-06-06 14:00:12 +0200

    Task: Add generic settings page for eCommerce - see description

---
+++ MrCMS.EcommerceApp.Tests/Admin/Controllers/EcommerceSettingsControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using FakeItEasy;
+using FluentAssertions;
+using MrCMS.Settings;
+using MrCMS.Web.Apps.Ecommerce.Areas.Admin.Controllers;
+using MrCMS.Web.Apps.Ecommerce.Entities.Tax;
+using MrCMS.Web.Apps.Ecommerce.Services.Tax;
+using MrCMS.Web.Apps.Ecommerce.Settings;
+using Xunit;
+
+namespace MrCMS.EcommerceApp.Tests.Admin.Controllers
+{
+    public class EcommerceSettingsControllerTests
+    {
+        private readonly EcommerceSettingsController _ecommerceSettingsController;
+        private readonly IConfigurationProvider _configurationProvider;
+        private readonly EcommerceSettings _ecommerceSettings;
+
+        public EcommerceSettingsControllerTests()
+        {
+            _configurationProvider = A.Fake<IConfigurationProvider>();
+            _ecommerceSettings = new EcommerceSettings();
+            _ecommerceSettingsController = new EcommerceSettingsController(_configurationProvider, _ecommerceSettings);
+        }
+
+        [Fact]
+        public void EcommerceSettingsController_Edit_ShouldCallSaveSettingsOnTheConfigurationProvider()
+        {
+            var settings = new EcommerceSettings();
+
+            var edit=_ecommerceSettingsController.Edit();
+
+            edit.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public void EcommerceSettingsController_EditPOST_ShouldCallSaveSettingsOnTheConfigurationProvider()
+        {
+            var settings = new EcommerceSettings();
+
+            _ecommerceSettingsController.Edit_POST(settings);
+
+            A.CallTo(() => _configurationProvider.SaveSettings(settings)).MustHaveHappened();
+        }
+    }
+}
---
+++ MrCMS.Web/Apps/Ecommerce/Settings/EcommerceSettings.cs
@@ -0,0 +1,23 @@
+﻿using MrCMS.Entities;
+using MrCMS.Settings;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MrCMS.Web.Apps.Ecommerce.Settings
+{
+    public class EcommerceSettings : SiteSettingsBase
+    {
+        [DisplayName("Page size for Administration")]
+        public int PageSizeAdmin { get; set; }
+        [DisplayName("Category Products per Page")]
+        public string CategoryProductsPerPage { get; set; }
+
+        public override bool RenderInSettings
+        {
+            get { return false; }
+        }
+    }
+}
