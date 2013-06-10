5b75d8e3c504be3ccb6d44633f16627b75bd913a
gmstr <gary.m.strong@gmail.com>
2013-06-10 08:54:18 +0100

    Removing page size from product search - to be determined by user an product setting

--- MrCMS.Web/Apps/Ecommerce/Pages/ProductSearch.cs
+++ MrCMS.Web/Apps/Ecommerce/Pages/ProductSearch.cs
@@ -5,7 +5,5 @@ namespace MrCMS.Web.Apps.Ecommerce.Pages
 {
     public class ProductSearch : Webpage, IUniquePage
     {
-        [DisplayName("Page Size")]
-        public virtual int PageSize { get; set; }
     }
 }
