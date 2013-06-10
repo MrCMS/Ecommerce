94bdff8fa2f2757cb32272b42d0c8dba843283d1
as <adem.sabic@thought.co.uk>
2013-06-10 18:35:10 +0200

    Task: Add a default layout for UI + Added Featured Products and Categories etc.

---
+++ MrCMS.Web/Themes/Ecommerce/Apps/Ecommerce/Content/custom/brands.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('#searchParam').keypress(function(key) {
+        var searchParam = $(this).val();
+        if (e.which == 13 && searchParam!="") {
+                $('form').submit();
+        }
+    });
+});
