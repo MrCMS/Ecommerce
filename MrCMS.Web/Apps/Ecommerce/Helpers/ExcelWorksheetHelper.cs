3b983cbe38ce58f9b4518a26e88036159b347d36
gmstr <gary.m.strong@gmail.com>
2013-06-14 11:06:33 +0100

    Tidying up code
    Starting work on simplifying Importer

---
+++ MrCMS.Web/Apps/Ecommerce/Helpers/ExcelWorksheetHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OfficeOpenXml;
+
+namespace MrCMS.Web.Apps.Ecommerce.Helpers
+{
+    public static class ExcelWorksheetHelper
+    {
+         public static bool HasValue(this ExcelWorksheet sheet, int row, int column)
+         {
+             return GetBaseValue(sheet,row,column) == null;
+         }
+
+         private static object GetBaseValue(ExcelWorksheet sheet, int row, int column)
+         {
+             return sheet.Cells[row, column] != null ? sheet.Cells[row, column].Value : null;
+         }
+         
+         public static T GetValue<T>(this ExcelWorksheet sheet, int row, int column) where T : class
+         {
+             return HasValue(sheet, row, column)
+                        ? Convert.ChangeType(GetBaseValue(sheet, row, column), typeof (T)) as T
+                        : null;
+         }
+    }
+}
