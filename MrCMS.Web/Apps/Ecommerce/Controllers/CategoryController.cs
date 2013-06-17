d58c766d313721cd1bed247500f1feab9145e982
as <adem.sabic@thought.co.uk>
2013-06-17 19:47:35 +0200

    Task: Page Design - Category page and Container

--- MrCMS.Web/Apps/Ecommerce/Controllers/CategoryController.cs
+++ MrCMS.Web/Apps/Ecommerce/Controllers/CategoryController.cs
@@ -1,25 +1,26 @@
 ﻿using System.Web.Mvc;
+using MrCMS.Entities.Documents.Web;
 using MrCMS.Web.Apps.Ecommerce.Pages;
 using MrCMS.Website.Controllers;
 using MrCMS.Web.Apps.Ecommerce.Services.Categories;
 using MrCMS.Web.Apps.Ecommerce.Services.Products;
-using MrCMS.Web.Apps.Ecommerce.Models;
-using System.Linq;
-
-using System;
 using System.Collections.Generic;
+using System;
+using MrCMS.Web.Apps.Ecommerce.Models;
 using MrCMS.Website;
 using MrCMS.Web.Apps.Ecommerce.Settings;
+using System.Linq;
+
 namespace MrCMS.Web.Apps.Ecommerce.Controllers
 {
-    public class ProductSearchController : MrCMSAppUIController<EcommerceApp>
+    public class CategoryController : MrCMSAppUIController<EcommerceApp>
     {
-        private readonly ICategoryService _categoryService;
+       private readonly ICategoryService _categoryService;
         private readonly IProductOptionManager _productOptionManager;
         private readonly IProductService _productService;
         private readonly IProductSearchService _productSearchService;
 
-        public ProductSearchController(ICategoryService categoryService, IProductOptionManager productOptionManager, IProductService productService, IProductSearchService productSearchService)
+        public CategoryController(ICategoryService categoryService, IProductOptionManager productOptionManager, IProductService productService, IProductSearchService productSearchService)
         {
             _categoryService = categoryService;
             _productOptionManager = productOptionManager;
@@ -27,18 +28,22 @@ namespace MrCMS.Web.Apps.Ecommerce.Controllers
             _productSearchService = productSearchService;
         }
 
-        public ViewResult Show(ProductSearch page)
+        public ViewResult Show(Category page)
         {
             ViewBag.ProductOptions = _productOptionManager.GetAllAttributeOptions();
             ViewBag.ProductSpecifications = _productOptionManager.ListSpecificationAttributes();
             ViewBag.ProductPriceRangeMin = 0;
             ViewBag.ProductPriceRangeMax = 5000;
             ViewBag.Categories = _categoryService.GetAll().Where(x => x.Parent != null && x.Parent.Parent == null && x.Products.Count() > 0).ToList();
+            if (page.Parent != null && page.Parent.Parent != null)
+                ViewBag.IsSubCategory = true;
+            else
+                ViewBag.IsSubCategory = false;
             return View(page);
         }
-
+        
         [HttpGet]
-        public PartialViewResult Results(string sortBy,string options,string specifications, decimal productPriceRangeMin = 0, decimal productPriceRangeMax = 0, int pageNo = 0, int pageSize=0)
+        public PartialViewResult Results(string sortBy, string options, string specifications, decimal productPriceRangeMin = 0, decimal productPriceRangeMax = 0, int pageNo = 0, int pageSize = 0, int categoryId = 0)
         {
             List<string> specs = new List<string>();
             if (!String.IsNullOrWhiteSpace(specifications))
@@ -48,13 +53,13 @@ namespace MrCMS.Web.Apps.Ecommerce.Controllers
                     string[] rawSpecs = specifications.Split(',');
                     foreach (var item in rawSpecs)
                     {
-                        if(item!=String.Empty)
+                        if (item != String.Empty)
                             specs.Add(item);
                     }
                 }
                 catch (Exception)
                 {
-                    
+
                 }
             }
             List<string> ops = new List<string>();
@@ -64,12 +69,12 @@ namespace MrCMS.Web.Apps.Ecommerce.Controllers
             }
 
             ProductPagedList products = new ProductPagedList(_productSearchService.SearchProducts(sortBy,
-                ops, 
-                specs, 
-                productPriceRangeMin, 
-                productPriceRangeMax, 
+                ops,
+                specs,
+                productPriceRangeMin,
+                productPriceRangeMax,
                 pageNo == 0 ? 1 : pageNo,
-                pageSize==0 ? Int32.Parse(MrCMSApplication.Get<EcommerceSettings>().CategoryProductsPerPage.Split(',').First()) : pageSize), null);
+                pageSize == 0 ? Int32.Parse(MrCMSApplication.Get<EcommerceSettings>().CategoryProductsPerPage.Split(',').First()) : pageSize,categoryId), null);
 
             return PartialView(products);
         }
