aa5a463ee7f059ab9561914229f8b9d992317448
as <adem.sabic@thought.co.uk>
2013-06-18 18:46:39 +0200

    Task: Cart UI - Structure

---
+++ MrCMS.Web/Apps/Ecommerce/Controllers/SetDeliveryDetailsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using MrCMS.Web.Apps.Ecommerce.Services.Cart;
+using MrCMS.Website.Controllers;
+using MrCMS.Web.Apps.Ecommerce.Pages;
+using MrCMS.Web.Apps.Ecommerce.Models;
+
+namespace MrCMS.Web.Apps.Ecommerce.Controllers
+{
+    public class SetDeliveryDetailsController : MrCMSAppUIController<EcommerceApp>
+    {
+        private readonly IGetCart _getCart;
+
+        public SetDeliveryDetailsController(IGetCart getCart)
+        {
+            _getCart = getCart;
+        }
+
+        public ViewResult Show(SetDeliveryDetails page)
+        {
+            return View(page);
+        }
+    }
+}
---
+++ MrCMS.Web/Apps/Ecommerce/Metadata/PaymentDetailsMetadata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using MrCMS.Entities.Documents;
+using MrCMS.Entities.Documents.Metadata;
+using MrCMS.Web.Apps.Ecommerce.Pages;
+
+namespace MrCMS.Web.Apps.Ecommerce.Metadata
+{
+    public class PaymentDetailsMetadata : DocumentMetadataMap<PaymentDetails>
+    {
+        public override ChildrenListType ChildrenListType
+        {
+            get { return ChildrenListType.WhiteList; }
+        }
+
+        public override IEnumerable<Type> ChildrenList
+        {
+            get { yield return typeof(PaymentDetails); }
+        }
+
+        public override bool RequiresParent { get { return true; } }
+        public override bool AutoBlacklist { get { return true; } }
+
+        public override string App
+        {
+            get { return "Ecommerce"; }
+        }
+
+        public override string WebGetController
+        {
+            get { return "PaymentDetails"; }
+        }
+        public override string WebGetAction
+        {
+            get { return "Show"; }
+        }
+    }
+}
