0a49ab7e05f0d4183a129d12cb033662c4e09665
as <adem.sabic@thought.co.uk>
2013-06-19 13:15:07 +0200

    Task: Cart UI structure (controles, views, css)

--- MrCMS.Web/Apps/Ecommerce/Controllers/SetDeliveryDetailsController.cs
+++ MrCMS.Web/Apps/Ecommerce/Controllers/SetDeliveryDetailsController.cs
@@ -3,20 +3,25 @@ using MrCMS.Web.Apps.Ecommerce.Services.Cart;
 using MrCMS.Website.Controllers;
 using MrCMS.Web.Apps.Ecommerce.Pages;
 using MrCMS.Web.Apps.Ecommerce.Models;
+using MrCMS.Web.Apps.Ecommerce.Services.Products;
 
 namespace MrCMS.Web.Apps.Ecommerce.Controllers
 {
     public class SetDeliveryDetailsController : MrCMSAppUIController<EcommerceApp>
     {
         private readonly IGetCart _getCart;
+        private readonly IProductSearchService _productSearchService;
 
-        public SetDeliveryDetailsController(IGetCart getCart)
+        public SetDeliveryDetailsController(IGetCart getCart, IProductSearchService productSearchService)
         {
+            _productSearchService = productSearchService;
             _getCart = getCart;
         }
 
         public ViewResult Show(SetDeliveryDetails page)
         {
+            ViewBag.ShopUrl = _productSearchService.GetSiteProductSearch().LiveUrlSegment;
+            ViewBag.BasketUrl = _getCart.GetSiteCart().LiveUrlSegment;
             return View(page);
         }
     }
