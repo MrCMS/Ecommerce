535fe53cd5ddcb8490e65e500a5e9492e3bc49b4
as <adem.sabic@thought.co.uk>
2013-07-15 11:29:29 +0200

    Testing Import/Export Manager - v3 - Unit Testing

--- MrCMS.EcommerceApp.Tests/Services/ImportExportManagerTests.cs
+++ MrCMS.EcommerceApp.Tests/Services/ImportExportManagerTests.cs
@@ -1,8 +1,12 @@
-﻿using FakeItEasy;
+﻿using System.Collections.Generic;
+using System.IO;
+using FakeItEasy;
 using FluentAssertions;
 using MrCMS.Services;
 using MrCMS.Web.Apps.Ecommerce.Services.ImportExport;
+using MrCMS.Web.Apps.Ecommerce.Services.ImportExport.DTOs;
 using MrCMS.Web.Apps.Ecommerce.Services.Products;
+using OfficeOpenXml;
 using Xunit;
 
 namespace MrCMS.EcommerceApp.Tests.Services
@@ -25,6 +29,49 @@ namespace MrCMS.EcommerceApp.Tests.Services
             _importExportManager = new ImportExportManager(_importProductsValidationService, _importProductsService, _productVariantService,
                                                            _indexService);
         }
+
+        [Fact]
+        public void ImportExportManager_ImportProductsFromExcel_ShouldNotBeNull()
+        {
+            var result = _importExportManager.ImportProductsFromExcel(GetDefaultStream());
+
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void ImportExportManager_ImportProductsFromExcel_ShouldReturnDictionary()
+        {
+            var result = _importExportManager.ImportProductsFromExcel(GetDefaultStream());
+
+            result.Should().HaveCount(0);
+        }
+
+        [Fact]
+        public void ImportExportManager_ImportProductsFromExcel_ShouldCallGetIndexesOfIndexService()
+        {
+            _importExportManager.ImportProductsFromExcel(GetDefaultStream());
+
+            A.CallTo(() => _indexService.GetIndexes()).MustHaveHappened();
+        }
+
+        [Fact]
+        public void ImportExportManager_ImportProductsFromExcel_ShouldCallImportProductsFromDTOsOfImportProductsService()
+        {
+            _importExportManager.ImportProductsFromExcel(GetDefaultStream());
+
+            A.CallTo(() => _importProductsService.ImportProductsFromDTOs(new List<ProductImportDataTransferObject>())).MustHaveHappened();
+        }
+
+        [Fact]
+        public void ImportExportManager_ImportProductsFromExcel_ShouldCallValidateBusinessLogicOfImportProductsValidationService()
+        {
+            var productsToImport = A.Fake<List<ProductImportDataTransferObject>>();
+
+            _importExportManager.ImportProductsFromExcel(GetDefaultStream());
+
+            A.CallTo(() => _importProductsValidationService.ValidateBusinessLogic(productsToImport)).MustHaveHappened();
+        }
+
         [Fact]
         public void ImportExportManager_ExportProductsToExcel_ShouldReturnByteArray()
         {
@@ -41,5 +88,9 @@ namespace MrCMS.EcommerceApp.Tests.Services
             A.CallTo(() => _productVariantService.GetAll()).MustHaveHappened();
         }
 
+        private static Stream GetDefaultStream()
+        {
+            return new MemoryStream(0);
+        }
     }
 }
