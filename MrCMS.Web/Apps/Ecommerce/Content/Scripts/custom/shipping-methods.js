60a68d0c4f07d8fa55bb8d811cc1a3a2da472a67
as <adem.sabic@thought.co.uk>
2013-07-22 12:52:37 +0200

    Creating Tax Aware class for Shipping Rates and fixing some bugs with code related to TaxesEnabled property

---
+++ MrCMS.Web/Apps/Ecommerce/Content/Scripts/custom/shipping-methods.js
@@ -0,0 +1,41 @@
+﻿$(function () {
+    $(document).on('change', "select[id='ShippingCalculation']", function () {
+        updateShippingMethod();
+    });
+    $(document).on('click', "#continue", function () {
+        if ($("#ShippingCalculation").val() !== null && $("#ShippingCalculation").val() !== "") {
+            $("form#deliveryDetails").submit();
+        }
+        return false;
+    });
+    
+    function updateShippingMethod() {
+        $.post('/Apps/Ecommerce/Cart/AddShippingMethod',
+           { id: $("select[id='ShippingCalculation']").val() },
+           function (response) {
+               parent.$.get('/Apps/Ecommerce/Cart/BasicDetails', function (result) {
+                   parent.$('#basic-details').replaceWith(result);
+               });
+               setCountry();
+           });
+        
+    }
+    function setCountry() {
+        if ($("#ShippingCalculation").val() !== null && $("#ShippingCalculation").val() !== "") {
+            $.post('/Apps/Ecommerce/Cart/GetShippingCalculationCountry',
+                  { id: $("select[id='ShippingCalculation']").val() },
+                  function (country) {
+                      if (country !== false) {
+                          $("select[id='Country_Id']").val(country);
+                          $("select[id='Country_Id']").prop('disabled', 'disabled');
+                      } else {
+                          $("select[id='Country_Id']").prop('disabled', false);
+                      }
+                  });
+        } else {
+            $("select[id='Country_Id']").prop('disabled', false);
+        }
+    }
+
+    setCountry();
+});
---
+++ MrCMS.Web/Apps/Ecommerce/Models/TaxAwareShippingCost.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using MrCMS.Web.Apps.Ecommerce.Entities.Tax;
+using MrCMS.Web.Apps.Ecommerce.Settings;
+using MrCMS.Website;
+
+namespace MrCMS.Web.Apps.Ecommerce.Models
+{
+    public class TaxAwareShippingRate
+    {
+        public static TaxAwareShippingRate Create(decimal? value, TaxRate rate, bool? shippingRateIncludesTax = null)
+        {
+            return PriceCreator(value, rate, shippingRateIncludesTax);
+        }
+
+        public static decimal GetPriceExcludingTax(decimal value, TaxRate rate, bool? shippingRateIncludesTax = null)
+        {
+            return PriceCreator(value, rate, shippingRateIncludesTax).PriceExcludingTax.GetValueOrDefault();
+        }
+        public static decimal GetPriceIncludingTax(decimal value, TaxRate rate, bool? shippingRateIncludesTax = null)
+        {
+            return PriceCreator(value, rate, shippingRateIncludesTax).PriceIncludingTax.GetValueOrDefault();
+        }
+        public static decimal? GetPriceExcludingTax(decimal? value, TaxRate rate, bool? shippingRateIncludesTax = null)
+        {
+            return PriceCreator(value, rate, shippingRateIncludesTax).PriceExcludingTax;
+        }
+        public static decimal? GetPriceIncludingTax(decimal? value, TaxRate rate, bool? shippingRateIncludesTax = null)
+        {
+            return PriceCreator(value, rate, shippingRateIncludesTax).PriceIncludingTax;
+        }
+
+        public static Func<decimal?, TaxRate, bool?, TaxAwareShippingRate> PriceCreator =
+            (arg1, rate, arg3) => new TaxAwareShippingRate(arg1, rate, arg3);
+
+        private readonly decimal? _value;
+        private readonly TaxRate _taxRate;
+        private readonly bool? _shippingRateIncludesTax;
+
+        protected decimal TaxRatePercentage
+        {
+            get { return _taxRate != null ? _taxRate.Percentage : 0m; }
+        }
+
+        public TaxAwareShippingRate(decimal? value, TaxRate taxRate, bool? shippingRateIncludesTax = null)
+        {
+            _value = value;
+            _taxRate = taxRate;
+            _shippingRateIncludesTax = shippingRateIncludesTax;
+        }
+
+        public decimal? PriceExcludingTax
+        {
+            get
+            {
+                return !_value.HasValue ? (decimal?)null :
+                Math.Round(ShippingRateIncludesTax
+                                          ? _value.Value / ((TaxRatePercentage + 100) / 100)
+                                          : _value.Value, 2, MidpointRounding.AwayFromZero);
+            }
+        }
+
+        private bool ShippingRateIncludesTax
+        {
+            get
+            {
+                return _shippingRateIncludesTax ?? (!MrCMSApplication.Get<TaxSettings>().TaxesEnabled ||
+                       MrCMSApplication.Get<TaxSettings>().ShippingRateIncludesTax);
+            }
+        }
+
+        public decimal? PriceIncludingTax
+        {
+            get
+            {
+                return !_value.HasValue
+                           ? (decimal?)null
+                           : Math.Round(ShippingRateIncludesTax
+                                            ? _value.Value
+                                            : _taxRate != null
+                                                  ? _value.Value * (_taxRate.Multiplier)
+                                                  : _value.Value, 2, MidpointRounding.AwayFromZero);
+            }
+        }
+    }
+}
