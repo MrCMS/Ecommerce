43bd3e674762317e8649dfc4653c0c108432a6f9
gmstr <gary.m.strong@gmail.com>
2013-07-23 18:19:39 +0100

    Refactoring checkout

--- MrCMS.Web/Apps/Ecommerce/Content/Scripts/custom/shipping-methods.js
+++ MrCMS.Web/Apps/Ecommerce/Content/Scripts/custom/shipping-methods.js
@@ -1,41 +1,14 @@
 ﻿$(function () {
-    $(document).on('change', "select[id='ShippingCalculation']", function () {
-        updateShippingMethod();
+    $(document).on('change', "select#ShippingCalculation", function () {
+        $.post('/Apps/Ecommerce/SetDeliveryDetails/SetShipping',
+          { id: $(this).val() }, function () {
+              $.get('/Apps/Ecommerce/Checkout/Summary', function (result) {
+                  $('#basic-details').replaceWith(result);
+              });
+              $.get("Apps/Ecommerce/SetDeliveryDetails/DeliveryAddress", function (response) {
+                  $('#delivery-address-container').replaceWith(response);
+                  $.validator.unobtrusive.parse('form');
+              });
+          });
     });
-    $(document).on('click', "#continue", function () {
-        if ($("#ShippingCalculation").val() !== null && $("#ShippingCalculation").val() !== "") {
-            $("form#deliveryDetails").submit();
-        }
-        return false;
-    });
-    
-    function updateShippingMethod() {
-        $.post('/Apps/Ecommerce/Cart/AddShippingMethod',
-           { id: $("select[id='ShippingCalculation']").val() },
-           function (response) {
-               parent.$.get('/Apps/Ecommerce/Cart/BasicDetails', function (result) {
-                   parent.$('#basic-details').replaceWith(result);
-               });
-               setCountry();
-           });
-        
-    }
-    function setCountry() {
-        if ($("#ShippingCalculation").val() !== null && $("#ShippingCalculation").val() !== "") {
-            $.post('/Apps/Ecommerce/Cart/GetShippingCalculationCountry',
-                  { id: $("select[id='ShippingCalculation']").val() },
-                  function (country) {
-                      if (country !== false) {
-                          $("select[id='Country_Id']").val(country);
-                          $("select[id='Country_Id']").prop('disabled', 'disabled');
-                      } else {
-                          $("select[id='Country_Id']").prop('disabled', false);
-                      }
-                  });
-        } else {
-            $("select[id='Country_Id']").prop('disabled', false);
-        }
-    }
-
-    setCountry();
 });
--- MrCMS.Web/Apps/Ecommerce/Controllers/EnterOrderEmailController.cs
+++ MrCMS.Web/Apps/Ecommerce/Controllers/EnterOrderEmailController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using MrCMS.Services;
+using MrCMS.Web.Apps.Ecommerce.Models;
 using MrCMS.Web.Apps.Ecommerce.Services.Cart;
 using MrCMS.Website.Controllers;
 using MrCMS.Web.Apps.Ecommerce.Pages;
@@ -10,22 +13,58 @@ namespace MrCMS.Web.Apps.Ecommerce.Controllers
 {
     public class EnterOrderEmailController : MrCMSAppUIController<EcommerceApp>
     {
-        private readonly IGetCart _getCart;
+        private readonly CartModel _cart;
+        private readonly ICartManager _cartManager;
+        private readonly IAuthorisationService _authorisationService;
+        private readonly IUserService _userService;
 
-        public EnterOrderEmailController(IGetCart getCart)
+        public EnterOrderEmailController(CartModel cart, ICartManager cartManager, IAuthorisationService authorisationService, IUserService userService)
         {
-            _getCart = getCart;
+            _cart = cart;
+            _cartManager = cartManager;
+            _authorisationService = authorisationService;
+            _userService = userService;
         }
 
         public ActionResult Show(EnterOrderEmail page)
         {
-            if (_getCart.GetCart().Items.Count == 0)
+            if (!_cart.Items.Any())
                 return Redirect(UniquePageHelper.GetUrl<Cart>());
-            if (String.IsNullOrWhiteSpace(_getCart.GetOrderEmail()) && CurrentRequestData.CurrentUser != null)
-                _getCart.SetOrderEmail(CurrentRequestData.CurrentUser.Email);
-            if (!String.IsNullOrWhiteSpace(_getCart.GetOrderEmail()) && CurrentRequestData.CurrentUser != null)
-                return Redirect(UniquePageHelper.GetUrl<SetDeliveryDetails>());
+            _cart.OrderEmail = _cart.OrderEmail ?? (CurrentRequestData.CurrentUser != null
+                                                        ? CurrentRequestData.CurrentUser.Email
+                                                        : null);
+            ViewData["cart"] = _cart;
             return View(page);
+        }
+
+        [HttpPost]
+        public ActionResult SetOrderEmail(string orderEmail)
+        {
+            _cartManager.SetOrderEmail(orderEmail);
+            return Redirect(UniquePageHelper.GetUrl<SetDeliveryDetails>());
+        }
+
+        [HttpPost]
+        public ActionResult SetOrderEmailAndLogin(EmailAndLoginModel model)
+        {
+            if (model.HavePassword)
+            {
+                //TODO: replace with ILoginService from 0.3.1 core
+                var user = _userService.GetUserByEmail(model.OrderEmail);
+                if (user != null)
+                {
+                    if (_authorisationService.ValidateUser(user, model.Password))
+                    {
+                        _authorisationService.SetAuthCookie(user.Email, false);
+                        _cartManager.SetOrderEmail(model.OrderEmail);
+                        return Redirect(UniquePageHelper.GetUrl<SetDeliveryDetails>());
+                    }
+                }
+                TempData.ErrorMessages().Add("There was an error logging in with the provided email and password");
+                return Redirect(UniquePageHelper.GetUrl<EnterOrderEmail>());
+            }
+            _cartManager.SetOrderEmail(model.OrderEmail);
+            return Redirect(UniquePageHelper.GetUrl<SetDeliveryDetails>());
         }
     }
 }
