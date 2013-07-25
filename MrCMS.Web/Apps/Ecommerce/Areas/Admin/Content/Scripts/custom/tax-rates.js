9ddd362a85d0e5fb231d5212ddd37b7605c7fedb
as <adem.sabic@thought.co.uk>
2013-07-25 13:55:53 +0200

    Adding Shipping Rates Taxes Enabled to Tax Settings

--- MrCMS.Web/Apps/Ecommerce/Areas/Admin/Content/Scripts/custom/tax-rates.js
+++ MrCMS.Web/Apps/Ecommerce/Areas/Admin/Content/Scripts/custom/tax-rates.js
@@ -2,4 +2,7 @@
     $('#TaxesEnabled').click(function () {
         $('form#Settings').submit();
     });
+    $('#ShippingRateTaxesEnabled').click(function () {
+        $('form#Settings').submit();
+    });
 })
