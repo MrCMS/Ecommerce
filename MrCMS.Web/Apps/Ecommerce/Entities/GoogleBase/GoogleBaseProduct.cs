4c99891ed55abee0833774d0bd4f206cf159ca58
as <adem.sabic@thought.co.uk>
2013-07-26 19:02:46 +0200

    Google Base Initial Structure

---
+++ MrCMS.Web/Apps/Ecommerce/Entities/GoogleBase/GoogleBaseProduct.cs
@@ -0,0 +1,11 @@
+﻿using MrCMS.Entities;
+using MrCMS.Web.Apps.Ecommerce.Entities.Products;
+
+namespace MrCMS.Web.Apps.Ecommerce.Entities.GoogleBase
+{
+    public class GoogleBaseProduct : SiteEntity
+    {
+        public virtual ProductVariant ProductVariant { get; set; }
+        public virtual string Category { get; set; }
+    }
+}
---
+++ MrCMS.Web/Apps/Ecommerce/Services/GoogleBase/GoogleBaseTaxonomyService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MrCMS.Helpers;
+
+namespace MrCMS.Web.Apps.Ecommerce.Services.GoogleBase
+{
+    public class GoogleBaseTaxonomyService : IGoogleBaseTaxonomyService
+    {
+        private static List<string> _googleBaseTaxonomy;
+        private static IEnumerable<string> GoogleBaseTaxonomyItems
+        {
+            get
+            {
+                var rows = GoogleBaseTaxonomyData.RawData.Split(new[] { "\n","\r" }, StringSplitOptions.RemoveEmptyEntries);
+                return _googleBaseTaxonomy ?? (_googleBaseTaxonomy = rows.Select(s => s).ToList());
+            }
+        }
+
+        public List<SelectListItem> GetOptions()
+        {
+            return GoogleBaseTaxonomyItems.BuildSelectItemList(item => item, item => item,emptyItem: null);
+        }
+    }
+}
