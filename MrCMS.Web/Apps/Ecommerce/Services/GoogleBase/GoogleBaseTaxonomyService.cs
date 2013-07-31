cff6c2ecfeb0ec0065282312c41088b8af88f42d
gmstr <gary.m.strong@gmail.com>
2013-07-31 10:25:52 +0100

    Restructure Taxonomy

--- MrCMS.Web/Apps/Ecommerce/Services/GoogleBase/GoogleBaseTaxonomyService.cs
+++ MrCMS.Web/Apps/Ecommerce/Services/GoogleBase/GoogleBaseTaxonomyService.cs
@@ -8,19 +8,9 @@ namespace MrCMS.Web.Apps.Ecommerce.Services.GoogleBase
 {
     public class GoogleBaseTaxonomyService : IGoogleBaseTaxonomyService
     {
-        private static List<string> _googleBaseTaxonomy;
-        private static IEnumerable<string> GoogleBaseTaxonomyItems
-        {
-            get
-            {
-                var rows = GoogleBaseTaxonomyData.RawData.Split(new[] { "\n","\r" }, StringSplitOptions.RemoveEmptyEntries);
-                return _googleBaseTaxonomy ?? (_googleBaseTaxonomy = rows.Select(s => s).ToList());
-            }
-        }
-
         public List<SelectListItem> GetOptions()
         {
-            return GoogleBaseTaxonomyItems.BuildSelectItemList(item => item, item => item,emptyItem: null);
+            return GoogleBaseTaxonomyData.Rows.BuildSelectItemList(item => item, item => item, emptyItem: null);
         }
     }
 }
