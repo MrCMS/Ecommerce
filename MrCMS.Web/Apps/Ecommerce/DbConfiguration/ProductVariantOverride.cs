a6643120c95a08ac6e3e62dfde471f7b793e98a4
gmstr <gary.m.strong@gmail.com>
2013-07-31 09:58:57 +0100

    Restructure of the mapping for google base product

---
+++ MrCMS.Web/Apps/Ecommerce/DbConfiguration/ProductVariantOverride.cs
@@ -0,0 +1,17 @@
+﻿using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+using MrCMS.Web.Apps.Ecommerce.Entities.Products;
+
+namespace MrCMS.Web.Apps.Ecommerce.DbConfiguration
+{
+    public class ProductVariantOverride : IAutoMappingOverride<ProductVariant>
+    {
+        public void Override(AutoMapping<ProductVariant> mapping)
+        {
+            mapping.HasMany(variant => variant.AttributeValues).KeyColumn("ProductVariantId").Cascade.All();
+            mapping.HasOne(variant => variant.GoogleBaseProduct)
+                   .PropertyRef(product => product.ProductVariant)
+                   .Cascade.All();
+        }
+    }
+}
