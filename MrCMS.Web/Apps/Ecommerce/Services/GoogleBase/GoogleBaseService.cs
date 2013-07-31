dc741dc38a6f3c260727cbf69ad737e69e0ea240
as <adem.sabic@thought.co.uk>
2013-07-31 16:23:30 +0200

    Google Base Refactoring

---
+++ MrCMS.Web/Apps/Ecommerce/Services/GoogleBase/GoogleBaseService.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using MrCMS.Helpers;
+using MrCMS.Web.Apps.Ecommerce.Entities.GoogleBase;
+using NHibernate;
+
+namespace MrCMS.Web.Apps.Ecommerce.Services.GoogleBase
+{
+    public class GoogleBaseService : IGoogleBaseService
+    {
+        private readonly ISession _session;
+
+        public GoogleBaseService(ISession session)
+        {
+            _session = session;
+        }
+
+        public GoogleBaseProduct Get(int id)
+        {
+            return _session.QueryOver<GoogleBaseProduct>().Where(x=>x.Id==id).SingleOrDefault();
+        }
+
+        public void Add(GoogleBaseProduct item)
+        {
+            _session.Transact(session => session.Save(item));
+        }
+
+        public void Update(GoogleBaseProduct item)
+        {
+            _session.Transact(session => session.Update(item));
+        }
+
+        public List<SelectListItem> GetGoogleCategories()
+        {
+            return GoogleBaseTaxonomyData.Rows.BuildSelectItemList(item => item, item => item, emptyItem: null);
+        }
+    }
+}
---
+++ MrCMS.Web/Apps/Ecommerce/Services/Misc/OptionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using MrCMS.Helpers;
+using System.Linq;
+using MrCMS.Web.Apps.Ecommerce.Helpers;
+
+namespace MrCMS.Web.Apps.Ecommerce.Services.Misc
+{
+    public class OptionService : IOptionService
+    {
+        public List<SelectListItem> GetEnumOptions<T>() where T:struct
+        {
+            return
+                Enum.GetValues(typeof(T))
+                    .Cast<T>()
+                    .BuildSelectItemList(item => GeneralHelper.GetDescriptionFromEnum(item as Enum),item=>item.ToString(), emptyItem: null);
+        }
+    }
+}
