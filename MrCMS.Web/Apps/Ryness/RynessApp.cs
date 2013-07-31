52ff39707cf2af24d52e5b8a931a6a3fba6ff90b
Andrew <andrew.king@thought.co.uk>
2013-07-31 14:41:30 +0100

    Ryness Theme

---
+++ MrCMS.Web/Apps/Ryness/RynessApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MrCMS.Apps;
+
+namespace MrCMS.Web.Apps.Ryness
+{
+    public class RynessApp : MrCMSApp
+    {
+        protected override void RegisterApp(MrCMSAppRegistrationContext context)
+        {
+        }
+
+        public override string AppName
+        {
+            get { return "Ryness"; }
+        }
+
+        protected override void RegisterServices(Ninject.IKernel kernel)
+        {
+        }
+
+        protected override void OnInstallation(NHibernate.ISession session, Installation.InstallModel model, Entities.Multisite.Site site)
+        {
+        }
+    }
+}
---
+++ MrCMS.Web/Apps/Ryness/Widgets/RynessNavigation.cs
@@ -0,0 +1,19 @@
+﻿using MrCMS.Entities.Widget;
+using NHibernate;
+
+namespace MrCMS.Web.Apps.Ryness.Widgets
+{
+    public class RynessNavigation : Widget
+    {
+        public override bool HasProperties
+        {
+            get { return false; }
+        }
+
+        public override object GetModel(ISession session)
+        {
+            return null;
+        }
+
+    }
+}
