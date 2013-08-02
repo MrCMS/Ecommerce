ca61e18b2227857f7f91ad754e7c80516541eb9a
Andrew <andrew.king@thought.co.uk>
2013-08-02 08:14:03 +0100

    starting to add ryness menu

---
+++ MrCMS.Web/Apps/Ecommerce/DbConfiguration/FeaturedCategoryWidgetOverride.cs
@@ -0,0 +1,15 @@
+﻿using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+using MrCMS.DbConfiguration.Types;
+using MrCMS.Web.Apps.Ecommerce.Widgets;
+
+namespace MrCMS.Web.Apps.Ecommerce.DbConfiguration
+{
+    public class FeaturedCategoryWidgetOverride : IAutoMappingOverride<FeaturedCategories>
+    {
+        public void Override(AutoMapping<FeaturedCategories> mapping)
+        {
+            mapping.Map(x => x.ListOfFeaturedCategories).CustomType<VarcharMax>().Length(4001);
+        }
+    }
+}
