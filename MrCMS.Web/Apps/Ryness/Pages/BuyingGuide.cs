3bb780cb4d89053fd25ca4dc0664980d58bdf0ee
Andrew <andrew.king@thought.co.uk>
2013-08-02 12:58:04 +0100

    Category Container page restyle

---
+++ MrCMS.Web/Apps/Ryness/Pages/BuyingGuide.cs
@@ -0,0 +1,10 @@
+﻿using MrCMS.Web.Apps.Core.Pages;
+
+namespace MrCMS.Web.Apps.Ryness.Pages
+{
+    public class BuyingGuide : TextPage
+    {
+        // Section Header
+        // Section Text
+    }
+}
