c85f69b16f996764d64c1245048c60db75d15286
Andrew <andrew.king@thought.co.uk>
2013-08-02 15:38:22 +0100

    css fix buying guide start

---
+++ MrCMS.Web/Apps/Ryness/Entities/BuyingGuideInformation.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using MrCMS.Entities;
+
+namespace MrCMS.Web.Apps.Ryness.Entities
+{
+    public class BuyingGuideInformation : SiteEntity
+    {
+        [Required]
+        public virtual string Title { get; set; }
+        [Required]
+        [AllowHtml]
+        public virtual string Text { get; set; }
+    }
+}
---
+++ MrCMS.Web/Apps/Ryness/Metadata/BuyingGuideMetadata.cs
@@ -0,0 +1,13 @@
+﻿using MrCMS.Entities.Documents.Metadata;
+using MrCMS.Web.Apps.Ryness.Pages;
+
+namespace MrCMS.Web.Apps.Ryness.Metadata
+{
+    public class BuyingGuideMetadata : DocumentMetadataMap<BuyingGuide>
+    {
+        public override string DefaultLayoutName
+        {
+            get { return "Content Page Layout"; }
+        }
+    }
+}
