00e6ecbc38264f5797a9c52e7f6b05bc07e71a04
Andrew <andrew.king@thought.co.uk>
2013-08-02 16:57:35 +0100

    latest

--- MrCMS.Web/Themes/Ryness/Content/Scripts/checkout/checkout.js
+++ MrCMS.Web/Themes/Ryness/Content/Scripts/checkout/checkout.js
@@ -1,18 +1,17 @@
 ﻿$(document).ready(function () {
     $('.carousel').carousel();
+    //$('#rdoYes').attr('value', true).change(
+    //    function () {
+    //        if ($(this).is(':checked')) {
+    //            $("#passwordbox").removeClass();
+    //            $("#passwordbox").addClass("span10");
+    //        }
+    //    });
 
-    $('#rdoYes').attr('value', true).change(
-        function () {
-            if ($(this).is(':checked')) {
-                $("#passwordbox").removeClass();
-                $("#passwordbox").addClass("span10");
-            }
-        });
-
-    $('#rdoNo').change(
-    function () {
-        if ($(this).is(':checked')) {
-            $("#passwordbox").addClass("span11 hidden");
-        }
-    });
+    //$('#rdoNo').change(
+    //function () {
+    //    if ($(this).is(':checked')) {
+    //        $("#passwordbox").addClass("span11 hidden");
+    //    }
+    //});
 });
