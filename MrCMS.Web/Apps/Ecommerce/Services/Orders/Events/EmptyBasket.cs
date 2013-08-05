ad5f62933bab06562433ac8544f0837bf977b7e1
gmstr <gary.m.strong@gmail.com>
2013-08-05 12:18:58 +0100

    Adding in basics of Order Events

---
+++ MrCMS.Web/Apps/Ecommerce/Services/Orders/Events/EmptyBasket.cs
@@ -0,0 +1,21 @@
+﻿using MrCMS.Web.Apps.Ecommerce.Entities.Orders;
+using MrCMS.Web.Apps.Ecommerce.Services.Cart;
+
+namespace MrCMS.Web.Apps.Ecommerce.Services.Orders.Events
+{
+    public class EmptyBasket : IOnOrderPlaced
+    {
+        private readonly ICartManager _cartManager;
+
+        public EmptyBasket(ICartManager cartManager)
+        {
+            _cartManager = cartManager;
+        }
+
+        public int Order { get { return 10; } }
+        public void OnOrderPlaced(Order order)
+        {
+            _cartManager.EmptyBasket();
+        }
+    }
+}
---
+++ MrCMS.Web/Apps/Ecommerce/Services/Orders/Events/IOnOrderPartiallyRefunded.cs
@@ -0,0 +1,9 @@
+﻿using MrCMS.Web.Apps.Ecommerce.Entities.Orders;
+
+namespace MrCMS.Web.Apps.Ecommerce.Services.Orders.Events
+{
+    public interface IOnOrderPartiallyRefunded : IOrderEvent
+    {
+        void OnOrderPartiallyRefunded(Order order, OrderRefund refund);
+    }
+}
