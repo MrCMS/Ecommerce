16b1bb5162d88832d9490c0f46def1ac3ac6adf9
gmstr <gary.m.strong@gmail.com>
2013-08-05 12:18:58 +0100

    Adding in basics of Order Events

---
+++ MrCMS.Web/Apps/Ecommerce/Services/Orders/Events/IOnOrderFullyRefunded.cs
@@ -0,0 +1,9 @@
+﻿using MrCMS.Web.Apps.Ecommerce.Entities.Orders;
+
+namespace MrCMS.Web.Apps.Ecommerce.Services.Orders.Events
+{
+    public interface IOnOrderFullyRefunded : IOrderEvent
+    {
+        void OnOrderFullyRefunded(Order order, OrderRefund refund);
+    }
+}
