2d29e7f15455126f58022cd8dc48187c884a4698
gmstr <gary.m.strong@gmail.com>
2013-08-05 10:02:44 +0100

    Fixing issues with settings
    Fixing logic for taxes on product variants

--- MrCMS.Web/Apps/Ecommerce/Payment/PaymentSettings.cs
+++ MrCMS.Web/Apps/Ecommerce/Payment/PaymentSettings.cs
@@ -7,5 +7,10 @@ namespace MrCMS.Web.Apps.Ecommerce.Payment
     {
         [DisplayName("Cash on delivery enabled")]
         public bool CashOnDeliveryEnabled { get; set; }
+
+        public override bool RenderInSettings
+        {
+            get { return false; }
+        }
     }
 }
