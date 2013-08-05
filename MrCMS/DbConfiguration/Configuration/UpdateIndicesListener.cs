24ea2669796beef7dd9ed8438fbc71499a4d99c0
gmstr <gary.m.strong@gmail.com>
2013-08-05 11:35:05 +0100

    Adding default favicon to prevent errors being logged.
    Updating listeners

--- MrCMS/DbConfiguration/Configuration/UpdateIndicesListener.cs
+++ MrCMS/DbConfiguration/Configuration/UpdateIndicesListener.cs
@@ -5,24 +5,48 @@ using NHibernate.Event;
 
 namespace MrCMS.DbConfiguration.Configuration
 {
-    public class UpdateIndicesListener : IPostUpdateEventListener, IPostInsertEventListener, IPostDeleteEventListener
+    public class UpdateIndicesListener :
+        IPostUpdateEventListener,
+        IPostInsertEventListener,
+        IPostDeleteEventListener,
+        IPostCollectionUpdateEventListener,
+        IPostCollectionRemoveEventListener,
+        IPostCollectionRecreateEventListener
     {
         public void OnPostUpdate(PostUpdateEvent @event)
         {
             var siteEntity = @event.Entity as SiteEntity;
-            if (siteEntity != null) TaskExecutor.ExecuteLater(Create(typeof (UpdateIndicesTask<>), siteEntity));
+            if (siteEntity != null) TaskExecutor.ExecuteLater(Create(typeof(UpdateIndicesTask<>), siteEntity));
         }
 
         public void OnPostInsert(PostInsertEvent @event)
         {
             var siteEntity = @event.Entity as SiteEntity;
-            if (siteEntity != null) TaskExecutor.ExecuteLater(Create(typeof (InsertIndicesTask<>), siteEntity));
+            if (siteEntity != null) TaskExecutor.ExecuteLater(Create(typeof(InsertIndicesTask<>), siteEntity));
         }
 
         public void OnPostDelete(PostDeleteEvent @event)
         {
             var siteEntity = @event.Entity as SiteEntity;
-            if (siteEntity != null) TaskExecutor.ExecuteLater(Create(typeof (DeleteIndicesTask<>), siteEntity));
+            if (siteEntity != null) TaskExecutor.ExecuteLater(Create(typeof(DeleteIndicesTask<>), siteEntity));
+        }
+
+        public void OnPostUpdateCollection(PostCollectionUpdateEvent @event)
+        {
+            var siteEntity = @event.AffectedOwnerOrNull as SiteEntity;
+            if (siteEntity != null) TaskExecutor.ExecuteLater(Create(typeof(UpdateIndicesTask<>), siteEntity));
+        }
+
+        public void OnPostRemoveCollection(PostCollectionRemoveEvent @event)
+        {
+            var siteEntity = @event.AffectedOwnerOrNull as SiteEntity;
+            if (siteEntity != null) TaskExecutor.ExecuteLater(Create(typeof(UpdateIndicesTask<>), siteEntity));
+        }
+
+        public void OnPostRecreateCollection(PostCollectionRecreateEvent @event)
+        {
+            var siteEntity = @event.AffectedOwnerOrNull as SiteEntity;
+            if (siteEntity != null) TaskExecutor.ExecuteLater(Create(typeof(UpdateIndicesTask<>), siteEntity));
         }
 
         private BackgroundTask Create(Type type, SiteEntity siteEntity)
