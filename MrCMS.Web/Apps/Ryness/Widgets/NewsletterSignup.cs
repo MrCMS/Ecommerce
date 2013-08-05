9a1ad23cb2193a8c910b7c27b12c32c5a074b905
Andrew <andrew.king@thought.co.uk>
2013-08-05 14:01:53 +0100

    Added widget for newsletter + theme

---
+++ MrCMS.Web/Apps/Ryness/Widgets/NewsletterSignup.cs
@@ -0,0 +1,9 @@
+﻿using MrCMS.Entities.Widget;
+
+namespace MrCMS.Web.Apps.Ryness.Widgets
+{
+    public class NewsletterSignup : Widget
+    {
+        public virtual string EmailAddress { get; set; }
+    }
+}
