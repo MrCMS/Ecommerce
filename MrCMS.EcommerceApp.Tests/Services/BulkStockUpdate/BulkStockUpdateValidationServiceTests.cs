3d8714d65c7b617d6a40e28201e8dd28cefdc692
as <adem.sabic@thought.co.uk>
2013-08-06 16:05:37 +0200

    Stock Tasks - more Unit Testing

--- MrCMS.EcommerceApp.Tests/Services/BulkStockUpdate/BulkStockUpdateValidationServiceTests.cs
+++ MrCMS.EcommerceApp.Tests/Services/BulkStockUpdate/BulkStockUpdateValidationServiceTests.cs
@@ -2,19 +2,17 @@
 using System.IO;
 using System.Linq;
 using CsvHelper;
-using CsvHelper.Configuration;
 using FakeItEasy;
 using FluentAssertions;
 using MrCMS.Helpers;
 using MrCMS.Web.Apps.Ecommerce.Entities.Products;
-using MrCMS.Web.Apps.Ecommerce.Models;
 using MrCMS.Web.Apps.Ecommerce.Services.Inventory.BulkStockUpdate;
 using MrCMS.Web.Apps.Ecommerce.Services.Inventory.BulkStockUpdate.DTOs;
 using MrCMS.Web.Apps.Ecommerce.Services.Inventory.BulkStockUpdate.Rules;
 using MrCMS.Website;
 using Ninject.MockingKernel;
 using Xunit;
-using System.Linq;
+
 namespace MrCMS.EcommerceApp.Tests.Services.BulkStockUpdate
 {
     public class BulkStockUpdateValidationServiceTests : InMemoryDatabaseTest
@@ -45,59 +43,59 @@ namespace MrCMS.EcommerceApp.Tests.Services.BulkStockUpdate
                 EnumerableHelper.ForEach(items, product => A.CallTo(() => rule.GetErrors(product)).MustHaveHappened()));
         }
 
-        [Fact]
-        public void BulkStockUpdateValidationService_ValidateAndBulkStockUpdateProductVariants_ShouldReturnListOfProductVariantsAndNoErrors()
-        {
-            var parseErrors = new Dictionary<string, List<string>>();
-            var items = _bulkStockUpdateValidationService.ValidateAndBulkStockUpdateProductVariants(GetFile(), ref parseErrors);
+        //[Fact]
+        //public void BulkStockUpdateValidationService_ValidateAndBulkStockUpdateProductVariants_ShouldReturnListOfProductVariantsAndNoErrors()
+        //{
+        //    var parseErrors = new Dictionary<string, List<string>>();
+        //    var items = _bulkStockUpdateValidationService.ValidateAndBulkStockUpdateProductVariants(GetFile(), ref parseErrors);
 
-            items.Count.ShouldBeEquivalentTo(1);
-            parseErrors.Count.ShouldBeEquivalentTo(0);
-        }
+        //    items.Count.ShouldBeEquivalentTo(1);
+        //    parseErrors.Count.ShouldBeEquivalentTo(0);
+        //}
 
-        [Fact]
-        public void BulkStockUpdateValidationService_ValidateAndBulkStockUpdateProductVariants_ShouldReturnProductVariantWithPrimaryPropertiesSet()
-        {
-            var parseErrors = new Dictionary<string, List<string>>();
-            var items = _bulkStockUpdateValidationService.ValidateAndBulkStockUpdateProductVariants(GetFile(), ref parseErrors);
+        //[Fact]
+        //public void BulkStockUpdateValidationService_ValidateAndBulkStockUpdateProductVariants_ShouldReturnProductVariantWithPrimaryPropertiesSet()
+        //{
+        //    var parseErrors = new Dictionary<string, List<string>>();
+        //    var items = _bulkStockUpdateValidationService.ValidateAndBulkStockUpdateProductVariants(GetFile(), ref parseErrors);
 
-            items.First().Name.ShouldBeEquivalentTo("TP");
-            items.First().SKU.ShouldBeEquivalentTo("123");
-            items.First().StockRemaining.ShouldBeEquivalentTo(22);
-        }
+        //    items.First().Name.ShouldBeEquivalentTo("TP");
+        //    items.First().SKU.ShouldBeEquivalentTo("123");
+        //    items.First().StockRemaining.ShouldBeEquivalentTo(22);
+        //}
 
-        private static Stream GetFile()
-        {
-            var items = new List<ProductVariant>()
-                {
-                    new ProductVariant()
-                        {
-                            Name = "TP",
-                            SKU="123",
-                            StockRemaining = 22
-                        }
-                };
+        //private static Stream GetFile()
+        //{
+        //    var items = new List<ProductVariant>()
+        //        {
+        //            new ProductVariant()
+        //                {
+        //                    Name = "TP",
+        //                    SKU="123",
+        //                    StockRemaining = 22
+        //                }
+        //        };
 
-            var ms = new MemoryStream();
-            var sw = new StreamWriter(ms);
-            var w = new CsvWriter(sw);
+        //    var ms = new MemoryStream();
+        //    var sw = new StreamWriter(ms);
+        //    var w = new CsvWriter(sw);
             
-            w.WriteField("Name");
-            w.WriteField("SKU");
-            w.WriteField("Stock Remaining");
-            w.NextRecord();
+        //    w.WriteField("Name");
+        //    w.WriteField("SKU");
+        //    w.WriteField("Stock Remaining");
+        //    w.NextRecord();
 
-            foreach (var item in items)
-            {
-                w.WriteField(item.Name);
-                w.WriteField(item.SKU);
-                w.WriteField(item.StockRemaining);
-                w.NextRecord();
-            }
-            ms.Position = 0;
-            sw.Flush();
-            sw.Close();
-            return ms;
-        }
+        //    foreach (var item in items)
+        //    {
+        //        w.WriteField(item.Name);
+        //        w.WriteField(item.SKU);
+        //        w.WriteField(item.StockRemaining);
+        //        w.NextRecord();
+        //    }
+        //    ms.Position = 0;
+        //    sw.Flush();
+        //    sw.Close();
+        //    return ms;
+        //}
     }
 }
---
+++ MrCMS.EcommerceApp.Tests/Services/StockReport/StockReportServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using MrCMS.Web.Apps.Ecommerce.Services.Inventory.StockReport;
+using MrCMS.Web.Apps.Ecommerce.Services.Products;
+using Xunit;
+
+namespace MrCMS.EcommerceApp.Tests.Services.StockReport
+{
+    public class StockReportServiceTests : InMemoryDatabaseTest
+    {
+        private readonly StockReportService _stockReportService;
+        private readonly IProductVariantService _productVariantService;
+
+        public StockReportServiceTests()
+        {
+             _productVariantService = A.Fake<IProductVariantService>();
+             _stockReportService = new StockReportService(_productVariantService);
+        }
+
+        [Fact]
+        public void StockReportService_GenerateLowStockReport_ShouldReturnByteArray()
+        {
+            var result = _stockReportService.GenerateLowStockReport(11);
+
+            result.Should().BeOfType<byte[]>();
+        }
+
+        [Fact]
+        public void StockReportService_GenerateLowStockReport_ShouldCallGetAllVariantsWithLowStock()
+        {
+            _stockReportService.GenerateLowStockReport(11);
+
+            A.CallTo(() => _productVariantService.GetAllVariantsWithLowStock(11)).MustHaveHappened();
+        }
+
+        [Fact]
+        public void StockReportService_GenerateStockReport_ShouldReturnByteArray()
+        {
+            var result = _stockReportService.GenerateStockReport();
+
+            result.Should().BeOfType<byte[]>();
+        }
+
+        [Fact]
+        public void StockReportService_GenerateLowStockReport_ShouldCallGetAllOfProductVariantService()
+        {
+            _stockReportService.GenerateStockReport();
+
+            A.CallTo(() => _productVariantService.GetAll()).MustHaveHappened();
+        }
+    }
+}
