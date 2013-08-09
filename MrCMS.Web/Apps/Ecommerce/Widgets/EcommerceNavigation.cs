8a5364aa158044e4f04bd15a98fcd0cf5e3e6f62
as <adem.sabic@thought.co.uk>
2013-08-09 18:56:01 +0200

    Category Only Navigation Widget CSS according to Ryness theme

--- MrCMS.Web/Apps/Ecommerce/Widgets/EcommerceNavigation.cs
+++ MrCMS.Web/Apps/Ecommerce/Widgets/EcommerceNavigation.cs
@@ -36,49 +36,10 @@ namespace MrCMS.Web.Apps.Ecommerce.Widgets
                        .Select(webpage => new NavigationRecord
                        {
                            Text = MvcHtmlString.Create(webpage.Name),
-                           Url = MvcHtmlString.Create("/" + webpage.LiveUrlSegment),
-                           Children = GetCategories(webpage, session)
+                           Url = MvcHtmlString.Create("/" + webpage.LiveUrlSegment)
                        }).ToList();
 
             return new NavigationList(navigationRecords.ToList());
-        }
-
-        public virtual List<NavigationRecord> GetCategories(Webpage entity, ISession session)
-        {
-            List<NavigationRecord> navigation = new List<NavigationRecord>();
-            if(entity.DocumentType.Contains("ProductSearch"))
-            {
-                IList<Category> categories = session.QueryOver<Category>().Cacheable().List();
-                foreach (var item in categories.Where(x=>x.Parent!=null && x.Parent.Parent==null))
-                {
-                    navigation.Add(new NavigationRecord
-                       {
-                           Text = MvcHtmlString.Create(item.Name),
-                           Url = MvcHtmlString.Create("/" + item.LiveUrlSegment),
-                           Children = GetChildCategories(item,session)
-                       });
-                }
-            }
-            
-            return navigation;
-        }
-
-        public virtual List<NavigationRecord> GetChildCategories(Document entity, ISession session)
-        {
-            List<NavigationRecord> navigation = new List<NavigationRecord>();
-            if (entity.Children.Any())
-            {
-                foreach (var item in entity.Children)
-                {
-                    navigation.Add(new NavigationRecord
-                        {
-                            Text = MvcHtmlString.Create(item.Name),
-                            Url = MvcHtmlString.Create("/" + item.UrlSegment),
-                            Children = GetChildCategories(item, session)
-                        });
-                }
-            }
-            return navigation;
         }
     }
 }
