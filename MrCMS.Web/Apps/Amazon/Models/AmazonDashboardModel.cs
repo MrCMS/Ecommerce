f07dacc0f19c083aa503eb34c4a1c0ed97a869d6
as <adem.sabic@thought.co.uk>
2013-09-03 14:52:25 +0200

    Amazon - Listings, Logs etc.

---
+++ MrCMS.Web/Apps/Amazon/Models/AmazonDashboardModel.cs
@@ -0,0 +1,13 @@
+﻿using MrCMS.Paging;
+using MrCMS.Web.Apps.Amazon.Entities.Logs;
+
+namespace MrCMS.Web.Apps.Amazon.Models
+{
+    public class AmazonDashboardModel
+    {
+        public IPagedList<AmazonLog> Logs { get; set; }
+        public int NoOfOrdersInLast7Days { get; set; }
+        public int NoOfPublishedListings { get; set; }
+        public int NoOfApiCallsToday { get; set; }
+    }
+}
