e2407e952136be9fd8eb2b5559fb51d13b907c95
as <adem.sabic@thought.co.uk>
2013-09-03 12:07:19 +0200

    Amazon App : Structure

---
+++ MrCMS.Web/Apps/Amazon/Areas/Admin/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using MrCMS.Website.Controllers;
+using System.Web.Mvc;
+
+namespace MrCMS.Web.Apps.Amazon.Areas.Admin.Controllers
+{
+    public class ApiController : MrCMSAppAdminController<AmazonApp>
+    {
+        [HttpGet]
+        public ViewResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ MrCMS.Web/Apps/Amazon/Models/AmazonAdminMenuModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using MrCMS.Models;
+
+namespace MrCMS.Web.Apps.Amazon.Models
+{
+    public class AmazonAdminMenuModel : IAdminMenuItem
+    {
+        private IDictionary<string, List<IMenuItem>> _children;
+        public string Text { get { return "Amazon"; } }
+        public string Url { get; private set; }
+        public bool CanShow { get { return true; } }
+        public IDictionary<string, List<IMenuItem>> Children
+        {
+            get
+            {
+                return _children ?? 
+                    (_children = new Dictionary<string, List<IMenuItem>>
+                    {
+                        {
+                            "Admin",
+                            new List<IMenuItem>
+                            {
+                                new ChildMenuItem("Dashboard", "/Admin/Apps/eBay/Amazon/Dashboard"),
+                                new ChildMenuItem("Listings", "/Admin/Apps/Amazon/Listing"),
+                                new ChildMenuItem("Orders", "/Admin/Apps/Amazon/Orders"),
+                            }
+                        },
+                        {
+                            "Settings",
+                            new List<IMenuItem>
+                            {
+                                new ChildMenuItem("Api", "/Admin/Apps/Amazon/Settings/Api")
+                            }
+                        },
+                        {
+                            "Auditing",
+                            new List<IMenuItem>
+                            {
+                                new ChildMenuItem("Logs", "/Admin/Apps/Amazon/Log"),
+                            }
+                        }
+                    });
+            }
+        }
+        public int DisplayOrder
+        {
+            get { return 51; }
+        }
+    }
+}
