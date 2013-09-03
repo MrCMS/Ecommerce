55909bbe1c1008d7c50c9f1f3dc8e4aeebb9a52e
as <adem.sabic@thought.co.uk>
2013-09-03 12:07:19 +0200

    Amazon App : Structure

---
+++ MrCMS.Web/Apps/Amazon/AmazonApp.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using MrCMS.Apps;
+using MrCMS.Entities.Multisite;
+using MrCMS.Installation;
+using MrCMS.Web.Areas.Admin.Controllers;
+using NHibernate;
+using Ninject;
+
+namespace MrCMS.Web.Apps.Amazon
+{
+    public class AmazonApp : MrCMSApp
+    {
+        public override string AppName
+        {
+            get { return "Amazon"; }
+        }
+
+        protected override void RegisterServices(IKernel kernel)
+        {
+
+        }
+
+        protected override void RegisterApp(MrCMSAppRegistrationContext context)
+        {
+            context.MapAreaRoute("Amazon Admin controllers", "Admin", "Admin/Apps/Amazon/{controller}/{action}/{id}",
+                                 new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                                 new[] { typeof(SettingsController).Namespace });
+        }
+
+        protected override void OnInstallation(ISession session, InstallModel model, Site site)
+        {
+            
+        }
+    }
+}
