ea45dbcf1b505b79573dc60cdbd597373299cecb
thought <will.anderson@thought.co.uk>
2013-09-04 13:06:01 +0100

    Fixed up order admin views
    Fixed issue with variant name not showing in basket

--- MrCMS.Web/Apps/Ecommerce/Entities/Products/ProductVariant.cs
+++ MrCMS.Web/Apps/Ecommerce/Entities/Products/ProductVariant.cs
@@ -181,7 +181,7 @@ namespace MrCMS.Web.Apps.Ecommerce.Entities.Products
 
         public virtual string DisplayName
         {
-            get { return Name ?? Product.Name; }
+            get { return !string.IsNullOrWhiteSpace(Name) ? Name : Product.Name; }
         }
         public virtual string SelectOptionName
         {
