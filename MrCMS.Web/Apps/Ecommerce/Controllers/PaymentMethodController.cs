80ed3d7acb12aa615acbd5a64c77ac7791cd387b
as <adem.sabic@thought.co.uk>
2013-09-06 16:10:49 +0200

    removing commented code from testing

--- MrCMS.Web/Apps/Ecommerce/Controllers/PaymentMethodController.cs
+++ MrCMS.Web/Apps/Ecommerce/Controllers/PaymentMethodController.cs
@@ -14,18 +14,18 @@ namespace MrCMS.Web.Apps.Ecommerce.Controllers
     {
         private readonly CartModel _cartModel;
         private readonly IOrderService _orderService;
-        //private readonly IPayPalExpressService _payPalExpressService;
-        //private readonly IPaypointPaymentService _paypointPaymentService;
+        private readonly IPayPalExpressService _payPalExpressService;
+        private readonly IPaypointPaymentService _paypointPaymentService;
         private readonly IDocumentService _documentService;
 
         public PaymentMethodController(CartModel cartModel, IOrderService orderService,
-            //IPayPalExpressService payPalExpressService, IPaypointPaymentService paypointPaymentService, 
+            IPayPalExpressService payPalExpressService, IPaypointPaymentService paypointPaymentService,
             IDocumentService documentService)
         {
             _cartModel = cartModel;
             _orderService = orderService;
-            //_payPalExpressService = payPalExpressService;
-            //_paypointPaymentService = paypointPaymentService;
+            _payPalExpressService = payPalExpressService;
+            _paypointPaymentService = paypointPaymentService;
             _documentService = documentService;
         }
 
@@ -49,51 +49,51 @@ namespace MrCMS.Web.Apps.Ecommerce.Controllers
             return PartialView();
         }
 
-        //[HttpPost]
-        //[ActionName("PayPalExpressCheckout")]
-        //public RedirectResult PayPalExpressCheckout_POST()
-        //{
-        //    var response = _payPalExpressService.DoExpressCheckout(_cartModel);
-        //    var order = _orderService.PlaceOrder(_cartModel, response.UpdateOrder);
-        //    return Redirect(UniquePageHelper.GetUrl<OrderPlaced>(new { id = order.Guid }));
-        //}
+        [HttpPost]
+        [ActionName("PayPalExpressCheckout")]
+        public RedirectResult PayPalExpressCheckout_POST()
+        {
+            var response = _payPalExpressService.DoExpressCheckout(_cartModel);
+            var order = _orderService.PlaceOrder(_cartModel, response.UpdateOrder);
+            return Redirect(UniquePageHelper.GetUrl<OrderPlaced>(new { id = order.Guid }));
+        }
 
-        //[HttpGet]
-        //public PartialViewResult Paypoint()
-        //{
-        //    ViewData["months"] = _paypointPaymentService.Months();
-        //    ViewData["start-years"] = _paypointPaymentService.StartYears();
-        //    ViewData["expiry-years"] = _paypointPaymentService.ExpiryYears();
-        //    ViewData["card-types"] = _paypointPaymentService.GetCardTypes();
-        //    return PartialView(TempData["paypoint-model"]);
-        //}
+        [HttpGet]
+        public PartialViewResult Paypoint()
+        {
+            ViewData["months"] = _paypointPaymentService.Months();
+            ViewData["start-years"] = _paypointPaymentService.StartYears();
+            ViewData["expiry-years"] = _paypointPaymentService.ExpiryYears();
+            ViewData["card-types"] = _paypointPaymentService.GetCardTypes();
+            return PartialView(TempData["paypoint-model"]);
+        }
 
-        //[HttpPost]
-        //[ActionName("Paypoint")]
-        //public ActionResult Paypoint_POST(PaypointPaymentDetailsModel model)
-        //{
-        //    var response = _paypointPaymentService.ProcessDetails(model,
-        //                                                          Url.Action("Response3DSecure", "Paypoint", null,
-        //                                                                     Request.Url.Scheme));
-        //    if (response.Requires3DSecure)
-        //    {
-        //        TempData["redirect-details"] = response.RedirectDetails;
-        //        return RedirectToAction("Redirect3DSecure", "Paypoint");
-        //    }
+        [HttpPost]
+        [ActionName("Paypoint")]
+        public ActionResult Paypoint_POST(PaypointPaymentDetailsModel model)
+        {
+            var response = _paypointPaymentService.ProcessDetails(model,
+                                                                  Url.Action("Response3DSecure", "Paypoint", null,
+                                                                             Request.Url.Scheme));
+            if (response.Requires3DSecure)
+            {
+                TempData["redirect-details"] = response.RedirectDetails;
+                return RedirectToAction("Redirect3DSecure", "Paypoint");
+            }
 
-        //    if (response.PaymentSucceeded)
-        //    {
-        //        var order = _orderService.PlaceOrder(_cartModel, o =>
-        //                                                             {
-        //                                                                 o.PaymentStatus = PaymentStatus.Paid;
-        //                                                                 o.AuthorisationToken = response.PaypointPaymentDetails.AuthCode;
-        //                                                             });
-        //        return _documentService.RedirectTo<OrderPlaced>(new { id = order.Guid });
-        //    }
+            if (response.PaymentSucceeded)
+            {
+                var order = _orderService.PlaceOrder(_cartModel, o =>
+                                                                     {
+                                                                         o.PaymentStatus = PaymentStatus.Paid;
+                                                                         o.AuthorisationToken = response.PaypointPaymentDetails.AuthCode;
+                                                                     });
+                return _documentService.RedirectTo<OrderPlaced>(new { id = order.Guid });
+            }
 
-        //    TempData["error-details"] = response.FailureDetails;
-        //    TempData["paypoint-model"] = model;
-        //    return _documentService.RedirectTo<PaymentDetails>();
-        //}
+            TempData["error-details"] = response.FailureDetails;
+            TempData["paypoint-model"] = model;
+            return _documentService.RedirectTo<PaymentDetails>();
+        }
     }
 }
