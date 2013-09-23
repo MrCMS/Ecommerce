0f944413b3b4a579caeb7e5d85db0e492b0e4cde
as <adem.sabic@thought.co.uk>
2013-09-23 18:24:28 +0200

    AmazonApp > OnInstallation

--- MrCMS.Web/Apps/Amazon/AmazonApp.cs
+++ MrCMS.Web/Apps/Amazon/AmazonApp.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using MarketplaceWebServiceFeedsClasses;
 using MrCMS.Apps;
 using MrCMS.Entities.Multisite;
 using MrCMS.Installation;
+using MrCMS.Settings;
 using MrCMS.Web.Apps.Amazon.DbConfiguration;
+using MrCMS.Web.Apps.Amazon.Settings;
 using MrCMS.Web.Areas.Admin.Controllers;
+using MrCMS.Website;
 using NHibernate;
 using Ninject;
 
@@ -33,7 +37,29 @@ namespace MrCMS.Web.Apps.Amazon
 
         protected override void OnInstallation(ISession session, InstallModel model, Site site)
         {
-            
+            var configurationProvider = new ConfigurationProvider(new SettingService(session), site);
+
+            var amazonAppSettings = configurationProvider.GetSiteSettings<AmazonAppSettings>();
+            amazonAppSettings.FeedsApiVersion = "2009-01-01";
+            amazonAppSettings.OrdersApiVersion = "2011-01-01";
+            amazonAppSettings.ProductsApiVersion = "2011-10-01";
+            amazonAppSettings.SignatureVersion = "2";
+            amazonAppSettings.SignatureMethod = "HmacSHA256";
+            amazonAppSettings.AppName = "MrCMS";
+            amazonAppSettings.AppPath = "/";
+            amazonAppSettings.AppVersion = MrCMSApplication.AssemblyVersion;
+            amazonAppSettings.AppLanguage = "C#";
+            amazonAppSettings.ApiEndpoint = "https://mws.amazonservices.com/";
+            amazonAppSettings.AmazonOrderDetailsUrl = "https://sellercentral.amazon.com/gp/orders-v2/details/?orderID=";
+            amazonAppSettings.AmazonManageOrdersUrl = "https://sellercentral.amazon.com/gp/orders-v2/list";
+            amazonAppSettings.AmazonManageInventoryUrl = "https://sellercentral.amazon.com/myi/search/ProductSummary";
+
+            configurationProvider.SaveSettings(amazonAppSettings);
+
+            var amazonSellerSettings = configurationProvider.GetSiteSettings<AmazonSellerSettings>();
+            amazonSellerSettings.BarcodeIsOfType = StandardProductIDType.EAN;
+
+            configurationProvider.SaveSettings(amazonSellerSettings);
         }
 
         public override IEnumerable<System.Type> Conventions
