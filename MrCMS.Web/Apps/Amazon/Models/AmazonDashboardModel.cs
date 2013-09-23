c4a208bf4886812906cedc312bb8f81307b35a01
as <adem.sabic@thought.co.uk>
2013-09-23 18:12:04 +0200

    Dashboard

--- MrCMS.Web/Apps/Amazon/Models/AmazonDashboardModel.cs
+++ MrCMS.Web/Apps/Amazon/Models/AmazonDashboardModel.cs
@@ -9,16 +9,16 @@ namespace MrCMS.Web.Apps.Amazon.Models
     {
         public AmazonDashboardModel()
         {
-            FilterUntil = CurrentRequestData.Now;
-            FilterFrom = CurrentRequestData.Now.AddDays(-7);
+            FilterUntil = CurrentRequestData.Now.Date;
+            FilterFrom = CurrentRequestData.Now.Date.AddDays(-7);
         }
 
         public IPagedList<AmazonLog> Logs { get; set; }
         public int NoOfOrders { get; set; }
         public int NoOfUnshippedOrders { get; set; }
         public double AverageOrderAmount { get; set; }
-        public double NoOfOrderedProducts { get; set; }
-        public double NoOfShippedProducts { get; set; }
+        public decimal NoOfOrderedProducts { get; set; }
+        public decimal NoOfShippedProducts { get; set; }
         public int NoOfActiveListings { get; set; }
         public int NoOfApiCalls { get; set; }
         public bool AppSettingsStatus { get; set; }
--- MrCMS.Web/Apps/Amazon/Services/Analytics/IAmazonAnalyticsService.cs
+++ MrCMS.Web/Apps/Amazon/Services/Analytics/IAmazonAnalyticsService.cs
@@ -6,12 +6,8 @@ namespace MrCMS.Web.Apps.Amazon.Services.Analytics
     public interface IAmazonAnalyticsService
     {
         void TrackNewApiCall(AmazonApiSection? apiSection, string apiOperation);
-        int GetNumberOfOrders(DateTime from, DateTime to);
-        double GetAverageOrderAmount(DateTime from, DateTime to);
-        int GetNumberUnshippedOrders(DateTime from, DateTime to);
-        int GetNumberOfActiveListings();
-        int GetNumberOfApiCalls(DateTime from, DateTime to);
-        double GetNumberOfOrderedProducts(DateTime from, DateTime to);
-        double GetNumberOfShippedProducts(DateTime from, DateTime to);
+        AmazonDashboardModel GetAmazonDashboardModel(DateTime? from, DateTime? to);
+        object GetRevenue(DateTime from, DateTime to);
+        object GetProductsSold(DateTime from, DateTime to);
     }
 }
