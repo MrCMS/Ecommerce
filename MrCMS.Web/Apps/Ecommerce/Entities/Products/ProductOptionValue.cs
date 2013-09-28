71d63e7be1c86d21a55b45e215b54936c809909e
thought <will.anderson@thought.co.uk>
2013-09-28 13:33:10 +0100

    fixing product options

--- MrCMS.Web/Apps/Ecommerce/Entities/Products/ProductOptionValue.cs
+++ MrCMS.Web/Apps/Ecommerce/Entities/Products/ProductOptionValue.cs
@@ -8,7 +8,7 @@ namespace MrCMS.Web.Apps.Ecommerce.Entities.Products
         public virtual string Value { get; set; }
         public virtual ProductVariant ProductVariant { get; set; }
 
-        public virtual int DisplayOrder { get { return ProductOption != null ? ProductVariant.Product.Options.IndexOf(ProductOption) : int.MaxValue; } }
+        public virtual int DisplayOrder { get { return ProductOption != null && ProductVariant != null ? ProductVariant.Product.Options.IndexOf(ProductOption) : int.MaxValue; } }
         public virtual string FormattedValue
         {
             get { return ProductOption != null ? string.Format("{0} - {1}", ProductOption.Name, Value) : Value; }
