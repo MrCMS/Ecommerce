dac02f1d258d25260c7c22da61239805d977b8f6
Andrew <andrew.king@thought.co.uk>
2013-09-30 17:09:22 +0100

    Vertical featured product widget for buying guide pages

---
+++ MrCMS.Web/Apps/Ryness/Pages/Store.cs
@@ -0,0 +1,15 @@
+﻿using MrCMS.Entities.Documents.Web;
+
+namespace MrCMS.Web.Apps.Ryness.Pages
+{
+    public class Store : Webpage
+    {
+        //address
+        //contacts
+        //manager
+        //opening times
+        //map
+
+        //textpage?
+    }
+}
