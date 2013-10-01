252e1c9953ddda149af6174664e10445ce09c637
thought <will.anderson@thought.co.uk>
2013-10-01 15:41:56 +0100

    Add kerridge settings

---
+++ MrCMS.Web/Apps/Ryness/Settings/KerridgeSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MrCMS.Settings;
+
+namespace MrCMS.Web.Apps.Ryness.Settings
+{
+    public class KerridgeSettings : SiteSettingsBase
+    {
+        public bool Enabled { get; set; }
+        public string KerridgeAccountNumber { get; set; }
+        public string KerridgeBranch { get; set; }
+        public string WebServiceUrl { get; set; }
+        public string Id { get; set; }
+        public string Password { get; set; }
+    }
+}
