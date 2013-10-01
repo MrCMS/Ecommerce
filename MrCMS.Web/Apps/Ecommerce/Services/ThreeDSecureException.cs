d1ed5f54ec0ff57db89f04b263fae264be5ef3e5
thought <will.anderson@thought.co.uk>
2013-10-01 08:56:01 +0100

    add serializable on 3dsecure obj

--- MrCMS.Web/Apps/Ecommerce/Services/ThreeDSecureException.cs
+++ MrCMS.Web/Apps/Ecommerce/Services/ThreeDSecureException.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 namespace MrCMS.Web.Apps.Ecommerce.Services
 {
+    [Serializable]
     public class ThreeDSecureException : Exception
     {
         public ThreeDSecureException()
@@ -13,7 +14,7 @@ namespace MrCMS.Web.Apps.Ecommerce.Services
         public ThreeDSecureException(NameValueCollection error)
             : base("3D Secure error")
         {
-            string.Join("&", error.AllKeys.Select(s => string.Format("{0}={1}", s, error[(string) s])));
+            RequestData = string.Join("&", error.AllKeys.Select(s => string.Format("{0}={1}", s, error[(string) s])));
         }
 
         public string RequestData { get; set; }
