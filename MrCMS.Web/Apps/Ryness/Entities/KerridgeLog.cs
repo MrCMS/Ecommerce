d2247ded2f7399c55b28e029547956c65902dff8
thought <will.anderson@thought.co.uk>
2013-10-01 14:36:26 +0100

    Added in Ryness kerridge integration

---
+++ MrCMS.Web/Apps/Ryness/Entities/KerridgeLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MrCMS.Entities;
+using MrCMS.Web.Apps.Ecommerce.Entities.Orders;
+
+namespace MrCMS.Web.Apps.Ryness.Entities
+{
+    public class KerridgeLog : SiteEntity
+    {
+        public virtual Order Order { get; set; }
+        public virtual bool Sent { get; set; }
+        public virtual string Message { get; set; }
+    }
+}
