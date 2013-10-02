7b70456523fc78261a9df0337e84f292ba53dd8a
as <adem.sabic@thought.co.uk>
2013-10-02 15:30:12 +0200

    more unit testing

---
+++ MrCMS.AmazonApp.Tests/Services/Orders/AmazonOrderSearchServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using FakeItEasy;
+using MrCMS.EcommerceApp.Tests;
+using MrCMS.Indexing.Querying;
+using MrCMS.Web.Apps.Amazon.Entities.Orders;
+using MrCMS.Web.Apps.Amazon.Indexing;
+using MrCMS.Web.Apps.Amazon.Services.Orders;
+
+namespace MrCMS.AmazonApp.Tests.Services.Orders
+{
+    public class AmazonOrderSearchServiceTests : InMemoryDatabaseTest
+    {
+        private ISearcher<AmazonOrder, AmazonOrderSearchIndex> _orderSearcher;
+        private AmazonOrderSearchService _amazonOrderSearchService;
+
+        public AmazonOrderSearchServiceTests()
+        {
+            _orderSearcher = A.Fake<ISearcher<AmazonOrder, AmazonOrderSearchIndex>>();
+            _amazonOrderSearchService = new AmazonOrderSearchService(_orderSearcher);
+        }
+
+    }
+}
---
+++ MrCMS.AmazonApp.Tests/Services/Orders/AmazonOrderServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using FakeItEasy;
+using MrCMS.EcommerceApp.Tests;
+using MrCMS.Web.Apps.Amazon.Services.Logs;
+using MrCMS.Web.Apps.Amazon.Services.Orders;
+
+namespace MrCMS.AmazonApp.Tests.Services.Orders
+{
+    public class AmazonOrderServiceTests : InMemoryDatabaseTest
+    {
+        private IAmazonLogService _amazonLogService;
+        private AmazonOrderService _amazonOrderService;
+
+        public AmazonOrderServiceTests()
+        {
+            _amazonLogService = A.Fake<IAmazonLogService>();
+            _amazonOrderService = new AmazonOrderService(Session,_amazonLogService);
+        }
+
+    }
+}
