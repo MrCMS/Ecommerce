00b572ea8d71b71836b0aaf31f5e0020fa6e96a7
as <adem.sabic@thought.co.uk>
2013-10-03 11:43:47 +0200

    fixing orders

---
+++ MrCMS.Web/Apps/Amazon/Tasks/SyncListings.cs
@@ -0,0 +1,19 @@
+﻿using MrCMS.Entities.Multisite;
+using MrCMS.Tasks;
+
+namespace MrCMS.Web.Apps.Amazon.Tasks
+{
+    public class SyncListings : BackgroundTask
+    {
+        public SyncListings(Site site)
+            : base(site)
+        {
+            
+        }
+
+        public override void Execute()
+        {
+
+        }
+    }
+}
