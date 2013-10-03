ff4d30a98aae4421487153ac82c5c94ef5caf994
gmstr <gary.m.strong@gmail.com>
2013-10-03 11:39:09 +0100

    Trying caching images' existence in the db

--- MrCMS/DbConfiguration/Conventions/AddIndexConvention.cs
+++ MrCMS/DbConfiguration/Conventions/AddIndexConvention.cs
@@ -11,6 +11,7 @@ namespace MrCMS.DbConfiguration.Conventions
     {
         public void Apply(IPropertyInstance instance)
         {
+
             if (instance.Name == "DocumentType")
             {
                 instance.Index(string.Format("IX_{0}_{1}", instance.EntityType.Name, instance.Property.Name));
