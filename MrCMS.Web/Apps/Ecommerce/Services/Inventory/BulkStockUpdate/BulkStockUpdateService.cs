92861b736f96c70b6ac14b19bf1f42647d1185ec
thought <will.anderson@thought.co.uk>
2013-10-04 17:40:05 +0100

    bulk stock update to be faster

--- MrCMS.Web/Apps/Ecommerce/Services/Inventory/BulkStockUpdate/BulkStockUpdateService.cs
+++ MrCMS.Web/Apps/Ecommerce/Services/Inventory/BulkStockUpdate/BulkStockUpdateService.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using MrCMS.Helpers;
 using MrCMS.Web.Apps.Ecommerce.Entities.Products;
 using MrCMS.Web.Apps.Ecommerce.Services.Inventory.BulkStockUpdate.DTOs;
 using MrCMS.Web.Apps.Ecommerce.Services.Products;
+using NHibernate;
 
 namespace MrCMS.Web.Apps.Ecommerce.Services.Inventory.BulkStockUpdate
 {
     public class BulkStockUpdateService : IBulkStockUpdateService
     {
         private readonly IProductVariantService _productVariantService;
+        private readonly ISession _session;
         private IList<ProductVariant> _allVariants;
+        private readonly List<ProductVariant> _variantsToUpdate;
 
-        public BulkStockUpdateService(IProductVariantService productVariantService)
+        public BulkStockUpdateService(IProductVariantService productVariantService, ISession session)
         {
             _productVariantService = productVariantService;
+            _session = session;
             _allVariants = new List<ProductVariant>();
+            _variantsToUpdate = new List<ProductVariant>();
         }
 
         public int BulkStockUpdateFromDTOs(IEnumerable<BulkStockUpdateDataTransferObject> items)
@@ -22,15 +28,20 @@ namespace MrCMS.Web.Apps.Ecommerce.Services.Inventory.BulkStockUpdate
             _allVariants = _productVariantService.GetAll();
 
             var noOfUpdatedItems = 0;
-            foreach (var dataTransferObject in items)
-            {
-                BulkStockUpdate(dataTransferObject, ref noOfUpdatedItems);
-            }
+            
+            _session.Transact(session =>
+               {
+                   foreach (var dataTransferObject in items)
+                   {
+                       BulkStockUpdate(dataTransferObject, ref noOfUpdatedItems);
+                   }
+                   _variantsToUpdate.ForEach(session.SaveOrUpdate);
+               });
 
             return noOfUpdatedItems;
         }
 
-        public ProductVariant BulkStockUpdate(BulkStockUpdateDataTransferObject itemDto, ref int noOfUpdatedItems)
+        public void BulkStockUpdate(BulkStockUpdateDataTransferObject itemDto, ref int noOfUpdatedItems)
         {
             if (_allVariants == null)
                 _allVariants = new List<ProductVariant>();
@@ -40,11 +51,9 @@ namespace MrCMS.Web.Apps.Ecommerce.Services.Inventory.BulkStockUpdate
             if (item != null && item.StockRemaining.Value != itemDto.StockRemaining.Value)
             {
                 item.StockRemaining = itemDto.StockRemaining;
-                _productVariantService.Update(item);
+                _variantsToUpdate.Add(item);
                 noOfUpdatedItems++;
-                return item;
             }
-            return new ProductVariant();
         }
     }
 }
