a3a42d9f14c1682e28aa89aafd2b3a804354c79d
as <adem.sabic@thought.co.uk>
2013-10-08 16:40:22 +0200

    commit fix

--- MrCMS.Web/Apps/Ecommerce/Services/Products/IProductVariantService.cs
+++ MrCMS.Web/Apps/Ecommerce/Services/Products/IProductVariantService.cs
@@ -10,6 +10,7 @@ namespace MrCMS.Web.Apps.Ecommerce.Services.Products
     {
         IList<ProductVariant> GetAll();
         IPagedList<ProductVariant> GetAllVariants(string queryTerm, int categoryId = 0, int page = 1);
+        IList<ProductVariant> GetAllVariantsForGoogleBase();
         ProductVariant GetProductVariantBySKU(string sku);
         void Add(ProductVariant productVariant);
         void Update(ProductVariant productVariant);
