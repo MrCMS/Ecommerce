a09deb7008d7c90637da148b6d6087e8fce28e39
gmstr <gary.m.strong@gmail.com>
2013-10-10 13:18:01 +0100

    Adding in Paypal IPN support

--- MrCMS.Web/Apps/Ecommerce/Controllers/PayPalExpressCheckoutController.cs
+++ MrCMS.Web/Apps/Ecommerce/Controllers/PayPalExpressCheckoutController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Text;
+using System.Web.Mvc;
 using MrCMS.Entities.Documents;
 using MrCMS.Entities.Documents.Web;
 using MrCMS.Services;
@@ -15,12 +16,14 @@ namespace MrCMS.Web.Apps.Ecommerce.Controllers
         private readonly IPayPalExpressService _payPalExpressService;
         private readonly CartModel _cart;
         private readonly IDocumentService _documentService;
+        private readonly IPayPalIPNService _payPalIPNService;
 
-        public PayPalExpressCheckoutController(IPayPalExpressService payPalExpressService, CartModel cart, IDocumentService documentService)
+        public PayPalExpressCheckoutController(IPayPalExpressService payPalExpressService, CartModel cart, IDocumentService documentService, IPayPalIPNService payPalIPNService)
         {
             _payPalExpressService = payPalExpressService;
             _cart = cart;
             _documentService = documentService;
+            _payPalIPNService = payPalIPNService;
         }
 
         [HttpPost]
@@ -40,6 +43,16 @@ namespace MrCMS.Web.Apps.Ecommerce.Controllers
             return response.Success
                        ? _documentService.RedirectTo<SetDeliveryDetails>()
                        : _documentService.RedirectTo<Cart>();
+        }
+
+        public ActionResult IPN()
+        {
+            byte[] param = Request.BinaryRead(Request.ContentLength);
+            string ipnData = Encoding.ASCII.GetString(param);
+
+            _payPalIPNService.HandleIPN(ipnData);
+
+            return Content("");
         }
     }
 }
---
+++ MrCMS.Web/Apps/Ecommerce/Payment/PayPalExpress/PayPalCartManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using MrCMS.Web.Apps.Ecommerce.Services.Cart;
+using PayPal.PayPalAPIInterfaceService.Model;
+
+namespace MrCMS.Web.Apps.Ecommerce.Payment.PayPalExpress
+{
+    public class PayPalCartManager : IPayPalCartManager
+    {
+        private readonly ICartManager _cartManager;
+
+        public PayPalCartManager(ICartManager cartManager)
+        {
+            _cartManager = cartManager;
+        }
+
+        public void UpdateCart(GetExpressCheckoutDetailsResponseDetailsType details)
+        {
+            _cartManager.SetOrderEmail(details.BuyerMarketingEmail);
+            _cartManager.SetPaymentMethod(new PayPalExpressCheckoutPaymentMethod().SystemName);
+            _cartManager.SetPayPalExpressInfo(details.Token,
+                                              details.PayerInfo.PayerID);
+
+            _cartManager.SetBillingAddress(details.BillingAddress.GetAddress());
+
+            var paymentDetails = details.PaymentDetails.FirstOrDefault();
+            if (paymentDetails != null)
+                _cartManager.SetShippingAddress(paymentDetails.ShipToAddress.GetAddress());
+        }
+    }
+}
