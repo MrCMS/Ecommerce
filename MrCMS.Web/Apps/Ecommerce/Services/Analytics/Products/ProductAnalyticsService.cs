1d2a52ebaa3b28ea100cbac78d26412769b480c9
as <adem.sabic@thought.co.uk>
2013-10-21 15:37:53 +0200

    Customer Who Bought This Also Bought - Refactored

--- MrCMS.Web/Apps/Ecommerce/Services/Analytics/Products/ProductAnalyticsService.cs
+++ MrCMS.Web/Apps/Ecommerce/Services/Analytics/Products/ProductAnalyticsService.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using System.Linq;
+using MrCMS.Web.Apps.Ecommerce.Entities.Orders;
+using MrCMS.Web.Apps.Ecommerce.Entities.Products;
 using MrCMS.Web.Apps.Ecommerce.Pages;
 using NHibernate;
-using NHibernate.Transform;
+using NHibernate.Criterion;
+using NHibernate.SqlCommand;
 
 namespace MrCMS.Web.Apps.Ecommerce.Services.Analytics.Products
 {
@@ -22,18 +25,25 @@ namespace MrCMS.Web.Apps.Ecommerce.Services.Analytics.Products
 
         public List<Product> GetListOfProductsWhoWhereAlsoBought(Product product)
         {
-            var items = _session.CreateSQLQuery(@"SELECT TOP 20 P.Id,P.Name,P.UrlSegment,P.Abstract FROM Ecommerce_OrderLine AS OL 
-                                                LEFT OUTER JOIN Ecommerce_ProductVariant AS PV ON OL.SKU=PV.SKU
-                                                INNER JOIN Document AS P ON P.Id=PV.ProductId
-                                                WHERE OL.OrderID IN (
-                                                SELECT OrderID 
-                                                FROM Ecommerce_OrderLine AS OL2 
-                                                LEFT OUTER JOIN Ecommerce_ProductVariant AS PV2 ON OL2.SKU=PV2.SKU 
-                                                WHERE PV2.ProductId=" +product.Id+@")
-                                                AND PV.ProductId <> "+product.Id+@"
-                                                GROUP BY P.Id,P.Name,P.UrlSegment,P.Abstract
-                                                ORDER BY COUNT(OL.SKU) DESC")
-                                                .SetResultTransformer(Transformers.AliasToBean<Product>()).List<Product>().ToList();
+            var items = _session.CreateCriteria<Product>()
+               .Add(
+                          Subqueries.PropertyIn("Id",
+                          DetachedCriteria.For<ProductVariant>()
+                          .SetProjection(Projections.Property("Product.Id"))
+                          .Add(Subqueries.PropertyIn("Id",
+                              DetachedCriteria.For<OrderLine>()
+                              .SetProjection(Projections.Property("ProductVariant.Id"))
+                              .Add(Subqueries.PropertyIn("Order.Id",
+                                  DetachedCriteria.For<OrderLine>()
+                                  .SetProjection(Projections.Property("Order.Id"))
+                                  .CreateCriteria("ProductVariant", JoinType.LeftOuterJoin)
+                                  .Add(Restrictions.Eq("Product.Id", product.Id))
+                              ))
+                          ))
+               ))
+               .Add(Restrictions.Not(Restrictions.Eq("Id", product.Id))).SetCacheable(true)
+               .SetMaxResults(20)
+              .List<Product>().ToList();
 
             return items;
         }
