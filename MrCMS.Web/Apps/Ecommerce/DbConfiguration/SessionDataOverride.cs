2b0ceabeebd35eaa71babc822132fa789415f392
gmstr <gary.m.strong@gmail.com>
2013-10-24 10:19:09 +0100

    Updating how the session data is persisted

---
+++ MrCMS.Web/Apps/Ecommerce/DbConfiguration/SessionDataOverride.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Automapping;
+using FluentNHibernate.Automapping.Alterations;
+using MrCMS.DbConfiguration;
+using MrCMS.Web.Apps.Ecommerce.Entities;
+using MrCMS.Web.Apps.Ecommerce.Services.Cart;
+
+namespace MrCMS.Web.Apps.Ecommerce.DbConfiguration
+{
+    public class SessionDataOverride : IAutoMappingOverride<SessionData>
+    {
+        public void Override(AutoMapping<SessionData> mapping)
+        {
+            mapping.Map(data => data.Data).MakeVarCharMax();
+            mapping.Map(data => data.Key).Index(string.Format("IX_SessionData_Key"));
+            mapping.Map(data => data.UserGuid).Index(string.Format("IX_SessionData_UserGuid"));
+        }
+    }
+}
--- MrCMS.Web/Apps/Ecommerce/Services/Cart/CartSessionManager.cs
+++ MrCMS.Web/Apps/Ecommerce/Services/Cart/CartSessionManager.cs
@@ -1,37 +1,69 @@
-﻿using System;
+﻿using System.Runtime.Serialization.Formatters.Binary;
 using System.Web;
+using MrCMS.Entities.Multisite;
+using MrCMS.Web.Apps.Ecommerce.Entities;
+using NHibernate;
+using Newtonsoft.Json;
+using MrCMS.Helpers;
 
 namespace MrCMS.Web.Apps.Ecommerce.Services.Cart
 {
     public class CartSessionManager : ICartSessionManager
     {
-        private readonly HttpSessionStateBase _session;
+        private readonly ISession _session;
+        private readonly IGetUserGuid _getUserGuid;
+        private readonly Site _site;
 
-        public CartSessionManager(HttpSessionStateBase session)
+        public CartSessionManager(ISession session, IGetUserGuid getUserGuid, Site site)
         {
             _session = session;
+            _getUserGuid = getUserGuid;
+            _site = site;
         }
 
         public T GetSessionValue<T>(string key, T defaultValue = default(T))
         {
-            if (_session != null)
+            var sessionData =
+                _session.QueryOver<SessionData>()
+                        .Where(data => data.UserGuid == _getUserGuid.UserGuid && data.Site == _site && data.Key == key)
+                        .Take(1)
+                        .Cacheable()
+                        .SingleOrDefault();
+            if (sessionData == null)
+                return defaultValue;
+            try
             {
-                try { return (T)Convert.ChangeType(_session[key], typeof(T)); }
-                catch { }
+                return JsonConvert.DeserializeObject<T>(sessionData.Data);
             }
-            return defaultValue;
+            catch
+            {
+                return defaultValue;
+            }
         }
 
         public void SetSessionValue<T>(string key, T item)
         {
-            if (_session != null)
-                _session[key] = item;
+            var sessionData =
+                _session.QueryOver<SessionData>()
+                        .Where(data => data.UserGuid == _getUserGuid.UserGuid && data.Site == _site && data.Key == key)
+                        .Take(1)
+                        .Cacheable()
+                        .SingleOrDefault() ?? new SessionData { Key = key, UserGuid = _getUserGuid.UserGuid };
+
+            sessionData.Data = JsonConvert.SerializeObject(item);
+            _session.Transact(session => session.SaveOrUpdate(sessionData));
         }
 
         public void RemoveValue(string key)
         {
-            if (_session != null)
-                _session.Remove(key);
+            var sessionData =
+                _session.QueryOver<SessionData>()
+                        .Where(data => data.UserGuid == _getUserGuid.UserGuid && data.Site == _site && data.Key == key)
+                        .Take(1)
+                        .Cacheable()
+                        .SingleOrDefault();
+            if (sessionData != null)
+                _session.Transact(session => session.Delete(sessionData));
         }
     }
 }
