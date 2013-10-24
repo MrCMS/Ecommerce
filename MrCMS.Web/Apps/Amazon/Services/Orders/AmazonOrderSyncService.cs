a1f58ca8a75d6d8bb9ea842480a6b50fe0975d9f
thought <will.anderson@thought.co.uk>
2013-10-24 12:34:50 +0100

    fix url with amazon sync

--- MrCMS.Web/Apps/Amazon/Services/Orders/AmazonOrderSyncService.cs
+++ MrCMS.Web/Apps/Amazon/Services/Orders/AmazonOrderSyncService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using MrCMS.Settings;
 using MrCMS.Web.Apps.Amazon.Services.Orders.Sync;
 using MrCMS.Web.Apps.Amazon.Settings;
@@ -49,12 +50,14 @@ namespace MrCMS.Web.Apps.Amazon.Services.Orders
             amazonSyncSettings.LastRun = to;
             _configurationProvider.SaveSettings(amazonSyncSettings);
             _amazonOrderSyncManager.GetUpdatedInfoFromAmazon(updatedOrdersRequest);
+
+
         }
 
         public GetUpdatedOrdersResult SyncSpecificOrders(string rawOrderIds)
         {
-            var orderIds=GetOrderIds(rawOrderIds);
-            return orderIds.Any() ? _amazonOrderSyncManager.GetUpdatedInfoFromAmazonAdHoc(orderIds) : 
+            var orderIds = GetOrderIds(rawOrderIds);
+            return orderIds.Any() ? _amazonOrderSyncManager.GetUpdatedInfoFromAmazonAdHoc(orderIds) :
                 new GetUpdatedOrdersResult() { ErrorMessage = "Please provide at least one valid Amazon Order Id." };
         }
 
