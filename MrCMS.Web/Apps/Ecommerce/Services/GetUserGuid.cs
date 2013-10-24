11359a52a058e34449a3d43510c9a20f750a66fe
gmstr <gary.m.strong@gmail.com>
2013-10-24 10:19:09 +0100

    Updating how the session data is persisted

---
+++ MrCMS.Web/Apps/Ecommerce/Services/GetUserGuid.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MrCMS.Website;
+
+namespace MrCMS.Web.Apps.Ecommerce.Services
+{
+    public class GetUserGuid : IGetUserGuid
+    {
+        public Guid UserGuid { get { return CurrentRequestData.UserGuid; } }
+    }
+}
