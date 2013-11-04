fa6c7311dc6bec94ca2e2e0f2a520d484fe3c8f0
gmstr <gary.m.strong@gmail.com>
2013-11-04 14:05:48 +0000

    Adding in basics of back in stock notifications

--- MrCMS.Web/Apps/Ecommerce/Controllers/ProductController.cs
+++ MrCMS.Web/Apps/Ecommerce/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using MrCMS.Web.Apps.Ecommerce.Entities.BackInStockNotification;
 using MrCMS.Website.Controllers;
 using MrCMS.Web.Apps.Ecommerce.Pages;
 using MrCMS.Web.Apps.Ecommerce.Services.Products;
@@ -10,19 +11,32 @@ namespace MrCMS.Web.Apps.Ecommerce.Controllers
     {
         private readonly ITrackingService _trackingService;
         private readonly IProductVariantService _productVariantService;
+        private readonly IBackInStockNotificationService _backInStockNotificationService;
 
-        public ProductController(ITrackingService trackingService, IProductVariantService productVariantService)
+        public ProductController(ITrackingService trackingService, IProductVariantService productVariantService, IBackInStockNotificationService backInStockNotificationService)
         {
             _trackingService = trackingService;
             _productVariantService = productVariantService;
+            _backInStockNotificationService = backInStockNotificationService;
         }
 
         public ViewResult Show(Product page, int? variant)
         {
             _trackingService.AddItemToRecentlyViewedItemsCookie(page.Id);
+            ViewData["back-in-stock"] = TempData["back-in-stock"];
             if (variant.HasValue)
                 ViewData["selected-variant"] = _productVariantService.Get(variant.Value);
             return View(page);
+        }
+
+        [HttpPost]
+        public RedirectResult BackInStock(BackInStockNotificationRequest request)
+        {
+            _backInStockNotificationService.SaveRequest(request);
+            TempData["back-in-stock"] = true;
+            if (request.ProductVariant != null && request.ProductVariant.Product != null)
+                return Redirect(string.Format("~/{0}", request.ProductVariant.Product.LiveUrlSegment));
+            return Redirect(Referrer.ToString());
         }
     }
 }
---
+++ MrCMS.Web/Apps/Ecommerce/Services/Products/IBackInStockNotificationService.cs
@@ -0,0 +1,9 @@
+﻿using MrCMS.Web.Apps.Ecommerce.Entities.BackInStockNotification;
+
+namespace MrCMS.Web.Apps.Ecommerce.Services.Products
+{
+    public interface IBackInStockNotificationService
+    {
+        void SaveRequest(BackInStockNotificationRequest request);
+    }
+}
