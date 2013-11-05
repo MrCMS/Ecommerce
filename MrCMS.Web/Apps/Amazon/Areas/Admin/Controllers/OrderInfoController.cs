5684c16c4c942b3bf64be28a428084ea94fdacdd
gmstr <gary.m.strong@gmail.com>
2013-11-05 14:51:23 +0000

    Updating order detail to show amazon info

---
+++ MrCMS.Web/Apps/Amazon/Areas/Admin/Controllers/OrderInfoController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using MrCMS.Web.Apps.Amazon.Services.Orders;
+using MrCMS.Web.Apps.Ecommerce.Entities.Orders;
+using MrCMS.Website.Controllers;
+
+namespace MrCMS.Web.Apps.Amazon.Areas.Admin.Controllers
+{
+    public class OrderInfoController : MrCMSAppAdminController<AmazonApp>
+    {
+        private readonly IAmazonOrderService _amazonOrderService;
+
+        public OrderInfoController(IAmazonOrderService amazonOrderService)
+        {
+            _amazonOrderService = amazonOrderService;
+        }
+
+        public ActionResult AmazonInfo(Order order)
+        {
+            if (order != null)
+                return PartialView(_amazonOrderService.GetByOrderId(order.Id));
+            return new EmptyResult();
+        }
+    }
+}
--- MrCMS.Web/Apps/Ecommerce/Models/OrderSearchModel.cs
+++ MrCMS.Web/Apps/Ecommerce/Models/OrderSearchModel.cs
@@ -1,19 +1,34 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.Mvc;
 using MrCMS.Paging;
 using MrCMS.Web.Apps.Ecommerce.Entities.Orders;
 using System;
+using MrCMS.Helpers;
+
 namespace MrCMS.Web.Apps.Ecommerce.Models
 {
     public class OrderSearchModel
     {
+        [DisplayName("Email or last name")]
         public string SearchText { get; set; }
+        [DisplayName("Order Id")]
         public string OrderId { get; set; }
+        [DisplayName("From")]
         public DateTime? DateFrom { get; set; }
+        [DisplayName("To")]
         public DateTime? DateTo { get; set; }
         [DisplayName("Payment Status")]
         public PaymentStatus? PaymentStatus { get; set; }
         [DisplayName("Shipping Status")]
         public ShippingStatus? ShippingStatus { get; set; }
         public IPagedList<Order> Results { get; set; }
+        [DisplayName("Sales Channel")]
+        public string SalesChannel { get; set; }
+
+        public List<SelectListItem> SalesChannelOptions
+        {
+            get { return EcommerceApp.SalesChannels.BuildSelectItemList(s => s, emptyItemText: "All"); }
+        }
     }
 }
