b1beede4a39a5feefbe42fdc06ccc6e5b171139e
Andrew <andrew.king@thought.co.uk>
2013-11-11 15:50:45 +0000

    product added to cart started

---
+++ MrCMS.Web/Apps/Ecommerce/Controllers/ProductAddedToCartController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using MrCMS.Web.Apps.Ecommerce.Entities.Products;
+using MrCMS.Web.Apps.Ecommerce.Models;
+using MrCMS.Web.Apps.Ecommerce.Pages;
+using MrCMS.Website.Controllers;
+using NHibernate;
+
+namespace MrCMS.Web.Apps.Ecommerce.Controllers
+{
+    public class ProductAddedToCartController : MrCMSAppUIController<EcommerceApp>
+    {
+        private readonly CartModel _cartModel;
+
+        public ProductAddedToCartController(CartModel cartModel)
+        {
+            _cartModel = cartModel;
+        }
+
+        public ViewResult Show(ProductAddedToCart page, ProductVariant productVariant)
+        {
+            ViewData["productvariant"] = productVariant;
+
+            return View(page);
+        }
+
+        public PartialViewResult ItemDetails(ProductVariant productVariant)
+        {
+            return PartialView(productVariant);
+        }
+
+        public PartialViewResult CartDetails()
+        {
+            return PartialView(_cartModel);
+        }
+    }
+}
