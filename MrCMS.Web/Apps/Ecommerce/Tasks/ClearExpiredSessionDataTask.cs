6d5db885ceba30c8dc2ef56cda2e6fb195a1a4d6
gmstr <gary.m.strong@gmail.com>
2013-11-13 10:48:44 +0000

    Adding expiry for encrypted data

---
+++ MrCMS.Web/Apps/Ecommerce/Tasks/ClearExpiredSessionDataTask.cs
@@ -0,0 +1,32 @@
+﻿using MrCMS.Entities.Multisite;
+using MrCMS.Tasks;
+using MrCMS.Web.Apps.Ecommerce.Entities;
+using MrCMS.Website;
+using NHibernate;
+
+namespace MrCMS.Web.Apps.Ecommerce.Tasks
+{
+    public class ClearExpiredSessionDataTask : BackgroundTask
+    {
+        public ClearExpiredSessionDataTask(Site site)
+            : base(site)
+        {
+        }
+
+        public override void Execute()
+        {
+            var statelessSession = MrCMSApplication.Get<ISessionFactory>().OpenStatelessSession();
+            var sessionDatas =
+                statelessSession.QueryOver<SessionData>().Where(data => data.ExpireOn <= CurrentRequestData.Now).List();
+
+            using (var transaction = statelessSession.BeginTransaction())
+            {
+                foreach (var sessionData in sessionDatas)
+                {
+                    statelessSession.Delete(sessionData);
+                }
+                transaction.Commit();
+            }
+        }
+    }
+}
