b2349486fdb269aa7dfdef152d033eb22df0d945
thought <will.anderson@thought.co.uk>
2013-11-29 17:57:39 +0000

    Fixing scheduled task checker to new up a new session on each check

--- MrCMS/Website/ScheduledTaskChecker.cs
+++ MrCMS/Website/ScheduledTaskChecker.cs
@@ -1,5 +1,7 @@
 ﻿using System.Timers;
 using MrCMS.Tasks;
+using NHibernate;
+using MrCMS.Helpers;
 
 namespace MrCMS.Website
 {
@@ -32,7 +34,7 @@ namespace MrCMS.Website
 
         private void AppendScheduledTasks(object sender, ElapsedEventArgs e)
         {
-            var scheduledTaskManager = MrCMSApplication.Get<IScheduledTaskManager>();
+            var scheduledTaskManager = new ScheduledTaskManager(MrCMSApplication.Get<ISessionFactory>().OpenFilteredSession());
             foreach (var scheduledTask in scheduledTaskManager.GetDueTasks())
                 TaskExecutor.ExecuteLater(scheduledTaskManager.GetTask(scheduledTask));
 
