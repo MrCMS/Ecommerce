f5d155002832fab40997d5a18af787dd3a820660
gmstr <gary.m.strong@gmail.com>
2013-11-29 16:51:21 +0000

    Updating product.js to make it pluggable

--- MrCMS.Web/Apps/Ecommerce/Content/Scripts/custom/product.js
+++ MrCMS.Web/Apps/Ecommerce/Content/Scripts/custom/product.js
@@ -3,23 +3,17 @@
 });
 
 var Product = new function() {
+    this.History = window.History;
+    this.setVariant = function(variantId) {
+        Product.History.pushState({ variant: variantId }, $('title').html(), location.pathname + '?variant' + variantId);
+    };
     this.init = function() {
-        var History = window.History; // Note: We are using a capital H instead of a lower h
         $(document).on('change', '#variant', function() {
-            var data = getData();
-            History.pushState(data, $('title').html(), location.pathname + buildUpQueryString(data));
+            Product.setVariant($('#variant').val());
         });
 
-        function getData() {
-            return { variant: $('#variant').val() };
-        }
-
-        function buildUpQueryString(data) {
-            return "?variant=" + data.variant;
-        }
-
         // Bind to StateChange Event
-        History.Adapter.bind(window, 'statechange', function() { // Note: We are using statechange instead of popstate
+        Product.History.Adapter.bind(window, 'statechange', function() { // Note: We are using statechange instead of popstate
             var State = History.getState();
             $.get('/product/variant-details/' + State.data.variant, function(response) {
                 $('#variant-details').replaceWith(response);
