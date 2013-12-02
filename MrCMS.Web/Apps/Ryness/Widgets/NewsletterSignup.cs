c6a02b1818ab8f2eeece46afec5c52dfa0711131
thought <will.anderson@thought.co.uk>
2013-12-02 20:10:01 +0000

    Removing Ryness

--- MrCMS.Web/Apps/Ryness/Widgets/NewsletterSignup.cs
+++
@@ -1,9 +0,0 @@
-﻿using MrCMS.Entities.Widget;
-
-namespace MrCMS.Web.Apps.Ryness.Widgets
-{
-    public class NewsletterSignup : Widget
-    {
-        public virtual string EmailAddress { get; set; }
-    }
-}
