8e00dbec2d22e9b97c7c74d9c09e96b02f3ade87
gmstr <gary.m.strong@gmail.com>
2013-12-02 12:18:57 +0000

    Moving builder into own file

--- MrCMS.EcommerceApp.Tests/Services/DownloadProductServiceTests.cs
+++ MrCMS.EcommerceApp.Tests/Services/DownloadProductServiceTests.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using FakeItEasy;
 using FluentAssertions;
+using MrCMS.EcommerceApp.Tests.Builders;
 using MrCMS.Helpers;
 using MrCMS.Services;
 using MrCMS.Web.Apps.Ecommerce.Entities.Orders;
@@ -138,55 +139,6 @@ namespace MrCMS.EcommerceApp.Tests.Services
             var errors = passing ? new List<string>() : new List<string> { "an error" };
             A.CallTo(() => downloadOrderedFileValidationRule.GetErrors(order, orderLine)).Returns(errors);
             _rules.Add(downloadOrderedFileValidationRule);
-        }
-    }
-
-    public class OrderLineBuilder
-    {
-        private string _downloadFileUrl;
-        private string _downloadContentType;
-        private string _downloadFileName;
-        private int _numberOfDownloads;
-
-        public OrderLineBuilder()
-        {
-            _downloadFileUrl = "test-url";
-            _downloadContentType = "content-type";
-            _downloadFileName = "file-name";
-        }
-
-        public OrderLineBuilder WithFileUrl(string fileUrl)
-        {
-            _downloadFileUrl = fileUrl;
-            return this;
-        }
-
-        public OrderLineBuilder WithContentType(string contentType)
-        {
-            _downloadContentType = contentType;
-            return this;
-        }
-
-        public OrderLineBuilder WithFileName(string fileName)
-        {
-            _downloadFileName = fileName;
-            return this;
-        }
-        public OrderLineBuilder WithNumberOfDownloads(int numberOfDownloads)
-        {
-            _numberOfDownloads = numberOfDownloads;
-            return this;
-        }
-
-        public OrderLine Build()
-        {
-            return new OrderLine
-                       {
-                           DownloadFileUrl = _downloadFileUrl,
-                           DownloadFileContentType = _downloadContentType,
-                           DownloadFileName = _downloadFileName,
-                           NumberOfDownloads = _numberOfDownloads
-                       };
         }
     }
 }
