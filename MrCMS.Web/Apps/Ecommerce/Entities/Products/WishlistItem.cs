c3fc62ffab8b7398a3dda59fe1831f092564351c
gmstr <gary.m.strong@gmail.com>
2014-01-29 17:21:37 +0000

    Adding initial implementation of wishlist

---
+++ MrCMS.Web/Apps/Ecommerce/Entities/Products/WishlistItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MrCMS.Entities;
+
+namespace MrCMS.Web.Apps.Ecommerce.Entities.Products
+{
+    public class WishlistItem : SiteEntity
+    {
+        public virtual Guid UserGuid { get; set; }
+        public virtual ProductVariant Item { get; set; }
+    }
+}
