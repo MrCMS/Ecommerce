31dfe7d1d13231f5b6627b509ee610f297b9d3dd
thought <will.anderson@thought.co.uk>
2014-01-29 13:31:54 +0000

    back in stock should be its own view - done
    adding validation for back in stock

--- MrCMS.Web/Apps/Ecommerce/Entities/BackInStockNotification/BackInStockNotificationRequest.cs
+++ MrCMS.Web/Apps/Ecommerce/Entities/BackInStockNotification/BackInStockNotificationRequest.cs
@@ -1,4 +1,6 @@
-﻿using MrCMS.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using MrCMS.Entities;
+using MrCMS.Helpers.Validation;
 using MrCMS.Web.Apps.Ecommerce.Entities.Products;
 
 namespace MrCMS.Web.Apps.Ecommerce.Entities.BackInStockNotification
@@ -6,6 +8,8 @@ namespace MrCMS.Web.Apps.Ecommerce.Entities.BackInStockNotification
     public class BackInStockNotificationRequest : SiteEntity
     {
         public virtual ProductVariant ProductVariant { get; set; }
+        [Required]
+        [EmailValidator]
         public virtual string Email { get; set; }
         public virtual bool IsNotified { get; set; }
     }
