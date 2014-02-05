32c2fd7e05508a6789256a917594cb8c6efe7be6
Gary Strong <gary.strong@thought.co.uk>
2014-02-05 10:40:16 +0000

    Adding tests for set categories

---
+++ MrCMS.EcommerceApp.Tests/Services/ImportExport/ImportProductsServiceTests/CategoryBuilder.cs
@@ -0,0 +1,30 @@
+﻿using MrCMS.Web.Apps.Ecommerce.Pages;
+
+namespace MrCMS.EcommerceApp.Tests.Services.ImportExport.ImportProductsServiceTests
+{
+    public class CategoryBuilder
+    {
+        private string _name = "Category";
+        private string _urlSegment = "test-builder";
+
+        public Category Build()
+        {
+            return new Category
+                {
+                    Name = _name,
+                    UrlSegment = _urlSegment
+                };
+        }
+        public CategoryBuilder WithName(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public CategoryBuilder WithUrlSegment(string urlSegment)
+        {
+            _urlSegment = urlSegment;
+            return this;
+        }
+    }
+}
---
+++ MrCMS.EcommerceApp.Tests/Services/ImportExport/ImportProductsServiceTests/ProductBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using MrCMS.Web.Apps.Ecommerce.Pages;
+using System.Linq;
+
+namespace MrCMS.EcommerceApp.Tests.Services.ImportExport.ImportProductsServiceTests
+{
+    public class ProductBuilder
+    {
+        private List<Category> _categories = new List<Category>();
+
+        public Product Build()
+        {
+            return new Product
+                {
+                    Categories = _categories
+                };
+        }
+
+        public ProductBuilder WithCategories(params Category[] categories)
+        {
+            _categories = categories.ToList();
+            return this;
+        }
+    }
+}
