b47b65566c026778f0fd933511bc5f857db68294
gmstr <gary.m.strong@gmail.com>
2014-03-19 10:39:33 +0000

    Updating logic to work with the latest version of jQuery
    Signed-off-by: gmstr <gary.m.strong@gmail.com>

--- MrCMS.Web/Apps/Ecommerce/Areas/Admin/Content/Scripts/custom/specification-attributes.js
+++ MrCMS.Web/Apps/Ecommerce/Areas/Admin/Content/Scripts/custom/specification-attributes.js
@@ -1,5 +1,52 @@
-﻿$(function () {
-    $('button#close').click(function () {
+﻿//$(function () {
+//    $('button#close').click(function () {
+//        var pId = $("#ProductId").val();
+//        var saId = $("#Option").val();
+//        var noOfOptions = $("#Option option").length;
+//        if (noOfOptions > 0) {
+//            var saValue = $("#Value option:selected").text();
+//            if (saValue === "Other") {
+//                saValue = $("#new-option-value").val();
+//            }
+//            $.post('/Admin/Apps/Ecommerce/Product/AddSpecification',
+//                { Option: saId, Value: saValue, ProductId: pId },
+//                function (response) {
+//                    parent.$.get('/Admin/Apps/Ecommerce/Product/Specifications/' + pId, function (products) {
+//                        parent.$('#specification-list').replaceWith(products);
+//                        parent.$.fancybox.close();
+//                    });
+//                    return false;
+//                });
+//        } else {
+//            parent.$.fancybox.close();
+//            return false;
+//        }
+//        return false;
+//    });
+
+//    $('#Option').live("change", function () {
+//        var saId = $(this).val();
+//        $.getJSON('/Admin/Apps/Ecommerce/Product/GetSpecificationAttributeOptions',
+//            { specificationAttributeId: saId },
+//            function (response) {
+//                $("#Value").empty();
+//                for (var i = 0, len = response.length; i < len; i++)
+//                    $("#Value").append("<option value=" + response[i].Value + ">" + response[i].Text + "</option>");
+//            });
+//    });
+
+//    $('#Value').live("change", function () {
+//        if ($(this).val() === "0") {
+//            $("#other").show();
+//        } else {
+//            $("#other").hide();
+//        }
+//    });
+//});
+
+var AddSpecifications = function () {
+    var updateCallingPage = function (event) {
+        event.preventDefault();
         var pId = $("#ProductId").val();
         var saId = $("#Option").val();
         var noOfOptions = $("#Option option").length;
@@ -15,16 +62,13 @@
                         parent.$('#specification-list').replaceWith(products);
                         parent.$.fancybox.close();
                     });
-                    return false;
                 });
         } else {
             parent.$.fancybox.close();
-            return false;
         }
-        return false;
-    });
-
-    $('#Option').live("change", function () {
+    };
+    var updateValues = function (event) {
+        event.preventDefault();
         var saId = $(this).val();
         $.getJSON('/Admin/Apps/Ecommerce/Product/GetSpecificationAttributeOptions',
             { specificationAttributeId: saId },
@@ -33,13 +77,22 @@
                 for (var i = 0, len = response.length; i < len; i++)
                     $("#Value").append("<option value=" + response[i].Value + ">" + response[i].Text + "</option>");
             });
-    });
-
-    $('#Value').live("change", function () {
-        if ($(this).val() === "0") {;
+    };
+    var showHideOther = function (event) {
+        if ($(this).val() === "0") {
             $("#other").show();
         } else {
             $("#other").hide();
         }
-    });
-})
+    };
+    return {
+        init: function () {
+            $(document).on('click', 'button#close', updateCallingPage);
+            $(document).on('change', '#Option', updateValues);
+            $(document).on('change', '#Value', showHideOther);
+        }
+    };
+};
+$(function () {
+    new AddSpecifications().init();
+});
