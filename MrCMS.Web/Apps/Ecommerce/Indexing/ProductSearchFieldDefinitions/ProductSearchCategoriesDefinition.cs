d994964aa42a172c457da52bcfc933f7a391f3b7
gmstr <gary.m.strong@gmail.com>
2014-03-20 10:30:53 +0000

    Adding related entities definitions to the product search fields

--- MrCMS.Web/Apps/Ecommerce/Indexing/ProductSearchFieldDefinitions/ProductSearchCategoriesDefinition.cs
+++ MrCMS.Web/Apps/Ecommerce/Indexing/ProductSearchFieldDefinitions/ProductSearchCategoriesDefinition.cs
@@ -1,17 +1,26 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Lucene.Net.Documents;
+using MrCMS.Entities;
 using MrCMS.Helpers;
+using MrCMS.Indexing;
 using MrCMS.Indexing.Management;
+using MrCMS.Tasks;
+using MrCMS.Web.Apps.Ecommerce.Entities.Products;
 using MrCMS.Web.Apps.Ecommerce.Pages;
+using NHibernate;
 
 namespace MrCMS.Web.Apps.Ecommerce.Indexing.ProductSearchFieldDefinitions
 {
     public class ProductSearchCategoriesDefinition : StringFieldDefinition<ProductSearchIndex, Product>
     {
-        public ProductSearchCategoriesDefinition(ILuceneSettingsService luceneSettingsService)
+        private readonly ISession _session;
+
+        public ProductSearchCategoriesDefinition(ISession session, ILuceneSettingsService luceneSettingsService)
             : base(luceneSettingsService, "categories", index: Field.Index.NOT_ANALYZED)
         {
+            _session = session;
         }
 
         protected override IEnumerable<string> GetValues(Product obj)
@@ -35,6 +44,39 @@ namespace MrCMS.Web.Apps.Ecommerce.Indexing.ProductSearchFieldDefinitions
                 yield return parent;
                 parent = parent.Parent.Unproxy() as Category;
             }
+        }
+
+        public override Dictionary<Type, Func<SystemEntity, IEnumerable<LuceneAction>>> GetRelatedEntities()
+        {
+            return new Dictionary<Type, Func<SystemEntity, IEnumerable<LuceneAction>>>
+                       {
+                           {
+                               typeof (Category),
+                               entity =>
+                                   {
+                                       if (entity is Category)
+                                       {
+                                           var category = (entity as Category);
+                                           Category categoryAlias = null;
+                                           IList<Product> products =
+                                               _session.QueryOver<Product>()
+                                                       .JoinAlias(product => product.Categories, () => categoryAlias)
+                                                       .Where(product => categoryAlias.Id == category.Id)
+                                                       .Cacheable()
+                                                       .List();
+                                           return products.Select(product =>
+                                                                  new LuceneAction
+                                                                      {
+                                                                          Entity = product.Unproxy(),
+                                                                          Operation = LuceneOperation.Update,
+                                                                          IndexDefinition =
+                                                                              IndexingHelper.Get<ProductSearchIndex>()
+                                                                      }).ToList();
+                                       }
+                                       return new List<LuceneAction>();
+                                   }
+                           }
+                       };
         }
     }
 }
