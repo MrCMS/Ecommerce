32c268fb8f6c1d986de18d212e9908eb063b8e83
gmstr <gary.m.strong@gmail.com>
2014-03-20 10:30:53 +0000

    Adding related entities definitions to the product search fields

--- MrCMS.Web/Apps/Ecommerce/Indexing/ProductSearchFieldDefinitions/ProductSearchBrandDefinition.cs
+++ MrCMS.Web/Apps/Ecommerce/Indexing/ProductSearchFieldDefinitions/ProductSearchBrandDefinition.cs
@@ -1,15 +1,26 @@
+using System;
 using System.Collections.Generic;
 using Lucene.Net.Documents;
+using MrCMS.Entities;
+using MrCMS.Helpers;
+using MrCMS.Indexing;
 using MrCMS.Indexing.Management;
+using MrCMS.Tasks;
+using MrCMS.Web.Apps.Ecommerce.Entities.Products;
 using MrCMS.Web.Apps.Ecommerce.Pages;
+using NHibernate;
+using System.Linq;
 
 namespace MrCMS.Web.Apps.Ecommerce.Indexing.ProductSearchFieldDefinitions
 {
     public class ProductSearchBrandDefinition : StringFieldDefinition<ProductSearchIndex, Product>
     {
-        public ProductSearchBrandDefinition(ILuceneSettingsService luceneSettingsService)
+        private readonly ISession _session;
+
+        public ProductSearchBrandDefinition(ISession session, ILuceneSettingsService luceneSettingsService)
             : base(luceneSettingsService, "brands", index: Field.Index.NOT_ANALYZED)
         {
+            _session = session;
         }
 
         protected override IEnumerable<string> GetValues(Product obj)
@@ -18,6 +29,37 @@ namespace MrCMS.Web.Apps.Ecommerce.Indexing.ProductSearchFieldDefinitions
                 yield return obj.Brand.Id.ToString();
 
             yield return null;
+        }
+
+        public override Dictionary<Type, Func<SystemEntity, IEnumerable<LuceneAction>>> GetRelatedEntities()
+        {
+            return new Dictionary<Type, Func<SystemEntity, IEnumerable<LuceneAction>>>
+                       {
+                           {
+                               typeof (Brand),
+                               entity =>
+                                   {
+                                       if (entity is Brand)
+                                       {
+                                           var brand = (entity as Brand);
+                                           var products =
+                                               _session.QueryOver<Product>()
+                                                       .Where(product => product.Brand.Id == brand.Id)
+                                                       .Cacheable()
+                                                       .List();
+                                           return products.Select(product =>
+                                                                  new LuceneAction
+                                                                      {
+                                                                          Entity = product.Unproxy(),
+                                                                          Operation = LuceneOperation.Update,
+                                                                          IndexDefinition =
+                                                                              IndexingHelper.Get<ProductSearchIndex>()
+                                                                      }).ToList();
+                                       }
+                                       return new List<LuceneAction>();
+                                   }
+                           }
+                       };
         }
     }
 }
--- MrCMS.Web/Apps/Ecommerce/Indexing/ProductSearchFieldDefinitions/ProductSearchNumberBoughtDefinition.cs
+++ MrCMS.Web/Apps/Ecommerce/Indexing/ProductSearchFieldDefinitions/ProductSearchNumberBoughtDefinition.cs
@@ -1,7 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Lucene.Net.Documents;
+using MrCMS.Entities;
+using MrCMS.Helpers;
+using MrCMS.Indexing;
 using MrCMS.Indexing.Management;
+using MrCMS.Tasks;
 using MrCMS.Web.Apps.Ecommerce.Entities.Orders;
 using MrCMS.Web.Apps.Ecommerce.Entities.Products;
 using MrCMS.Web.Apps.Ecommerce.Pages;
@@ -13,9 +18,12 @@ namespace MrCMS.Web.Apps.Ecommerce.Indexing.ProductSearchFieldDefinitions
 {
     public class ProductSearchNumberBoughtDefinition : IntegerFieldDefinition<ProductSearchIndex, Product>
     {
-        public ProductSearchNumberBoughtDefinition(ILuceneSettingsService luceneSettingsService)
+        private readonly ISession _session;
+
+        public ProductSearchNumberBoughtDefinition(ISession session, ILuceneSettingsService luceneSettingsService)
             : base(luceneSettingsService, "numberbought", index: Field.Index.NOT_ANALYZED)
         {
+            _session = session;
         }
 
         protected override IEnumerable<int> GetValues(Product obj)
@@ -23,10 +31,33 @@ namespace MrCMS.Web.Apps.Ecommerce.Indexing.ProductSearchFieldDefinitions
             yield return GetNumberBought(obj.Variants);
         }
 
-        private static int GetNumberBought(IList<ProductVariant> variants)
+        private int GetNumberBought(IList<ProductVariant> variants)
         {
-            var orderLines = MrCMSApplication.Get<ISession>().QueryOver<OrderLine>().Where(line => line.ProductVariant.IsIn(variants.ToList())).List();
+            var orderLines = _session.QueryOver<OrderLine>().Where(line => line.ProductVariant.IsIn(variants.ToList())).List();
             return orderLines.Sum(line => line.Quantity);
+        }
+        public override Dictionary<Type, Func<SystemEntity, IEnumerable<LuceneAction>>> GetRelatedEntities()
+        {
+            return new Dictionary<Type, Func<SystemEntity, IEnumerable<LuceneAction>>>
+                       {
+                           {
+                               typeof (OrderLine),
+                               GetActions
+                           }
+                       };
+        }
+
+        private static IEnumerable<LuceneAction> GetActions(SystemEntity entity)
+        {
+            var line = entity as OrderLine;
+            if (line != null && line.ProductVariant != null && line.ProductVariant.Product != null)
+                yield return new LuceneAction
+                                 {
+                                     Entity = line.ProductVariant.Product.Unproxy(),
+                                     Operation = LuceneOperation.Update,
+                                     IndexDefinition =
+                                         IndexingHelper.Get<ProductSearchIndex>()
+                                 };
         }
     }
 }
