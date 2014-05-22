9a8729426642f901cb7b5b859f99be33a15afae8
Andrew <andrew.king@thought.co.uk>
2014-05-22 17:19:36 +0100

    more entity

---
+++ MrCMS.Web/Apps/Ecommerce/Entities/NewsletterBuilder/ContentItems/Abstract.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MrCMS.Entities;
+
+namespace MrCMS.Web.Apps.Ecommerce.Entities.NewsletterBuilder.ContentItems
+{
+    public class Abstract //: SiteEntity
+    {
+        //public virtual string Name { get; set; }
+    }
+}
