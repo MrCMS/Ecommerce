6c86e1d22ebec485f36a6de61d3c5aac26cc005e
Andrew <andrew.king@thought.co.uk>
2014-05-22 16:40:04 +0100

    CRUD for Templates

---
+++ MrCMS.Web/Apps/Ecommerce/Entities/NewsletterBuilder/Newsletter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MrCMS.Entities;
+
+namespace MrCMS.Web.Apps.Ecommerce.Entities.NewsletterBuilder
+{
+    public class Newsletter : SiteEntity
+    {
+        public virtual string Name { get; set; }
+    }
+}
---
+++ MrCMS.Web/Apps/Ecommerce/Services/NewsletterBuilder/INewsletterTemplateService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using MrCMS.Paging;
+using MrCMS.Web.Apps.Ecommerce.Entities.NewsletterBuilder;
+
+namespace MrCMS.Web.Apps.Ecommerce.Services.NewsletterBuilder
+{
+    public interface INewsletterTemplateService
+    {
+        IList<NewsletterTemplate> GetAll();
+        IPagedList<NewsletterTemplate> GetAllPaged();
+        void Add(NewsletterTemplate newsletterTemplate);
+        void Edit(NewsletterTemplate newsletterTemplate);
+        void Delete(NewsletterTemplate newsletterTemplate);
+    }
+}
