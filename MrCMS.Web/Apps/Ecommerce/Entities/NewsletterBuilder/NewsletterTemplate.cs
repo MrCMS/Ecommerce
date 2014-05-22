bd074b62a1af640e14598704d0bf7920c590109c
Andrew <andrew.king@thought.co.uk>
2014-05-22 15:23:26 +0100

    Created NewsletterTemplate Entity with override for varchar max

---
+++ MrCMS.Web/Apps/Ecommerce/Entities/NewsletterBuilder/NewsletterTemplate.cs
@@ -0,0 +1,17 @@
+﻿using MrCMS.Entities;
+
+namespace MrCMS.Web.Apps.Ecommerce.Entities.NewsletterBuilder
+{
+    public class NewsletterTemplate : SiteEntity
+    {
+        public virtual string Name { get; set; }
+        public virtual string BaseTemplate { get; set; }
+        public virtual string Divider { get; set; }
+        public virtual string FreeTextTemplate { get; set; }
+        public virtual string ImageAndTextTemplate { get; set; }
+        public virtual string ProductGridTemplate { get; set; }
+        public virtual string ProductRowTemplate { get; set; }
+        public virtual string ProductTemplate { get; set; }
+        public virtual string BannerTemplate { get; set; } 
+    }
+}
