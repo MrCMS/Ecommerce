05bf9e3b01e9977cb0fc417fbd9199092e664a4d
Mike McBain <mike.mcbain@thought.co.uk>
2014-05-27 17:20:46 +0100

    Amend implementation for url resolving

--- MrCMS.Web/Apps/Ecommerce/Parsing/BannerItemParser.cs
+++ MrCMS.Web/Apps/Ecommerce/Parsing/BannerItemParser.cs
@@ -1,19 +1,26 @@
 ﻿using System.Text.RegularExpressions;
 using MrCMS.Web.Apps.Ecommerce.Entities.NewsletterBuilder;
 using MrCMS.Web.Apps.Ecommerce.Entities.NewsletterBuilder.ContentItems;
+using MrCMS.Web.Apps.Ecommerce.Helpers;
 
 namespace MrCMS.Web.Apps.Ecommerce.Parsing
 {
-    public class BannerItemParser : AbstractNewsletterItemParser<Banner>
+    public class BannerItemParser : INewsletterItemParser<Banner>
     {
         private static readonly Regex ImageRegex = new Regex(@"\[(?i)ImageUrl\]");
         private static readonly Regex LinkRegex = new Regex(@"\[(?i)LinkUrl\]");
+        private readonly IUrlHelper _urlHelper;
 
-        public override string Parse(NewsletterTemplate template, Banner item)
+        public BannerItemParser(IUrlHelper urlHelper)
+        {
+            _urlHelper = urlHelper;
+        }
+
+        public string Parse(NewsletterTemplate template, Banner item)
         {
             string output = template.BannerTemplate;
-            output = ImageRegex.Replace(output, ToAbsolute(item.ImageUrl));
-            output = LinkRegex.Replace(output, ToAbsolute(item.LinkUrl));
+            output = ImageRegex.Replace(output, _urlHelper.ToAbsolute(item.ImageUrl));
+            output = LinkRegex.Replace(output, _urlHelper.ToAbsolute(item.LinkUrl));
             return output;
         }
     }
