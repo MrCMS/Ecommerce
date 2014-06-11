365ec5f3a5414187c82b04dbbda9ccb900770674
Andrew King <andrew.king@thought.co.uk>
2014-06-11 16:23:15 +0100

    css amends

--- MrCMS.Web/Apps/Ecommerce/Content/Scripts/custom/product.js
+++ MrCMS.Web/Apps/Ecommerce/Content/Scripts/custom/product.js
@@ -25,3 +25,17 @@ var Product = new function () {
     this.onChangeVariant = function () {
     };
 };
+$(function () {
+    $('#readFullDescription a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
