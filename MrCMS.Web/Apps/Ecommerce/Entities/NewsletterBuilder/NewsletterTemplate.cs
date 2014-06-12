3e35773e32905a97058cca56a1ab4d26f4f0f46e
gmstr <gary.m.strong@gmail.com>
2014-06-12 15:40:46 +0100

    Tweaking newsletter builder

--- MrCMS.Web/Apps/Ecommerce/Entities/NewsletterBuilder/NewsletterTemplate.cs
+++ MrCMS.Web/Apps/Ecommerce/Entities/NewsletterBuilder/NewsletterTemplate.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using MrCMS.Entities;
 
@@ -9,13 +10,20 @@ namespace MrCMS.Web.Apps.Ecommerce.Entities.NewsletterBuilder
     {
         [Required]
         public virtual string Name { get; set; }
+        [DisplayName("Base Template")]
         public virtual string BaseTemplate { get; set; }
         public virtual string Divider { get; set; }
+        [DisplayName("Free Text Template")]
         public virtual string FreeTextTemplate { get; set; }
+        [DisplayName("Image and Text Template")]
         public virtual string ImageAndTextTemplate { get; set; }
+        [DisplayName("Product Grid Template")]
         public virtual string ProductGridTemplate { get; set; }
+        [DisplayName("Product Row Template")]
         public virtual string ProductRowTemplate { get; set; }
+        [DisplayName("Product Template")]
         public virtual string ProductTemplate { get; set; }
+        [DisplayName("Banner Template")]
         public virtual string BannerTemplate { get; set; }
 
         public virtual IList<Newsletter> Newsletters { get; set; }
--- MrCMS.Web/Apps/Ecommerce/Services/NewsletterBuilder/INewsletterTemplateService.cs
+++ MrCMS.Web/Apps/Ecommerce/Services/NewsletterBuilder/INewsletterTemplateService.cs
@@ -7,7 +7,6 @@ namespace MrCMS.Web.Apps.Ecommerce.Services.NewsletterBuilder
     public interface INewsletterTemplateService
     {
         IList<NewsletterTemplate> GetAll();
-        IPagedList<NewsletterTemplate> GetAllPaged();
         void Add(NewsletterTemplate newsletterTemplate);
         void Edit(NewsletterTemplate newsletterTemplate);
         void Delete(NewsletterTemplate newsletterTemplate);
