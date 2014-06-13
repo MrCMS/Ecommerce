5a7bb93a68ef79c44f22f173271fea03b633bc39
gmstr <gary.m.strong@gmail.com>
2014-06-13 15:15:29 +0100

    Copy to clipboard for newsletter builder

---
+++ MrCMS.Web/Apps/Ecommerce/Areas/Admin/Content/Scripts/custom/edit-newsletter.js
@@ -0,0 +1,20 @@
+﻿var EditNewsletter = function () {
+
+    var client;
+    return {
+        init: function () {
+            client = new ZeroClipboard($("#copy-link"), {
+                moviePath: "/Apps/Ecommerce/Areas/Admin/Content/Scripts/lib/zero-clipboard/ZeroClipboard.swf"
+            });
+            client.on('dataRequested', function (client, args) {
+                $.get($("#copy-link").data('preview-url'), function (data) {
+                    client.setText(data);
+                    $('#data-copied-notice').show();
+                });
+            });
+        }
+    };
+};
+$(function () {
+    new EditNewsletter().init();
+});
