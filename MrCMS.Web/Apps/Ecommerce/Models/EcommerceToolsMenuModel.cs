4b2112bc24b269283a874a9c7a27347886d892f1
WillAnd <will.anderson@thought.co.uk>
2014-08-11 13:29:12 +0100

    Removed ecommerce menus in favour of fly out menu (settings)
    fixed bug in newsletter builder

--- MrCMS.Web/Apps/Ecommerce/Models/EcommerceToolsMenuModel.cs
+++ MrCMS.Web/Apps/Ecommerce/Models/EcommerceToolsMenuModel.cs
@@ -6,7 +6,7 @@ namespace MrCMS.Web.Apps.Ecommerce.Models
     public class EcommerceToolsMenuModel : IAdminMenuItem
     {
         private SubMenu _children;
-        public string Text { get { return "Tools"; } }
+        public string Text { get { return "Tools & Reports"; } }
         public string Url { get; private set; }
         public bool CanShow { get { return true; } }
         public SubMenu Children
@@ -30,6 +30,26 @@ namespace MrCMS.Web.Apps.Ecommerce.Models
                                        "/Admin/Apps/Ecommerce/Stock/BulkStockUpdate"),
                                    new ChildMenuItem("Bulk Shipping Update",
                                        "/Admin/Apps/Ecommerce/Order/BulkShippingUpdate"),
+                               }
+                           },
+                           {
+                               "Sales Reports",
+                               new List<ChildMenuItem>
+                               {
+                                   new ChildMenuItem("Sales by day",
+                                       "/Admin/Apps/Ecommerce/Report/SalesByDay"),
+                                   new ChildMenuItem("Sales by payment type",
+                                       "/Admin/Apps/Ecommerce/Report/SalesByPaymentType"),
+                                   new ChildMenuItem("Sales by shipping type",
+                                       "/Admin/Apps/Ecommerce/Report/SalesByShippingType"),
+                               }
+                           },
+                           {
+                               "Orders Reports",
+                               new List<ChildMenuItem>
+                               {
+                                   new ChildMenuItem("Orders by shipping type",
+                                       "/Admin/Apps/Ecommerce/Report/OrdersByShippingType"),
                                }
                            }
                        });
