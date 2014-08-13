c9832e62b8bc886ebf5e01facdc975d6f0c2f4c2
WillAnd <will.anderson@thought.co.uk>
2014-08-13 10:11:12 +0100

    bringing in search enhancements from ryness to make cache work

--- MrCMS.Web/Apps/Ecommerce/Controllers/ProductSearchController.cs
+++ MrCMS.Web/Apps/Ecommerce/Controllers/ProductSearchController.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Web.Mvc;
-using System.Web.Mvc.Html;
-using MrCMS.Helpers;
-using MrCMS.Services.Caching;
-using MrCMS.Web.Apps.Ecommerce.Helpers;
+﻿using System.Web.Mvc;
 using MrCMS.Web.Apps.Ecommerce.ModelBinders;
 using MrCMS.Web.Apps.Ecommerce.Pages;
-using MrCMS.Web.Apps.Ecommerce.Services;
 using MrCMS.Website.Binders;
-using MrCMS.Website.Caching;
 using MrCMS.Website.Controllers;
+using MrCMS.Web.Apps.Ecommerce.Services.Categories;
 using MrCMS.Web.Apps.Ecommerce.Services.Products;
 using MrCMS.Web.Apps.Ecommerce.Models;
 using System.Collections.Generic;
@@ -22,21 +16,19 @@ namespace MrCMS.Web.Apps.Ecommerce.Controllers
         private readonly IProductSearchIndexService _productSearchIndexService;
         private readonly CartModel _cart;
         private readonly IProductSearchQueryService _productSearchQueryService;
-        private readonly IHtmlCacheService _htmlCacheService;
 
         public ProductSearchController(IProductSearchIndexService productSearchIndexService, CartModel cart,
-            IProductSearchQueryService productSearchQueryService, IHtmlCacheService htmlCacheService)
+            IProductSearchQueryService productSearchQueryService)
         {
             _productSearchIndexService = productSearchIndexService;
             _cart = cart;
             _productSearchQueryService = productSearchQueryService;
-            _htmlCacheService = htmlCacheService;
         }
 
         public ViewResult Show(ProductSearch page, [IoCModelBinder(typeof(ProductSearchQueryModelBinder))]ProductSearchQuery query)
         {
             ViewData["query"] = query;
-            //SetViewData(query);
+            SetViewData(query);
             return View(page);
         }
 
@@ -47,30 +39,47 @@ namespace MrCMS.Web.Apps.Ecommerce.Controllers
             ViewData["product-price-range-max"] = 5000;
         }
 
-        public ActionResult Query([IoCModelBinder(typeof(ProductSearchQueryModelBinder))]ProductSearchQuery query)
-        {
-            return _htmlCacheService.GetContent(this, _productSearchQueryService.GetCachingInfo(query),
-                helper => helper.Action("QueryInternal", new {query}));
-        }
-
-        public PartialViewResult QueryInternal(ProductSearchQuery query)
+        public PartialViewResult Query([IoCModelBinder(typeof(ProductSearchQueryModelBinder))]ProductSearchQuery query)
         {
             SetViewData(query);
-            return PartialView("Query", query);
+            return PartialView(query);
         }
 
         [HttpGet]
-        public ActionResult Results([IoCModelBinder(typeof(ProductSearchQueryModelBinder))]ProductSearchQuery query)
-        {
-            return _htmlCacheService.GetContent(this, _productSearchQueryService.GetCachingInfo(query),
-                helper => helper.Action("ResultsInternal", new {query}));
-        }
-
-        public PartialViewResult ResultsInternal(ProductSearchQuery query)
+        public PartialViewResult Results([IoCModelBinder(typeof(ProductSearchQueryModelBinder))]ProductSearchQuery query)
         {
             ViewData["query"] = query;
             ViewData["cart"] = _cart;
-            return PartialView("Results", _productSearchIndexService.SearchProducts(query));
+            return PartialView(_productSearchIndexService.SearchProducts(query));
+        }
+    }
+
+    public interface IProductSearchQueryService
+    {
+        void SetViewData(ProductSearchQuery query, ViewDataDictionary viewData);
+    }
+
+    public class ProductSearchQueryService : IProductSearchQueryService
+    {
+        private readonly IProductOptionManager _productOptionManager;
+        private readonly IBrandService _brandService;
+        private readonly ICategoryService _categoryService;
+
+        public ProductSearchQueryService(IProductOptionManager productOptionManager, IBrandService brandService, ICategoryService categoryService)
+        {
+            _productOptionManager = productOptionManager;
+            _brandService = brandService;
+            _categoryService = categoryService;
+        }
+
+        public void SetViewData(ProductSearchQuery query, ViewDataDictionary viewData)
+        {
+            var productOptionSearchData = _productOptionManager.GetSearchData(query);
+            viewData["product-options"] = productOptionSearchData.AttributeOptions;
+            viewData["product-specifications"] = productOptionSearchData.SpecificationOptions;
+            viewData["product-brands"] = _brandService.GetAvailableBrands(query);
+            viewData["categories"] = _categoryService.GetCategoriesForSearch(query);
+
         }
     }
 }
