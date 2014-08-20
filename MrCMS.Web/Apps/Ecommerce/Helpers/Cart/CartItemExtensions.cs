788878a04552cc58c62ad5a51335e4a404eb2e08
WillAnd <will.anderson@thought.co.uk>
2014-08-20 11:05:27 +0100

    Extension method for giftcard recipient

--- MrCMS.Web/Apps/Ecommerce/Helpers/Cart/CartItemExtensions.cs
+++ MrCMS.Web/Apps/Ecommerce/Helpers/Cart/CartItemExtensions.cs
@@ -1,5 +1,7 @@
 using MrCMS.Web.Apps.Ecommerce.Entities.Cart;
+using MrCMS.Web.Apps.Ecommerce.Models;
 using MrCMS.Web.Apps.Ecommerce.Services.Shipping;
+using Newtonsoft.Json;
 
 namespace MrCMS.Web.Apps.Ecommerce.Helpers.Cart
 {
@@ -12,6 +14,17 @@ namespace MrCMS.Web.Apps.Ecommerce.Helpers.Cart
                 return true;
             return !productVariant.HasRestrictedShipping ||
                    productVariant.RestrictedTo.Contains(shippingMethod.TypeName);
+        }
+
+        public static string GiftCardRecipient(this CartItem item)
+        {
+            if (item == null || item.Item == null || !item.Item.IsGiftCard)
+                return null;
+
+            var giftCardInfo = JsonConvert.DeserializeObject<GiftCardInfo>(item.Data);
+            if (giftCardInfo == null)
+                return null;
+            return giftCardInfo.RecipientName;
         }
     }
 }
