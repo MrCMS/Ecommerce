6e25547c472a5c8be286a50f8b115234a83edb4b
WillAnd <will.anderson@thought.co.uk>
2014-10-25 11:37:58 +0100

    change version

--- MrCMS.Web/Apps/Ecommerce/EcommerceApp.cs
+++ MrCMS.Web/Apps/Ecommerce/EcommerceApp.cs
@@ -32,7 +32,7 @@ namespace MrCMS.Web.Apps.Ecommerce
 
         public override string Version
         {
-            get { return "0.2.1"; }
+            get { return "0.2.2"; }
         }
 
         protected override void RegisterServices(IKernel kernel)
