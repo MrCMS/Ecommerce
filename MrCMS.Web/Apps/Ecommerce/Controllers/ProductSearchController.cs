6edb17b72784f586d401ca370fe15474ab2a5819
Myo Zar Ni <myo.zarni@thought.co.uk>
2014-11-20 17:21:00 +0000

    search logs

--- MrCMS.Web/Apps/Ecommerce/Controllers/ProductSearchController.cs
+++ MrCMS.Web/Apps/Ecommerce/Controllers/ProductSearchController.cs
@@ -1,11 +1,15 @@
 ﻿using System.Web.Mvc;
 using System.Web.Mvc.Html;
+using MrCMS.Entities.People;
 using MrCMS.Services.Caching;
+using MrCMS.Web.Apps.Ecommerce.Entities.Search;
 using MrCMS.Web.Apps.Ecommerce.ModelBinders;
 using MrCMS.Web.Apps.Ecommerce.Models;
 using MrCMS.Web.Apps.Ecommerce.Pages;
 using MrCMS.Web.Apps.Ecommerce.Services;
 using MrCMS.Web.Apps.Ecommerce.Services.Products;
+using MrCMS.Web.Apps.Ecommerce.Services.Search;
+using MrCMS.Website;
 using MrCMS.Website.Binders;
 using MrCMS.Website.Controllers;
 
@@ -15,21 +19,33 @@ namespace MrCMS.Web.Apps.Ecommerce.Controllers
     {
         private readonly CartModel _cart;
         private readonly IHtmlCacheService _htmlCacheService;
+        private readonly ISearchLogService _searchLogService;
         private readonly IProductSearchIndexService _productSearchIndexService;
         private readonly IProductSearchQueryService _productSearchQueryService;
 
         public ProductSearchController(IProductSearchIndexService productSearchIndexService, CartModel cart,
-            IProductSearchQueryService productSearchQueryService, IHtmlCacheService htmlCacheService)
+            IProductSearchQueryService productSearchQueryService, IHtmlCacheService htmlCacheService, ISearchLogService searchLogService)
         {
             _productSearchIndexService = productSearchIndexService;
             _cart = cart;
             _productSearchQueryService = productSearchQueryService;
             _htmlCacheService = htmlCacheService;
+            _searchLogService = searchLogService;
         }
 
         public ViewResult Show(ProductSearch page,
             [IoCModelBinder(typeof(ProductSearchQueryModelBinder))] ProductSearchQuery query)
         {
+            User user = null;
+            if (CurrentRequestData.CurrentUser != null)
+                user = CurrentRequestData.CurrentUser;
+            _searchLogService.Add(new SearchLog
+                    {
+                        Text = query.SearchTerm,
+                        User = user
+                    }
+            );
+
             ViewData["query"] = query;
             SetViewData(query);
             return View(page);
