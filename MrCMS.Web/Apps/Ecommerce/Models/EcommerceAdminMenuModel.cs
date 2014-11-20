bb4a2b4c914f1b5061c2855e504c9f150d7b40d6
Myo Zar Ni <myo.zarni@thought.co.uk>
2014-11-20 17:21:00 +0000

    search logs

--- MrCMS.Web/Apps/Ecommerce/Models/EcommerceAdminMenuModel.cs
+++ MrCMS.Web/Apps/Ecommerce/Models/EcommerceAdminMenuModel.cs
@@ -56,6 +56,8 @@ namespace MrCMS.Web.Apps.Ecommerce.Models
                         ACLOption.Create(new ProductSpecificationAttributeACL(), ProductSpecificationAttributeACL.List)),
                     new ChildMenuItem("Discounts", _urlHelper.Action("Index", "Discount"),
                         ACLOption.Create(new DiscountACL(), DiscountACL.List)),
+                    new ChildMenuItem("Search Logs", _urlHelper.Action("Index", "SearchLog"),
+                        ACLOption.Create(new SearchLogACL(), SearchLogACL.List)),
                 };
                 if (_ecommerceSettings.GiftCardsEnabled)
                 {
---
+++ MrCMS.Web/Apps/Ecommerce/Services/Search/ISearchLogService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using MrCMS.Paging;
+using MrCMS.Web.Apps.Ecommerce.Entities.Search;
+
+namespace MrCMS.Web.Apps.Ecommerce.Services.Search
+{
+    public interface ISearchLogService
+    {
+        SearchLog GetById(int id);
+        IList<SearchLog> GetAll();
+        void Add(SearchLog searchLog);
+        void Update(SearchLog searchLog);
+        void Delete(SearchLog searchLog);
+        IPagedList<SearchLog> GetPaged(int pageNum, string search, int pageSize = 10);
+    }
+}
