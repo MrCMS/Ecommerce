54ed93d26154f092331562b5d9b401d9756ec8be
gmstr <gary.m.strong@gmail.com>
2014-12-08 11:47:12 +0000

    Beginning javascript refactor

---
+++ MrCMS.Web/Apps/Ecommerce/Content/Scripts/custom/discount.js
@@ -0,0 +1,21 @@
+﻿(function ($) {
+    function addDiscount(event) {
+        event.preventDefault();
+        var form = $(event.target);
+        $.post(form.attr('action'), form.serialize(), function (result) {
+            $(document).trigger('add-discount', result);
+        });
+    }
+
+    function removeDiscount(event) {
+        event.preventDefault();
+        $.post($(event.target).data('remove-discount-code'), function (result) {
+            $(document).trigger('remove-discount', result);
+        });
+    }
+
+    $(function () {
+        $(document).on('submit', "form[data-apply-discount]", addDiscount);
+        $(document).on('click', '[data-remove-discount-code]', removeDiscount);
+    });
+})(jQuery);
