9be322ede7e1777f062b7c233f4386b8e61d6287
gmstr <gary.m.strong@gmail.com>
2014-12-16 16:07:37 +0000

    Improving resource names for product review settings

--- MrCMS.Web/Apps/Ecommerce/Settings/ProductReviewSettings.cs
+++ MrCMS.Web/Apps/Ecommerce/Settings/ProductReviewSettings.cs
@@ -5,20 +5,21 @@ namespace MrCMS.Web.Apps.Ecommerce.Settings
 {
     public class ProductReviewSettings : SiteSettingsBase
     {
-        [DisplayName("Product Reviews")]
-        public bool EnableProductReviews { get; set; }
-
-        [DisplayName("Guest Reviews")]
-        public bool GuestReviews { get; set; }
-        [DisplayName("Helpfulness Votes")]
-        public bool HelpfulnessVotes { get; set; }
-
-        [DisplayName("Page Size")]
-        public int PageSize { get; set; }
-
         public ProductReviewSettings()
         {
             PageSize = 10;
         }
+
+        [DisplayName("Enable Product Reviews?")]
+        public bool EnableProductReviews { get; set; }
+
+        [DisplayName("Enable Guest Reviews?")]
+        public bool GuestReviews { get; set; }
+
+        [DisplayName("Enable Helpfulness Votes?")]
+        public bool HelpfulnessVotes { get; set; }
+
+        [DisplayName("Page Size")]
+        public int PageSize { get; set; }
     }
 }
