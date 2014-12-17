18e2a9a2275944c50636ef02e7708d0b5efee00e
Myo Zar Ni <myo.zarni@thought.co.uk>
2014-12-17 17:21:47 +0000

    product reviews & gif message

--- MrCMS.Web/Apps/Ecommerce/Services/ProductReviews/HelpfulnessVoteService.cs
+++ MrCMS.Web/Apps/Ecommerce/Services/ProductReviews/HelpfulnessVoteService.cs
@@ -1,7 +1,13 @@
 using System.Collections.Generic;
+using System.Linq;
+using MarketplaceWebServiceFeedsClasses;
+using MrCMS.Entities.People;
 using MrCMS.Helpers;
 using MrCMS.Web.Apps.Ecommerce.Entities.ProductReviews;
+using MrCMS.Web.Apps.Ecommerce.Models;
+using MrCMS.Website;
 using NHibernate;
+using Product = MrCMS.Web.Apps.Ecommerce.Pages.Product;
 
 namespace MrCMS.Web.Apps.Ecommerce.Services.ProductReviews
 {
@@ -14,39 +20,113 @@ namespace MrCMS.Web.Apps.Ecommerce.Services.ProductReviews
             _session = session;
         }
 
-        public HelpfulnessVote GetById(int id)
+        public HelpfulnessVoteResponse Upvote(HelpfulnessVoteModel voteModel)
         {
-            return _session.QueryOver<HelpfulnessVote>().Where(x => x.Id == id).SingleOrDefault();
+            var productReview = _session.Get<ProductReview>(voteModel.ProductReviewId);
+            if (productReview == null)
+            {
+                return new HelpfulnessVoteResponse()
+                {
+                    Type = ProductReviewResponseType.Error,
+                    Message = "Could not find product review to vote"
+                };
+            }
+
+            Product productPage = productReview.ProductVariant.Product;
+            User currentUser = CurrentRequestData.CurrentUser;
+            if (currentUser != null)
+            {
+                return RegisterLoggedInVote(voteModel, productReview, currentUser, productPage, true);
+            }
+
+            return RegisterAnonymousVote(voteModel, productReview, productPage, true);
         }
 
-        public IList<HelpfulnessVote> GetAll()
+        public HelpfulnessVoteResponse Downvote(HelpfulnessVoteModel voteModel)
         {
-            return _session.QueryOver<HelpfulnessVote>().OrderBy(x => x.CreatedOn).Asc.Cacheable().List();
+            var productReview = _session.Get<ProductReview>(voteModel.ProductReviewId);
+            if (productReview == null)
+            {
+                return new HelpfulnessVoteResponse
+                {
+                    Type = ProductReviewResponseType.Error,
+                    Message = "Could not find product review to vote"
+                };
+            }
+            Product productPage = productReview.ProductVariant.Product;
+            User currentUser = CurrentRequestData.CurrentUser;
+            if (currentUser != null)
+            {
+                return RegisterLoggedInVote(voteModel, productReview, currentUser, productPage, false);
+            }
+            return RegisterAnonymousVote(voteModel, productReview, productPage, false);
         }
 
-        public void Add(HelpfulnessVote helpfulnessVote)
+        private HelpfulnessVoteResponse RegisterAnonymousVote(HelpfulnessVoteModel voteModel, ProductReview productReview, Product productPage, bool isHelpful)
         {
-            _session.Transact(session => session.Save(helpfulnessVote));
+            if (productReview.Votes.Any(v => v.IPAddress == voteModel.IPAddress))
+            {
+                return new HelpfulnessVoteResponse
+                {
+                    Type = ProductReviewResponseType.Info,
+                    Message = "Already voted",
+                    RedirectUrl = "~/" + productPage.LiveUrlSegment
+                };
+            }
+
+            var vote = new HelpfulnessVote
+            {
+                IsHelpful = isHelpful,
+                ProductReview = productReview,
+                IPAddress = voteModel.IPAddress
+            };
+
+            productReview.Votes.Add(vote);
+            _session.Transact(session => session.Save(vote));
+            return new HelpfulnessVoteResponse
+            {
+                Message = "Vote registered",
+                RedirectUrl = "~/" + productPage.LiveUrlSegment,
+                Type = ProductReviewResponseType.Success
+            };
         }
 
-        public void Update(HelpfulnessVote helpfulnessVote)
+        private HelpfulnessVoteResponse RegisterLoggedInVote(HelpfulnessVoteModel voteModel, ProductReview productReview, User currentUser, Product productPage, bool isHelpful)
         {
-            _session.Transact(session => session.Update(helpfulnessVote));
-        }
-
-        public void Delete(HelpfulnessVote helpfulnessVote)
-        {
-            _session.Transact(session => session.Delete(helpfulnessVote));
-        }
-
-        public int GetAllHelpfulVotesCount(ProductReview productReview)
-        {
-            return _session.QueryOver<HelpfulnessVote>().Where(a => a.IsHelpful && a.ProductReview.Id == productReview.Id).Cacheable().RowCount();
-        }
-
-        public int GetAllUnhelpfulVotesCount(ProductReview productReview)
-        {
-            return _session.QueryOver<HelpfulnessVote>().Where(a => !a.IsHelpful && a.ProductReview.Id == productReview.Id).Cacheable().RowCount();
+            if (productReview.Votes.Any(v => v.IsHelpful == isHelpful && v.User == currentUser))
+            {
+                return new HelpfulnessVoteResponse
+                {
+                    Type = ProductReviewResponseType.Info,
+                    Message = "Already voted",
+                    RedirectUrl = "~/" + productPage.LiveUrlSegment
+                };
+            }
+            List<HelpfulnessVote> oppositeVotes =
+                productReview.Votes.Where(v => v.IsHelpful != isHelpful && v.User == currentUser).ToList();
+            if (oppositeVotes.Any())
+            {
+                _session.Transact(session => oppositeVotes.ForEach(v =>
+                {
+                    productReview.Votes.Remove(v);
+                    session.Delete(v);
+                }));
+            }
+            var vote = new HelpfulnessVote
+            {
+                IsHelpful = isHelpful,
+                User = currentUser,
+                ProductReview = productReview,
+                IPAddress = voteModel.IPAddress
+            };
+            productReview.Votes.Add(vote);
+            _session.Transact(session => session.Save(vote));
+            return new HelpfulnessVoteResponse
+            {
+                Message = "Vote registered",
+                RedirectUrl = "~/" + productPage.LiveUrlSegment,
+                Type = ProductReviewResponseType.Success
+            };
         }
     }
 }
