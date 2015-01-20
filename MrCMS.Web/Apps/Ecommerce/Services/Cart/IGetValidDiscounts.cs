64a4b7fda9703733a23eadda6e97a7e4373a9953
gmstr <gary.m.strong@gmail.com>
2015-01-20 14:56:36 +0000

    Updating discount application to display more information about why they were not applied

--- MrCMS.Web/Apps/Ecommerce/Services/Cart/IGetValidDiscounts.cs
+++ MrCMS.Web/Apps/Ecommerce/Services/Cart/IGetValidDiscounts.cs
@@ -7,5 +7,6 @@ namespace MrCMS.Web.Apps.Ecommerce.Services.Cart
     public interface IGetValidDiscounts
     {
         IList<Discount> Get(CartModel cart, List<string> discountCodes);
+        CheckCodeResult CheckCode(CartModel cart, string discountCode);
     }
 }
