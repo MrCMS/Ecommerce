c7cb38151b47d9f65128b74d7cf1e4523daf56e5
gmstr <gary.m.strong@gmail.com>
2015-01-20 11:15:45 +0000

    Ensuring that SagePay vendor name is to lower case for signature check

--- MrCMS.Web/Apps/Ecommerce/Models/ProductSearchQuery.cs
+++ MrCMS.Web/Apps/Ecommerce/Models/ProductSearchQuery.cs
@@ -1,26 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using Lucene.Net.Analysis;
-using Lucene.Net.Documents;
-using Lucene.Net.Index;
-using Lucene.Net.QueryParsers;
-using Lucene.Net.Search;
-using MrCMS.Helpers;
-using MrCMS.Indexing;
-using MrCMS.Indexing.Management;
-using MrCMS.Indexing.Utils;
-using MrCMS.Web.Apps.Ecommerce.Helpers;
-using MrCMS.Web.Apps.Ecommerce.Indexing;
-using MrCMS.Web.Apps.Ecommerce.Indexing.ProductSearchFieldDefinitions;
-using MrCMS.Web.Apps.Ecommerce.Pages;
-using MrCMS.Web.Apps.Ecommerce.Services.Products;
-using MrCMS.Web.Apps.Ecommerce.Settings;
-using MrCMS.Website;
-using NHibernate;
-using Filter = Lucene.Net.Search.Filter;
-using Version = Lucene.Net.Util.Version;
 
 namespace MrCMS.Web.Apps.Ecommerce.Models
 {
