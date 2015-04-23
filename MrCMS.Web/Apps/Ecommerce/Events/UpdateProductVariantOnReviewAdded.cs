add6f6829bd0800c73a2045a2e5a8bfef9a974da
Andrew King <andrew.king@thought.co.uk>
2015-04-23 15:40:16 +0100

    star rating

---
+++ MrCMS.Web/Apps/Ecommerce/Events/UpdateProductVariantOnReviewAdded.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using MrCMS.Events;
+using MrCMS.Helpers;
+using MrCMS.Web.Apps.Ecommerce.Entities.ProductReviews;
+using NHibernate;
+
+namespace MrCMS.Web.Apps.Ecommerce.Events
+{
+    public class UpdateProductVariantOnReviewAdded : IOnAdded<ProductReview>
+    {
+        private readonly ISession _session;
+
+        public UpdateProductVariantOnReviewAdded(ISession session)
+        {
+            _session = session;
+        }
+
+        public void Execute(OnAddedArgs<ProductReview> args)
+        {
+            if (args.Item.Approved == false)
+                return;
+
+            var productVariant = args.Item.ProductVariant;
+            var reviews =
+                _session.QueryOver<ProductReview>()
+                    .Where(x => x.ProductVariant.Id == productVariant.Id && x.Approved == true)
+                    .Cacheable()
+                    .List();
+
+            if(!reviews.Any())
+                return;
+
+            productVariant.NumberOfReviews = reviews.Count;
+            var rating = (decimal)(reviews.Average(x => x.Rating));
+            productVariant.Rating = (Math.Round((rating * 2), MidpointRounding.AwayFromZero) / 2);
+
+            _session.Transact(session => session.Update(productVariant));
+        }
+    }
+}
---
+++ MrCMS.Web/Apps/Ecommerce/UrlGenerators/ProductUrlGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using MrCMS.Entities.Documents.Web;
+using MrCMS.Helpers;
+using MrCMS.Services;
+using MrCMS.Web.Apps.Ecommerce.Pages;
+using MrCMS.Web.Apps.Ecommerce.Settings;
+
+namespace MrCMS.Web.Apps.Ecommerce.UrlGenerators
+{
+    public class ProductUrlGenerator : WebpageUrlGenerator<Product>
+    {
+        private readonly EcommerceSettings _settings;
+
+        public ProductUrlGenerator(EcommerceSettings settings)
+        {
+            _settings = settings;
+        }
+
+        public override string GetUrl(string pageName, Webpage parent, bool useHierarchy)
+        {
+            var productUrl = _settings.ProductUrl ?? "product";
+
+            var stringBuilder = new StringBuilder();
+
+            if (useHierarchy && parent != null)
+            {
+                stringBuilder.Insert(0, SeoHelper.TidyUrl(parent.UrlSegment) + "/");
+            }
+
+            stringBuilder.Append(string.Format("{0}/", productUrl));
+            stringBuilder.Append(SeoHelper.TidyUrl(pageName));
+            return stringBuilder.ToString();
+        }
+    }
+}
