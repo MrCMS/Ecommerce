23d47c327505a4fc6bcccad90a2cd861148fded2
Andrew King <andrew.king@thought.co.uk>
2015-04-23 15:49:44 +0100

    deleted on event

--- MrCMS.Web/Apps/Ecommerce/Events/UpdateProductVariantOnReviewDelete.cs
+++ MrCMS.Web/Apps/Ecommerce/Events/UpdateProductVariantOnReviewDelete.cs
@@ -26,11 +26,16 @@ namespace MrCMS.Web.Apps.Ecommerce.Events
                     .List();
 
             if (!reviews.Any())
-                return;
-
-            productVariant.NumberOfReviews = reviews.Count;
-            var rating = (decimal)(reviews.Average(x => x.Rating));
-            productVariant.Rating = (Math.Round((rating * 2), MidpointRounding.AwayFromZero) / 2);
+            {
+                productVariant.NumberOfReviews = 0;
+                productVariant.Rating = 0;
+            }
+            else
+            {
+                productVariant.NumberOfReviews = reviews.Count;
+                var rating = (decimal)(reviews.Average(x => x.Rating));
+                productVariant.Rating = (Math.Round((rating * 2), MidpointRounding.AwayFromZero) / 2);
+            }
 
             _session.Transact(session => session.Update(productVariant));
         }
