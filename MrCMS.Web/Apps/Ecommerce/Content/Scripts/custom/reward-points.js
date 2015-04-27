5157e1cf67f42add896edb7e68da1cef2c5661f2
gmstr <gary.m.strong@gmail.com>
2015-04-27 11:17:01 +0100

    Adding changing including reward points against an order to the order summary

---
+++ MrCMS.Web/Apps/Ecommerce/Content/Scripts/custom/reward-points.js
@@ -0,0 +1,11 @@
+﻿(function ($) {
+    function toggleUseRewardPoints(event) {
+        event.preventDefault();
+        $.post('/Apps/Ecommerce/UseRewardPoints', { useRewardPoints: $(event.target).is(':checked') }, function(result) {
+            $(document).trigger('reward-points-updated', result);
+        });
+    }
+    $(function () {
+        $(document).on('change', "[data-use-reward-points]", toggleUseRewardPoints);
+    });
+})(jQuery);
