0fcf04c7859dc74b5ebb4f00451e4c8dd9eb4597
Andrew King <andrew.king@thought.co.uk>
2015-04-28 10:39:37 +0100

    user orders

---
+++ MrCMS.Web/Apps/Ecommerce/Controllers/UserAccountReviewsController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MrCMS.Website.Controllers;
+
+namespace MrCMS.Web.Apps.Ecommerce.Controllers
+{
+    public class UserAccountReviewsController : MrCMSAppUIController<EcommerceApp>
+    {
+    }
+}
---
+++ MrCMS.Web/Apps/Ecommerce/Models/UserAccountOrdersSearchModel.cs
@@ -0,0 +1,11 @@
+namespace MrCMS.Web.Apps.Ecommerce.Models
+{
+    public class UserAccountOrdersSearchModel
+    {
+        public UserAccountOrdersSearchModel()
+        {
+            Page = 1;
+        }
+        public int Page { get; set; }
+    }
+}
