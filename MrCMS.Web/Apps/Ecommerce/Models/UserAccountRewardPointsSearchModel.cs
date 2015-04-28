cf73830e8315a1d7e7b10796e6200839f3ebafb5
Andrew King <andrew.king@thought.co.uk>
2015-04-28 13:36:48 +0100

    Reward points in user account

---
+++ MrCMS.Web/Apps/Ecommerce/Models/UserAccountRewardPointsSearchModel.cs
@@ -0,0 +1,12 @@
+namespace MrCMS.Web.Apps.Ecommerce.Models
+{
+    public class UserAccountRewardPointsSearchModel
+    {
+        public UserAccountRewardPointsSearchModel()
+        {
+            Page = 1;
+        }
+
+        public int Page { get; set; }
+    }
+}
