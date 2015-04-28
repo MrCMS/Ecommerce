2ef606023dec918e1acb4f8976fc861209eea108
Andrew King <andrew.king@thought.co.uk>
2015-04-28 10:39:37 +0100

    user orders

---
+++ MrCMS.Web/Apps/Ecommerce/ModelBinders/UserAccountOrdersModelBinder.cs
@@ -0,0 +1,28 @@
+using System.Web.Mvc;
+using MrCMS.Web.Apps.Ecommerce.Models;
+using MrCMS.Website.Binders;
+using Ninject;
+
+namespace MrCMS.Web.Apps.Ecommerce.ModelBinders
+{
+    public class UserAccountOrdersModelBinder : MrCMSDefaultModelBinder
+    {
+        public UserAccountOrdersModelBinder(IKernel kernel) : base(kernel)
+        {
+        }
+
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            int pageVal;
+            int? page = int.TryParse(GetValueFromContext(controllerContext, "page"), out pageVal)
+                ? pageVal
+                : (int?) null;
+
+            var model = new UserAccountOrdersSearchModel();
+            if (page != null)
+                model.Page = page.Value;
+
+            return model;
+        }
+    }
+}
