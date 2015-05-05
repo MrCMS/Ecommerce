ab1b3aa4ed538d605c8c84e63001446d2870e133
Andrew King <andrew.king@thought.co.uk>
2015-05-05 17:28:38 +0100

    work

---
+++ MrCMS.Web/Apps/Ecommerce/Payment/Braintree/Models/BraintreePaymentDetailsModel.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MrCMS.Web.Apps.Ecommerce.Payment.Braintree.Models
+{
+    public class BraintreePaymentDetailsModel
+    {
+        [Required]
+        public string CardNumber { get; set; }
+
+        [Required]
+        public string CardholderName { get; set; }
+
+        [Required]
+        public string CVV { get; set; }
+
+        [Required]
+        public int ExpirationMonth { get; set; }
+
+        [Required]
+        public int ExpirationYear { get; set; }
+
+        public string PostalCode { get; set; }
+    }
+}
