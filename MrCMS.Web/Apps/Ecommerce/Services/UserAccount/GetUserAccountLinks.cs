e713ca09a8821d5eb546cca9eaa483df7fbdb027
gmstr <gary.m.strong@gmail.com>
2015-05-05 10:47:04 +0100

    Refactoring to reduce repetition.

--- MrCMS.Web/Apps/Ecommerce/Services/UserAccount/GetUserAccountLinks.cs
+++ MrCMS.Web/Apps/Ecommerce/Services/UserAccount/GetUserAccountLinks.cs
@@ -40,28 +40,19 @@ namespace MrCMS.Web.Apps.Ecommerce.Services.UserAccount
 
         private List<Webpage> GetPages()
         {
-            var userAccountInfo = _uniquePageService.GetUniquePage<UserAccountInfo>();
-            var userAccountAddresses = _uniquePageService.GetUniquePage<UserAccountAddresses>();
-            var userAccountChangePassword = _uniquePageService.GetUniquePage<UserAccountChangePassword>();
-            var userAccountOrders = _uniquePageService.GetUniquePage<UserAccountOrders>();
-            var userAccountReviews = _uniquePageService.GetUniquePage<UserAccountReviews>();
-            var userAccountRewards = _uniquePageService.GetUniquePage<UserAccountRewardPoints>();
+            // Try and get them all
+            var pages = new List<Webpage>
+            {
+                _uniquePageService.GetUniquePage<UserAccountInfo>(),
+                _uniquePageService.GetUniquePage<UserAccountAddresses>(),
+                _uniquePageService.GetUniquePage<UserAccountChangePassword>(),
+                _uniquePageService.GetUniquePage<UserAccountOrders>(),
+                _uniquePageService.GetUniquePage<UserAccountReviews>(),
+                _uniquePageService.GetUniquePage<UserAccountRewardPoints>()
+            };
 
-            var pages = new List<Webpage>();
-            if (userAccountInfo != null)
-                pages.Add(userAccountInfo);
-            if (userAccountChangePassword != null)
-                pages.Add(userAccountChangePassword);
-            if (userAccountOrders != null)
-                pages.Add(userAccountOrders);
-            if (userAccountAddresses != null)
-                pages.Add(userAccountAddresses);
-            if (userAccountReviews != null && _productReviewSettings.EnableProductReviews)
-                pages.Add(userAccountReviews);
-            if (userAccountRewards != null && _ecommerceSettings.RewardPointsEnabled)
-                pages.Add(userAccountRewards);
-
-            return pages;
+            // Filter out ones that haven't been added
+            return pages.FindAll(webpage => webpage != null);
         }
     }
 }
