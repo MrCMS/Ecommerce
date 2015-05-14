25a8212606d8cfb3bca04120fc65604edc51c87e
gmstr <gary.m.strong@gmail.com>
2015-05-14 11:10:14 +0100

    Refactoring to move login check logic to MustBeLoggedInAttribute

---
+++ MrCMS.Web/Apps/Ecommerce/Filters/MustBeLoggedInAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using MrCMS.Entities.People;
+using MrCMS.Services;
+using MrCMS.Web.Apps.Core.Pages;
+using MrCMS.Website;
+
+namespace MrCMS.Web.Apps.Ecommerce.Filters
+{
+    public class MustBeLoggedInAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var uniquePageService = filterContext.HttpContext.Get<IUniquePageService>();
+            User user = CurrentRequestData.CurrentUser;
+            if (user == null)
+                filterContext.Result = uniquePageService.RedirectTo<LoginPage>();
+        }
+    }
+}
