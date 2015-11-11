dcae7e90db82e850f014ac8def0d991c924f1eb6
WillAnd <will.anderson@thought.co.uk>
2015-11-11 09:20:43 +0000

    Update changes from Ry Amazon App

--- MrCMS.Web/Apps/Amazon/DbConfiguration/AmazonTableNameConvention.cs
+++ MrCMS.Web/Apps/Amazon/DbConfiguration/AmazonTableNameConvention.cs
@@ -5,6 +5,7 @@ using MrCMS.Apps;
 using MrCMS.Entities.Messaging;
 using MrCMS.Entities.People;
 using MrCMS.Entities.Widget;
+using MrCMS.Messages;
 
 namespace MrCMS.Web.Apps.Amazon.DbConfiguration
 {
@@ -15,7 +16,7 @@ namespace MrCMS.Web.Apps.Amazon.DbConfiguration
             var entityType = instance.EntityType;
             if (MrCMSApp.AllAppTypes.ContainsKey(entityType) && MrCMSApp.AllAppTypes[entityType] == AmazonApp.AmazonAppName)
             {
-                if (!entityType.IsSubclassOf(typeof(Document)) &&
+                if (!entityType.IsSubclassOf(typeof(Document)) && !entityType.IsSubclassOf(typeof(MessageTemplate)) &&
                     !entityType.IsSubclassOf(typeof(UserProfileData)) && !entityType.IsSubclassOf(typeof(Widget)))
                 {
                     instance.Table(string.Format(AmazonApp.AmazonAppName+"_{0}", instance.TableName.Replace("`", "")));
