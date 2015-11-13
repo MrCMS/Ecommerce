b745ebd21d4e1224fbaa2686db94bbdae4a34757
gmstr <gary.m.strong@gmail.com>
2015-11-13 13:52:51 +0000

    Updating Google Base loading

--- MrCMS.EcommerceApp.Tests/Services/GoogleBaseManagerTests.cs
+++ MrCMS.EcommerceApp.Tests/Services/GoogleBaseManagerTests.cs
@@ -4,28 +4,26 @@ using MrCMS.Helpers;
 using MrCMS.Web.Apps.Ecommerce.Entities.GoogleBase;
 using MrCMS.Web.Apps.Ecommerce.Entities.Products;
 using MrCMS.Web.Apps.Ecommerce.Services;
-using MrCMS.Web.Apps.Ecommerce.Services.Products;
-using Xunit;
 using MrCMS.Web.Apps.Ecommerce.Services.GoogleBase;
-using MrCMS.Web.Apps.Ecommerce.Services.Orders;
 using MrCMS.Web.Apps.Ecommerce.Services.Pricing;
+using Xunit;
 
 namespace MrCMS.EcommerceApp.Tests.Services
 {
     public class GoogleBaseManagerTests : InMemoryDatabaseTest
     {
         private readonly GoogleBaseManager _googleBaseManager;
-        private readonly IProductVariantService _productVariantService;
-        private IGoogleBaseShippingService _googleBaseShippingService;
-        private IGetStockRemainingQuantity _getStockRemainingQuantity;
         private readonly IProductPricingMethod _productPricingMethod = A.Fake<IProductPricingMethod>();
+        private readonly IGetStockRemainingQuantity _getStockRemainingQuantity;
+
+        private readonly IGoogleBaseShippingService _googleBaseShippingService;
 
         public GoogleBaseManagerTests()
         {
-            _productVariantService = A.Fake<IProductVariantService>();
             _googleBaseShippingService = A.Fake<IGoogleBaseShippingService>();
             _getStockRemainingQuantity = A.Fake<IGetStockRemainingQuantity>();
-            _googleBaseManager = new GoogleBaseManager(Session, _productVariantService, _googleBaseShippingService, _getStockRemainingQuantity, _productPricingMethod);
+            _googleBaseManager = new GoogleBaseManager(Session, _googleBaseShippingService, _getStockRemainingQuantity,
+                _productPricingMethod);
         }
 
         [Fact]
@@ -45,18 +43,10 @@ namespace MrCMS.EcommerceApp.Tests.Services
         }
 
         [Fact]
-        public void GoogleBaseManager_ExportProductsToGoogleBase_ShouldCallGetAllVariantsOfProductVariantService()
-        {
-            _googleBaseManager.ExportProductsToGoogleBase();
-
-            A.CallTo(() => _productVariantService.GetAllVariantsForGoogleBase()).MustHaveHappened();
-        }
-
-        [Fact]
         public void GoogleBaseService_SaveGoogleBaseProduct_ShouldUpdateItem()
         {
             var pv = new ProductVariant();
-            var item = new GoogleBaseProduct { ProductVariant = pv };
+            var item = new GoogleBaseProduct {ProductVariant = pv};
 
             Session.Transact(session => session.Save(item));
             item.Grouping = "Group2";
