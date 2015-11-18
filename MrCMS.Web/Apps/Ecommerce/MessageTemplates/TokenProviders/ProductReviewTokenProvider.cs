7f6880f43ff9ea71a848b717c2e370dcf8118381
Md Ibrahim Rashid <mdibrahimrashid@gmail.com>
2015-11-18 20:27:42 +0530

    Added Product Review Admin Template

---
+++ MrCMS.Web/Apps/Ecommerce/MessageTemplates/TokenProviders/ProductReviewTokenProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using MrCMS.Services;
+using MrCMS.Web.Apps.Ecommerce.Entities.ProductReviews;
+
+namespace MrCMS.Web.Apps.Ecommerce.MessageTemplates.TokenProviders
+{
+    public class ProductReviewTokenProvider : ITokenProvider<ProductReview>
+    {
+        private IDictionary<string, Func<ProductReview, string>> _tokens;
+        public IDictionary<string, Func<ProductReview, string>> Tokens { get { return _tokens = _tokens ?? GetTokens(); } }
+        private IDictionary<string, Func<ProductReview, string>> GetTokens()
+        {
+            return new Dictionary<string, Func<ProductReview, string>>
+                {
+                    {"ProductUrl", request => request.ProductVariant.Product != null ? request.ProductVariant.Product.AbsoluteUrl  : null},
+                    {"Name", request => request.ProductVariant.Product.Name},
+                    {"UserName",request=>request.User.Name}
+                };
+        }
+    }
+}
