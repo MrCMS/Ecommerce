28dba2c04d366b32afab01a6676e5f45f71f9ef3
Daniel Knox <yandawl@gmail.com>
2016-04-04 14:45:46 +0100

    Updated to universal analytics to log transactions with Google

--- MrCMS.Web/Apps/Ecommerce/Services/Analytics/IGoogleAnalyticsService.cs
+++
@@ -1,9 +0,0 @@
-using MrCMS.Web.Apps.Ecommerce.Entities.Orders;
-
-namespace MrCMS.Web.Apps.Ecommerce.Services.Analytics
-{
-    public interface IGoogleAnalyticsService
-    {
-        string GetAnalayticsCode(Order order, string trackingScript);
-    }
-}
