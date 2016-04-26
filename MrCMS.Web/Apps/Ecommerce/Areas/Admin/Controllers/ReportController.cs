c670cb6220ae77ebb205d6dff5e879af4462b1ef
Daniel Knox <yandawl@gmail.com>
2016-04-26 14:25:48 +0100

    * Fixed an issue with chart on dashboard where duplicate revenue entries would show on the first two days * Added details of orders and order items for the sales by day report

--- MrCMS.Web/Apps/Ecommerce/Areas/Admin/Controllers/ReportController.cs
+++ MrCMS.Web/Apps/Ecommerce/Areas/Admin/Controllers/ReportController.cs
@@ -1,27 +1,40 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using MrCMS.Web.Apps.Ecommerce.ACL;
+using MrCMS.Web.Apps.Ecommerce.Areas.Admin.Models;
 using MrCMS.Web.Apps.Ecommerce.Models;
+using MrCMS.Web.Apps.Ecommerce.Services.Analytics.Orders;
 using MrCMS.Web.Apps.Ecommerce.Services.Reports;
 using MrCMS.Website;
 using MrCMS.Website.Controllers;
-using MrCMS.Website.Filters;
 
 namespace MrCMS.Web.Apps.Ecommerce.Areas.Admin.Controllers
 {
     public class ReportController : MrCMSAppAdminController<EcommerceApp>
     {
         private readonly IReportService _reportService;
+        private readonly IOrderAnalyticsService _orderAnalyticsService;
 
-        public ReportController(IReportService reportService)
+        public ReportController(IReportService reportService, IOrderAnalyticsService orderAnalyticsService)
         {
             _reportService = reportService;
+            _orderAnalyticsService = orderAnalyticsService;
         }
 
         [HttpGet]
         [MrCMSACLRule(typeof(SalesByDayACL), SalesByDayACL.View)]
         public ViewResult SalesByDay(ChartModel model)
         {
-            return View(model);
+            var data = _orderAnalyticsService.GetRevenueWithOrdersGroupedByDate(model.From, model.To);
+
+            var viewModel = new SalesByDayViewModel
+            {
+                From = model.From,
+                To = model.To,
+                Sales = data.ToList()
+            };
+
+            return View(viewModel);
         }
 
         [HttpPost]
---
+++ MrCMS.Web/Apps/Ecommerce/Areas/Admin/Models/Sales.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MrCMS.Web.Apps.Ecommerce.Areas.Admin.Models
+{
+    public class Sales
+    {
+        public DateTime Date { get; set; }
+        public int OrdersCount { get; set; }
+        public int OrderItemsCount { get; set; }
+        public decimal TotalRevenue { get; set; }
+    }
+}
