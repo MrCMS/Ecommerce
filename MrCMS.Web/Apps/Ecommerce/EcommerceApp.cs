da401c5efc0760052209d9b6ef217f6c8d96b735
WillAnd <will.anderson@thought.co.uk>
2016-07-26 10:30:06 +0100

    update release number

--- MrCMS.Web/Apps/Ecommerce/EcommerceApp.cs
+++ MrCMS.Web/Apps/Ecommerce/EcommerceApp.cs
@@ -30,7 +30,7 @@ namespace MrCMS.Web.Apps.Ecommerce
 
         public override string Version
         {
-            get { return "0.4.1"; }
+            get { return "0.5"; }
         }
 
         protected override void RegisterServices(IKernel kernel)
